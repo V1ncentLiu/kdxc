1e46a7484059b478e4f70b76088fa8b9554e0251
chenchengxue <chenchengxue@kuaidaoapp.com>
2018-12-26 09:01:17 +0800

    全局异常配置

---
+++ src/main/java/com/kuaidao/manageweb/constant/Constants.java
@@ -0,0 +1,11 @@
+package com.kuaidao.manageweb.constant;
+
+/**
+ *  manageweb 常量類
+ * @author: Chen Chengxue
+ * @date: 2018年12月26日 上午8:56:29   
+ * @version V1.0
+ */
+public class Constants {
+
+}
---
+++ src/main/java/com/kuaidao/manageweb/handler/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.kuaidao.manageweb.handler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+
+    private static Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+    
+    public static final String DEFAULT_ERROR_VIEW = "error";
+
+    @ExceptionHandler(value = Exception.class)
+    public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
+        logger.error("GlobalExceptionHandler ",e);
+   
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", e);
+        mav.addObject("url", req.getRequestURL());
+        mav.setViewName(DEFAULT_ERROR_VIEW);
+        return mav;
+    }
+
+}
