ed802fba5083d613aad23f44183181b0c2acb46c
chenchengxue <chenchengxue@kuaidaoapp.com>
2018-12-29 13:47:51 +0800

    添加 自定义菜单 controller

--- src/main/java/com/kuaidao/manageweb/ManageWebApplication.java
+++ src/main/java/com/kuaidao/manageweb/ManageWebApplication.java
@@ -1,19 +1,19 @@
 package com.kuaidao.manageweb;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-import feign.Logger;
 
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
 @EnableCircuitBreaker
+//@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 public class ManageWebApplication 
 {
     public static void main( String[] args )
---
+++ src/main/java/com/kuaidao/manageweb/controller/customefield/CustomFieldController.java
@@ -0,0 +1,35 @@
+package com.kuaidao.manageweb.controller.customefield;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *  自定义字段 
+ * @author: Chen Chengxue
+ * @date: 2018年12月28日 下午1:45:12   
+ * @version V1.0
+ */
+@Controller
+@RequestMapping("/customfield/customField")
+public class CustomFieldController {
+    
+    /***
+     *   菜单  首页
+     * @return
+     */
+    @RequestMapping("/customFieldMenuPage")
+    public String customFieldMenuPage() {
+        return "";
+    }
+    
+    
+    /***
+     *  自定义字段 首页
+     * @return
+     */
+    @RequestMapping("/customFieldPage")
+    public String customFieldPage() {
+        return "customfield/customFieldPage";
+    }
+
+}
