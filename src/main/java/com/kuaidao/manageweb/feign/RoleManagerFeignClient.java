71454272ab54b20a2afc5bb0d4d35aed480e965d
gaoandli <gao_pengcheng@163.com>
2019-01-03 17:40:10 +0800

    代码提交

---
+++ src/main/java/com/kuaidao/manageweb/feign/RoleManagerFeignClient.java
@@ -0,0 +1,49 @@
+package com.kuaidao.manageweb.feign;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.kuaidao.common.constant.SysErrorCodeEnum;
+import com.kuaidao.common.entity.JSONResult;
+import com.kuaidao.sys.dto.role.RoleInfoDTO;
+import com.kuaidao.sys.dto.role.RoleQueryDTO;
+
+@FeignClient(name = "sys-service", fallback = RoleManagerFeignClient.HystrixClientFallback.class)
+public interface RoleManagerFeignClient {
+	
+
+	@RequestMapping(method = RequestMethod.POST, value = "/role/roleManager/queryRoleList")
+	public JSONResult<List<RoleInfoDTO>> queryRoleList(RoleQueryDTO dto);
+	
+	
+	
+	
+
+	@Component
+	static class HystrixClientFallback implements RoleManagerFeignClient {
+
+		private static final Logger logger = LoggerFactory.getLogger(RoleManagerFeignClient.class);
+
+		@SuppressWarnings("rawtypes")
+		private JSONResult fallBackError(String name) {
+			logger.error(name + "接口调用失败：无法获取目标服务");
+			return new JSONResult().fail(SysErrorCodeEnum.ERR_REST_FAIL.getCode(),
+					SysErrorCodeEnum.ERR_REST_FAIL.getMessage());
+		}
+
+		@SuppressWarnings("unchecked")
+		@Override
+		public JSONResult<List<RoleInfoDTO>> queryRoleList(RoleQueryDTO dto) {
+
+			return fallBackError("查询角色列表数据失败");
+		}
+
+	}
+
+}
