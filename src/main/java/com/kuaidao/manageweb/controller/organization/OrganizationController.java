103142e828a547dda18989965a663d6920c7e313
chenchengxue <chenchengxue@kuaidaoapp.com>
2019-01-03 16:50:38 +0800

    添加 feignclient

--- src/main/java/com/kuaidao/manageweb/controller/organization/OrganizationController.java
+++ src/main/java/com/kuaidao/manageweb/controller/organization/OrganizationController.java
@@ -1,8 +1,32 @@
 package com.kuaidao.manageweb.controller.organization;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.github.pagehelper.PageHelper;
+import com.kuaidao.common.entity.IdEntity;
+import com.kuaidao.common.entity.JSONResult;
+import com.kuaidao.common.entity.PageBean;
+import com.kuaidao.common.entity.TreeData;
+import com.kuaidao.common.util.CommonUtil;
+import com.kuaidao.manageweb.feign.organization.OrganizationFeignClient;
+import com.kuaidao.sys.dto.organization.OrganitionRespDTO;
+import com.kuaidao.sys.dto.organization.OrganizationAddAndUpdateDTO;
+import com.kuaidao.sys.dto.organization.OrganizationQueryDTO;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.validation.Valid;
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 
 /**
  * 组织机构类
@@ -13,7 +37,11 @@ import org.springframework.stereotype.Controller;
 @Controller
 @RequestMapping("/organization/organization")
 public class OrganizationController {
-    
+
+    private static Logger logger = LoggerFactory.getLogger(OrganizationController.class);
+    @Autowired
+    OrganizationFeignClient organizationFeignClient;
+
     /**
      * 组织机构首页
      * @return
@@ -22,5 +50,71 @@ public class OrganizationController {
     public String organizationPage() {
         return "organization/organizationPage";
     }
+
+    @PostMapping("/save")
+    public JSONResult save(@Valid @RequestBody OrganizationAddAndUpdateDTO orgDTO,
+            BindingResult result) throws Exception {
+        if (result.hasErrors()) {
+            return CommonUtil.validateParam(result);
+        }
+        return organizationFeignClient.save(orgDTO);
+    }
+
+
+    /**
+     *   更新组织机构
+     * @param orgDTO
+     * @return
+     */
+    @PostMapping("/update")
+    public JSONResult update(@Valid @RequestBody OrganizationAddAndUpdateDTO orgDTO,
+            BindingResult result) {
+        if (result.hasErrors()) {
+            return CommonUtil.validateParam(result);
+        }
+        return organizationFeignClient.update(orgDTO);
+    }
+
+
+    /**
+     *   删除组织机构
+     * @param orgDTO
+     * @return
+     */
+    @PostMapping("/delete")
+    @ResponseBody
+    public JSONResult delete(@RequestBody IdEntity idEntity) {
+        String id = idEntity.getId();
+        logger.info("delete organization by id{{}}", id);
+        return organizationFeignClient.delete(idEntity);
+    }
+
+
+    @PostMapping("/queryOrgDataByParam")
+    @ResponseBody
+    public JSONResult<PageBean<OrganitionRespDTO>> queryOrgDataByParam(int pageNum, int pageSize,
+            @RequestBody OrganizationQueryDTO queryDTO) {
+        queryDTO.setPageNum(pageNum);
+        queryDTO.setPageSize(pageSize);
+        return organizationFeignClient.queryOrgDataByParam(queryDTO);
+    }
+
+    /**
+     * 查询组织信息 
+     * @param queryDTO
+     * @return
+     */
+    @PostMapping("/queryOrgByParam")
+    public JSONResult<List<OrganitionRespDTO>> queryOrgByParam(
+            @RequestBody OrganizationQueryDTO queryDTO) {
+        return organizationFeignClient.queryOrgByParam(queryDTO);
+    }
+    
+    @PostMapping("/query")
+    @ResponseBody
+    public JSONResult<TreeData> query(){
+        return null;
+    }
+    
 
 }
---
+++ src/main/java/com/kuaidao/manageweb/feign/organization/OrganizationFeignClient.java
@@ -0,0 +1,98 @@
+package com.kuaidao.manageweb.feign.organization;
+
+import java.util.List;
+import javax.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import com.kuaidao.common.constant.SysErrorCodeEnum;
+import com.kuaidao.common.entity.IdEntity;
+import com.kuaidao.common.entity.JSONResult;
+import com.kuaidao.common.entity.PageBean;
+import com.kuaidao.common.entity.TreeData;
+import com.kuaidao.sys.dto.organization.OrganitionRespDTO;
+import com.kuaidao.sys.dto.organization.OrganizationAddAndUpdateDTO;
+import com.kuaidao.sys.dto.organization.OrganizationQueryDTO;
+
+@FeignClient(name = "sys-service",path="/sys/organization/organization",fallback = OrganizationFeignClient.HystrixClientFallback.class)
+public interface OrganizationFeignClient {
+
+    @PostMapping("/save")
+    public JSONResult save(@RequestBody OrganizationAddAndUpdateDTO orgDTO);
+    
+    @PostMapping("/update")
+    public JSONResult update(@RequestBody OrganizationAddAndUpdateDTO orgDTO);   
+    
+    @RequestMapping(method = RequestMethod.POST, value = "/delete")
+    public JSONResult delete(@RequestBody IdEntity idEntity);
+    
+    @PostMapping("/queryOrgDataByParam")
+    public JSONResult<PageBean<OrganitionRespDTO>> queryOrgDataByParam(@RequestBody OrganizationQueryDTO queryDTO);
+    
+    @PostMapping("/queryOrgByParam")
+    public JSONResult<List<OrganitionRespDTO>> queryOrgByParam( @RequestBody OrganizationQueryDTO queryDTO);
+    
+    /**
+     * 查询组织机构数
+     * @param queryDTO
+     * @return
+     */
+    @PostMapping("/query")
+    JSONResult<TreeData> query();
+    
+    @Component
+    static class HystrixClientFallback implements  OrganizationFeignClient{
+        
+        private static Logger logger = LoggerFactory.getLogger(OrganizationFeignClient.class);
+
+        
+        private JSONResult fallBackError(String name) {
+            logger.error(name + "接口调用失败：无法获取目标服务");
+            return new JSONResult().fail(SysErrorCodeEnum.ERR_REST_FAIL.getCode(),
+                    SysErrorCodeEnum.ERR_REST_FAIL.getMessage());
+        }
+
+
+        @Override
+        public JSONResult save(OrganizationAddAndUpdateDTO orgDTO) {
+            return fallBackError("保存组织机构");
+        }
+
+
+        @Override
+        public JSONResult update(OrganizationAddAndUpdateDTO orgDTO) {
+            return fallBackError("更新组织机构");
+        }
+
+
+        @Override
+        public JSONResult delete(IdEntity idEntity) {
+            return fallBackError("删除组织机构");
+        }
+
+
+        @Override
+        public JSONResult<PageBean<OrganitionRespDTO>> queryOrgDataByParam(OrganizationQueryDTO queryDTO) {
+            return fallBackError("查询组织机构数据，分页");
+        }
+
+
+        @Override
+        public JSONResult<List<OrganitionRespDTO>> queryOrgByParam(OrganizationQueryDTO queryDTO) {
+            return fallBackError("查询组织机构");
+        }
+
+
+        @Override
+        public JSONResult<TreeData> query() {
+            return fallBackError("查询组织机构树");
+        }
+    }
+
+}
