82a2d5140a1dcba3838efdafaffee66e4b018f98
zhangxingyu <408744797@qq.com>
2019-01-10 17:41:55 +0800

    tijao

--- src/main/java/com/kuaidao/manageweb/controller/user/UserController.java
+++ src/main/java/com/kuaidao/manageweb/controller/user/UserController.java
@@ -19,8 +19,11 @@ import com.kuaidao.common.entity.IdEntityLong;
 import com.kuaidao.common.entity.JSONResult;
 import com.kuaidao.common.entity.PageBean;
 import com.kuaidao.common.util.CommonUtil;
+import com.kuaidao.common.util.MD5Util;
 import com.kuaidao.manageweb.feign.role.RoleManagerFeignClient;
 import com.kuaidao.manageweb.feign.user.UserInfoFeignClient;
+import com.kuaidao.sys.constant.UserErrorCodeEnum;
+import com.kuaidao.sys.dto.user.UpdateUserPasswordReq;
 import com.kuaidao.sys.dto.user.UserInfoDTO;
 import com.kuaidao.sys.dto.user.UserInfoPageParam;
 import com.kuaidao.sys.dto.user.UserInfoReq;
@@ -115,6 +118,44 @@ public class UserController {
         }
 
         return userInfoFeignClient.update(userInfoReq);
+    }
+
+    /**
+     * 修改密码
+     * 
+     * @param orgDTO
+     * @return
+     */
+    @PostMapping("/updatePassword")
+    @ResponseBody
+    public JSONResult updateMenu(@Valid @RequestBody UpdateUserPasswordReq updateUserPasswordReq,
+            BindingResult result) {
+
+        if (result.hasErrors()) {
+            return CommonUtil.validateParam(result);
+        }
+        Long id = updateUserPasswordReq.getId();
+        // 查询用户信息
+        JSONResult<UserInfoDTO> jsonResult = userInfoFeignClient.get(new IdEntityLong(id));
+        UserInfoDTO data = jsonResult.getData();
+        if (JSONResult.SUCCESS.equals(jsonResult.getCode()) && data != null) {
+            // 判断密码是否正确
+            if (data.getPassword().equals(MD5Util.StringToMd5(MD5Util
+                    .StringToMd5(updateUserPasswordReq.getOldPassword() + data.getSalt())))) {
+                UserInfoReq userInfoReq = new UserInfoReq();
+                userInfoReq.setId(id);
+                userInfoReq.setPassword(updateUserPasswordReq.getNewPassword());
+                // 修改密码
+                return userInfoFeignClient.update(userInfoReq);
+            } else {
+                return new JSONResult().fail(UserErrorCodeEnum.ERR_WRONG_PHONE_PASSWORD.getCode(),
+                        UserErrorCodeEnum.ERR_WRONG_PHONE_PASSWORD.getMessage());
+            }
+        } else {
+            return new JSONResult().fail(UserErrorCodeEnum.ERR_ACCOUNT_NOT_EXIST.getCode(),
+                    UserErrorCodeEnum.ERR_ACCOUNT_NOT_EXIST.getMessage());
+        }
+
     }
 
     /**
