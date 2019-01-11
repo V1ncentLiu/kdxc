fdcbb99cd8e74cd425f396933624bac55b973d77
zhangxingyu <408744797@qq.com>
2019-01-11 21:58:14 +0800

    提交

--- src/main/java/com/kuaidao/manageweb/ManageWebApplication.java
+++ src/main/java/com/kuaidao/manageweb/ManageWebApplication.java
@@ -16,28 +16,27 @@ import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 @EnableEurekaClient
 @EnableFeignClients
 @EnableCircuitBreaker
-public class ManageWebApplication 
-{
+public class ManageWebApplication {
 
-    
-    public static void main( String[] args )
-    {
-    	SpringApplication.run(ManageWebApplication.class, args);
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageWebApplication.class, args);
     }
 
-    
     @Bean("jackson2ObjectMapperBuilderCustomizer")
     public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
-        Jackson2ObjectMapperBuilderCustomizer customizer = new Jackson2ObjectMapperBuilderCustomizer() {
-            @Override
-            public void customize(Jackson2ObjectMapperBuilder jacksonObjectMapperBuilder) {
-                jacksonObjectMapperBuilder.serializerByType(Long.class, ToStringSerializer.instance)
-                        .serializerByType(Long.TYPE, ToStringSerializer.instance);
-            }
-        };
+        Jackson2ObjectMapperBuilderCustomizer customizer =
+                new Jackson2ObjectMapperBuilderCustomizer() {
+                    @Override
+                    public void customize(Jackson2ObjectMapperBuilder jacksonObjectMapperBuilder) {
+                        jacksonObjectMapperBuilder
+                                .serializerByType(Long.class, ToStringSerializer.instance)
+                                .serializerByType(Long.TYPE, ToStringSerializer.instance);
+                    }
+                };
         return customizer;
     }
 
 
-    
+
 }
