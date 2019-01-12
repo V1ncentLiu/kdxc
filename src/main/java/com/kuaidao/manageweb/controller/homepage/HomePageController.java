3ff00e7d163ac130e17a6d997d3339155afdd0d1
chenchengxue <chenchengxue@kuaidaoapp.com>
2019-01-12 17:14:43 +0800

    首页菜单

--- src/main/java/com/kuaidao/manageweb/controller/homepage/HomePageController.java
+++ src/main/java/com/kuaidao/manageweb/controller/homepage/HomePageController.java
@@ -1,15 +1,13 @@
 package com.kuaidao.manageweb.controller.homepage;
 
-import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.kuaidao.sys.dto.module.ModuleInfoDTO;
+import com.kuaidao.sys.dto.module.IndexModuleDTO;
 import com.kuaidao.sys.dto.user.UserInfoDTO;
-import lombok.Data;
 
 @Controller
 @RequestMapping("/homePage")
@@ -21,29 +19,12 @@ public class HomePageController {
      */
     @RequestMapping("/index")
     public String index(HttpServletRequest request) {
-     /*   List<MenuData> list = new ArrayList<>();
-        for (int i = 0; i < 2; i++) {
-            MenuData menuData = new MenuData();
-            menuData.setMenuGroup("菜单组-"+i);
-            List<Menu> subList = new ArrayList<>();
-            for (int j = 0; j < 3; j++) {
-                Menu menu = new Menu();
-                menu.setName("菜单-"+j);
-                menu.setUrl("/customfield/customField/customFieldMenuPage");
-                
-                subList.add(menu);
-            }
-            menuData.setSubList(subList);
-            list.add(menuData);
-        }
         
-        
-       request.setAttribute("menuList",list);*/
-        
-     /*   Subject subject = SecurityUtils.getSubject();
+        Subject subject = SecurityUtils.getSubject();
         UserInfoDTO user = (UserInfoDTO) subject.getSession().getAttribute("user");
         request.setAttribute("user", user);
-        List<ModuleInfoDTO> menuList = user.getMenuList();*/
+        List<IndexModuleDTO> menuList = user.getMenuList();
+        request.setAttribute("menuList",menuList);
        return "index";
     }
 
