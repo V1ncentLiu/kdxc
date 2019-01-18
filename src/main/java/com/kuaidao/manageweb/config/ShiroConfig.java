8a6f4f90c8ec65734a7e7b15e79b1d684ac802a1
zhangxingyu <408744797@qq.com>
2019-01-18 09:42:34 +0800

    tijao

--- src/main/java/com/kuaidao/manageweb/config/ShiroConfig.java
+++ src/main/java/com/kuaidao/manageweb/config/ShiroConfig.java
@@ -34,7 +34,6 @@ public class ShiroConfig {
         // 拦截器.
         Map<String, String> filterChainDefinitionMap = new LinkedHashMap<String, String>();
         // 配置不会被拦截的链接 顺序判断
-        filterChainDefinitionMap.put("/**", "anon");
         filterChainDefinitionMap.put("/css/**", "anon");
         filterChainDefinitionMap.put("/images/**", "anon");
         filterChainDefinitionMap.put("/js/**", "anon");
