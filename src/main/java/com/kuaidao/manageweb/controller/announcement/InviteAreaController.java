e4f70bfaceb2b760ed4e0e0538c0f77ac2fc6315
jiangzhenxueAdministrator <1427692900@qq.com>
2019-01-18 14:00:49 +0800

    邀约区域

--- src/main/java/com/kuaidao/manageweb/controller/announcement/InviteAreaController.java
+++ src/main/java/com/kuaidao/manageweb/controller/announcement/InviteAreaController.java
@@ -4,7 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kuaidao.manageweb.util.DownFile;
+
+import java.io.File;
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @Auther: admin
@@ -38,4 +45,37 @@ public class InviteAreaController {
     public String addInviteArea(HttpServletRequest request) {
         return "inviteArea/addInviteArea";
     }
+    /**
+     * 删除邀约区域
+     * 
+     * @return
+     */
+    @RequestMapping("/deleInviteArea")
+    public String deleInviteArea(HttpServletRequest request) {
+    	String id = request.getParameter("id");
+        return "inviteArea/addInviteArea";
+    }
+    
+    /**
+     * 模板下载
+     * @param request
+     * @param response
+     * @throws Exception
+     */
+	@RequestMapping(value = "/download")
+	public void download(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		DownFile df = new DownFile();
+		// 文件模板在%TOMCAT_HOME%\\webapps\\YourWebProject\\WEB-INF\\model\\文件夹中
+		String filePath = request.getSession().getServletContext().getRealPath("model");
+		String fileName = "area-division.xlsx";
+
+		try {
+			df.downFile(filePath + File.separator + fileName, fileName, request, response);
+		} catch (IOException e) {
+			logger.error("邀约区域下载模板失败：", e);
+		} catch (Exception e) {
+			logger.error("邀约区域下载模板失败：", e);
+		}
+	}
 }
