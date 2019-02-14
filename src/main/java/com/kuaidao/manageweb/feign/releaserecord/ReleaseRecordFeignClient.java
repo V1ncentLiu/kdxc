11e8490eb4a5740e18fde19737d52765de3d23f5
YangBiao <yangbiao@kd.cn>
2019-02-14 14:40:35 +0800

    释放记录-Feign客户端

---
+++ src/main/java/com/kuaidao/manageweb/feign/releaserecord/ReleaseRecordFeignClient.java
@@ -0,0 +1,62 @@
+package com.kuaidao.manageweb.feign.releaserecord;
+
+import com.kuaidao.aggregation.dto.cluereleaserecord.ReleaseRecordInsertOrUpdateDTO;
+import com.kuaidao.aggregation.dto.cluereleaserecord.ReleaseRecordReqDTO;
+import com.kuaidao.aggregation.dto.cluereleaserecord.ReleaseRecordRespDTO;
+import com.kuaidao.aggregation.dto.pubcusres.ClueQueryParamDTO;
+import com.kuaidao.aggregation.dto.pubcusres.PublicCustomerResourcesReqDTO;
+import com.kuaidao.aggregation.dto.pubcusres.PublicCustomerResourcesRespDTO;
+import com.kuaidao.common.constant.SysErrorCodeEnum;
+import com.kuaidao.common.entity.JSONResult;
+import com.kuaidao.common.entity.PageBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+
+/**
+ *
+ * 功能描述: 
+ *      数据字典
+ * @auther  yangbiao
+ * @date: 2019/1/8 17:35
+ */
+@FeignClient(name = "aggregation-service-ooo1",path="/aggregation/abnormaluser",fallback = ReleaseRecordFeignClient.HystrixClientFallback.class)
+public interface ReleaseRecordFeignClient {
+
+    @RequestMapping("/insert")
+    public JSONResult<Boolean> saveReleaseRecord(@Valid @RequestBody ReleaseRecordInsertOrUpdateDTO dto);
+
+    public JSONResult<PageBean<ReleaseRecordRespDTO>> queryPageList(@RequestBody ReleaseRecordReqDTO dto);
+
+    @Component
+    static class HystrixClientFallback implements ReleaseRecordFeignClient {
+
+        private static Logger logger = LoggerFactory.getLogger(HystrixClientFallback.class);
+
+        @SuppressWarnings("rawtypes")
+        private JSONResult fallBackError(String name) {
+            logger.error(name + "接口调用失败：无法获取目标服务");
+            return new JSONResult().fail(SysErrorCodeEnum.ERR_REST_FAIL.getCode(),
+                    SysErrorCodeEnum.ERR_REST_FAIL.getMessage());
+        }
+
+        @Override
+        public JSONResult<Boolean> saveReleaseRecord(ReleaseRecordInsertOrUpdateDTO dto) {
+            return fallBackError("释放记录-新增");
+        }
+
+        @Override
+        public JSONResult<PageBean<ReleaseRecordRespDTO>> queryPageList(ReleaseRecordReqDTO dto) {
+            return fallBackError("释放记录-分页查询");
+        }
+    }
+
+}
