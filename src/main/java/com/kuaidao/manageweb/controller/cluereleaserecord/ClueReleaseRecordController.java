0615221c2bce2ecd68c6e92c70b27267107c4bdd
YangBiao <yangbiao@kd.cn>
2019-02-18 17:35:25 +0800

    控制台日志输出

--- src/main/java/com/kuaidao/manageweb/controller/cluereleaserecord/ClueReleaseRecordController.java
+++ src/main/java/com/kuaidao/manageweb/controller/cluereleaserecord/ClueReleaseRecordController.java
@@ -3,13 +3,23 @@ package com.kuaidao.manageweb.controller.cluereleaserecord;
 import com.kuaidao.aggregation.dto.cluereleaserecord.ReleaseRecordInsertOrUpdateDTO;
 import com.kuaidao.aggregation.dto.cluereleaserecord.ReleaseRecordReqDTO;
 import com.kuaidao.aggregation.dto.cluereleaserecord.ReleaseRecordRespDTO;
+import com.kuaidao.common.constant.RoleCodeEnum;
+import com.kuaidao.common.constant.SystemCodeConstant;
 import com.kuaidao.common.entity.JSONResult;
 import com.kuaidao.common.entity.PageBean;
 import com.kuaidao.common.util.CommonUtil;
+import com.kuaidao.manageweb.feign.organization.OrganizationFeignClient;
 import com.kuaidao.manageweb.feign.releaserecord.ReleaseRecordFeignClient;
+import com.kuaidao.manageweb.feign.role.RoleManagerFeignClient;
 import com.kuaidao.manageweb.feign.user.UserInfoFeignClient;
+import com.kuaidao.manageweb.util.CommUtil;
+import com.kuaidao.sys.dto.organization.OrganizationDTO;
+import com.kuaidao.sys.dto.organization.OrganizationQueryDTO;
+import com.kuaidao.sys.dto.role.RoleInfoDTO;
+import com.kuaidao.sys.dto.role.RoleQueryDTO;
 import com.kuaidao.sys.dto.user.UserInfoDTO;
 import com.kuaidao.sys.dto.user.UserInfoPageParam;
+import com.netflix.discovery.converters.Auto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,6 +50,12 @@ public class ClueReleaseRecordController {
     @Autowired
     UserInfoFeignClient userInfoFeignClient;
 
+    @Autowired
+    RoleManagerFeignClient roleManagerFeignClient;
+
+    @Autowired
+    OrganizationFeignClient organizationFeignClient;
+
     /**
      * 新增
      */
@@ -58,9 +74,86 @@ public class ClueReleaseRecordController {
     @RequestMapping("/queryPageList")
     public JSONResult<PageBean<ReleaseRecordRespDTO>> queryPageList(@RequestBody ReleaseRecordReqDTO dto){
 
-
+        UserInfoDTO user =  CommUtil.getCurLoginUser();
+        List<RoleInfoDTO> roleList = user.getRoleList();
+        List dxList = new ArrayList();
+        /**
+         * 数据权限说明：
+         *    管理员权限：
+         *      能够看见全部数据
+         *    电销顾问：
+         *      能够看见自己以及所在电销组下电销创业顾问创建的数据
+         *    电销副总：
+         *      能够看见事业部下所有人的释放记录
+         *
+         *    角色说明：
+         *      事业部下：
+         *          电销副总
+         *              管理：多个电销组
+         *              电销组:
+         *                  电销总监
+         *                  管理多个电销创业顾问
+         *
+         */
+        if(roleList!=null&&roleList.get(0)!=null){
+            if(RoleCodeEnum.GLY.name().equals(roleList.get(0).getRoleCode())){
+            }else if(RoleCodeEnum.DXFZ.name().equals(roleList.get(0).getRoleCode())){ // 电销副总，查看整个事业部。
+                //
+                dxList =allUser();
+                dto.setUserIdsList(dxList);
+            }else {  // 创业顾问查看自己
+                dxList.add(user.getId());
+                dto.setUserIdsList(dxList);
+            }
+        }
         JSONResult<PageBean<ReleaseRecordRespDTO>> result = releaseRecordFeignClient.queryPageList(dto);
         return result;
+    }
+
+
+    /**
+     * 电销副总：
+     *      管理多个电销组
+     * 一个电销组中：
+     *      一个电销总监，多个电销创业顾问
+     * 电销副总：能够看见整个事业部的释放记录
+     *      就是能够看见：当前组织 以及 当前组织的全部下属组织机构
+     *
+     *   当前释放记录中：没有组织机构。
+     *      故而，查询，当前组织以及下属组织的全部人员
+     *
+     *
+     *  下面方法：
+     *      查询的是：当前组织下 包括 全部下属组织的 所有用户
+     *
+     * @return
+     */
+    private List allUser(){
+
+        List<UserInfoDTO> resList = new ArrayList();
+        UserInfoDTO user =  CommUtil.getCurLoginUser();
+        OrganizationQueryDTO organizationQueryDTO = new OrganizationQueryDTO();
+        organizationQueryDTO.setParentId(user.getOrgId());
+        organizationQueryDTO.setSystemCode(SystemCodeConstant.HUI_JU);
+        JSONResult<List<OrganizationDTO>> orgResult = organizationFeignClient.listDescenDantByParentId(organizationQueryDTO);
+
+        if(JSONResult.SUCCESS.equals(orgResult.getCode())){
+            List<OrganizationDTO> orgList = orgResult.getData();
+            List<Long> idList = new ArrayList<Long>();
+            for(OrganizationDTO orgDTO:orgList){
+                idList.add(orgDTO.getId());
+            }
+            idList.add(user.getOrgId());
+            UserInfoPageParam userparam = new UserInfoPageParam();
+            userparam.setPageSize(10000);
+            userparam.setPageNum(1);
+            userparam.setOrgIdList(idList);
+            JSONResult<PageBean<UserInfoDTO>> userResult = userInfoFeignClient.list(userparam);
+            if(JSONResult.SUCCESS.equals(userResult.getCode())){
+                resList = userResult.getData().getData();
+            }
+        }
+        return resList;
     }
 
 }
--- src/main/java/com/kuaidao/manageweb/controller/invalidcustomer/InvalidCustomerResources.java
+++ src/main/java/com/kuaidao/manageweb/controller/invalidcustomer/InvalidCustomerResources.java
@@ -1,13 +1,38 @@
 package com.kuaidao.manageweb.controller.invalidcustomer;
 
+import com.kuaidao.aggregation.dto.pubcusres.ClueQueryParamDTO;
+import com.kuaidao.aggregation.dto.pubcusres.PublicCustomerResourcesReqDTO;
+import com.kuaidao.aggregation.dto.pubcusres.PublicCustomerResourcesRespDTO;
+import com.kuaidao.common.constant.OrgTypeConstant;
+import com.kuaidao.common.constant.RoleCodeEnum;
 import com.kuaidao.common.entity.JSONResult;
 import com.kuaidao.common.entity.PageBean;
+import com.kuaidao.manageweb.feign.InvalidCustomer.InvalidCustomerFeignClient;
+import com.kuaidao.manageweb.feign.dictionary.DictionaryItemFeignClient;
+import com.kuaidao.manageweb.feign.organization.OrganizationFeignClient;
+import com.kuaidao.manageweb.feign.publiccustomer.PublicCustomerFeignClient;
+import com.kuaidao.manageweb.feign.role.RoleManagerFeignClient;
+import com.kuaidao.manageweb.feign.user.UserInfoFeignClient;
+import com.kuaidao.manageweb.util.CommUtil;
+import com.kuaidao.sys.dto.organization.OrganizationQueryDTO;
+import com.kuaidao.sys.dto.organization.OrganizationRespDTO;
+import com.kuaidao.sys.dto.role.RoleInfoDTO;
+import com.kuaidao.sys.dto.role.RoleQueryDTO;
+import com.kuaidao.sys.dto.user.UserInfoDTO;
+import com.kuaidao.sys.dto.user.UserInfoPageParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author  yangbiao
@@ -22,54 +47,115 @@ public class InvalidCustomerResources {
 
     private static Logger logger = LoggerFactory.getLogger(InvalidCustomerResources.class);
 
+    @Autowired
+    private DictionaryItemFeignClient dictionaryItemFeignClient;
 
-    /**
-     * 释放记录
-     */
-    @PostMapping("/releaseRecord")
-    @ResponseBody
-    public JSONResult<PageBean> releaseRecord(){
-        return null;
-    }
+    @Autowired
+    private OrganizationFeignClient organizationFeignClient;
 
-    /**
-     * 资源还原
-     */
+    @Autowired
+    UserInfoFeignClient userInfoFeignClient;
+
+    @Autowired
+    RoleManagerFeignClient roleManagerFeignClient;
+
+    @Autowired
+    InvalidCustomerFeignClient invalidCustomerFeignClient;
+
     @PostMapping("/resourceReduction")
     @ResponseBody
-    public JSONResult<PageBean> resourceReduction(){
-        return null;
+    public JSONResult<Boolean> resourceReduction(@RequestBody PublicCustomerResourcesReqDTO dto){
+        return invalidCustomerFeignClient.resourceReduction(dto);
     }
 
     /**
      * 分页查询
      */
     @RequestMapping("/listPage")
-    public String listPage(){
+    public String listPage(HttpServletRequest request){
         logger.info("------------ 公共客户资源列表 ---------------");
-        return "dictionary/dicListPage";
+        OrganizationQueryDTO orgDto = new OrganizationQueryDTO();
+        orgDto.setOrgType(OrgTypeConstant.DXZ);
+        //电销组
+        JSONResult<List<OrganizationRespDTO>> dzList = organizationFeignClient.queryOrgByParam(orgDto);
+        request.setAttribute("dzList", dzList.getData());
+        request.setAttribute("dxgwList", dxcygws());
+        request.setAttribute("dxzjList", dxzjs());
+
+        return "invalidcustomer/invalidCustomer";
     }
+
     @PostMapping("/queryPage")
     @ResponseBody
-    public JSONResult<PageBean> queryListPage(){
-        return null;
+    public JSONResult<PageBean<PublicCustomerResourcesRespDTO>> queryListPage(@RequestBody ClueQueryParamDTO dto){
+        Date date1 = dto.getCreateTime1();
+        Date date2 = dto.getCreateTime2();
+        if(date1!=null && date2!=null ){
+            if(date1.getTime()>date2.getTime()){
+                return new JSONResult().fail("-1","创建时间，开始时间大于结束时间!");
+            }
+        }
+
+        Date date3 = dto.getReleaseTime1();
+        Date date4 = dto.getReleaseTime2();
+        if(date3!=null && date4!=null ){
+            if(date3.getTime()>date4.getTime()){
+                return new JSONResult().fail("-1","释放时间，开始时间大于结束时间!");
+            }
+        }
+        return invalidCustomerFeignClient.queryListPage(dto);
     }
 
-    /**
-     * 重复手机号
-     */
-    @PostMapping("/repeatPhones")
-    @ResponseBody
-    public JSONResult<PageBean> repeatPhones(){
-        return null;
+
+    private List dxcygws(){
+        RoleQueryDTO query = new RoleQueryDTO();
+        query.setRoleCode(RoleCodeEnum.DXCYGW.name());
+        UserInfoDTO user =  CommUtil.getCurLoginUser();
+        JSONResult<List<RoleInfoDTO>> roleJson = roleManagerFeignClient.qeuryRoleByName(query);
+        List<UserInfoDTO> resList = new ArrayList();
+        if (JSONResult.SUCCESS.equals(roleJson.getCode())) {
+            List<RoleInfoDTO> roleList = roleJson.getData();
+            if (null != roleList && roleList.size() > 0) {
+                RoleInfoDTO roleDto = roleList.get(0);
+                UserInfoPageParam param = new UserInfoPageParam();
+                param.setRoleId(roleDto.getId());
+                param.setOrgId(user.getOrgId());
+                param.setPageSize(10000);
+                param.setPageNum(1);
+                JSONResult<PageBean<UserInfoDTO>> userListJson = userInfoFeignClient.list(param);
+                if (JSONResult.SUCCESS.equals(userListJson.getCode())) {
+                    PageBean<UserInfoDTO> pageList = userListJson.getData();
+                    resList = pageList.getData();
+                }
+            }
+        }
+        return resList;
     }
 
-    /**
-     * 跟进记录
-     */
-    @PostMapping("/followUpRecord")
-    @ResponseBody
-    public JSONResult<PageBean> followUpRecord(){
-        return null;
+
+    private List dxzjs(){
+        // 这个查询是不对的啊
+        RoleQueryDTO query = new RoleQueryDTO();
+        query.setRoleCode(RoleCodeEnum.DXZJ.name());
+        UserInfoDTO user =  CommUtil.getCurLoginUser();
+        JSONResult<List<RoleInfoDTO>> roleJson = roleManagerFeignClient.qeuryRoleByName(query);
+        List<UserInfoDTO> resList = new ArrayList();
+        if (JSONResult.SUCCESS.equals(roleJson.getCode())) {
+            List<RoleInfoDTO> roleList = roleJson.getData();
+            if (null != roleList && roleList.size() > 0) {
+                RoleInfoDTO roleDto = roleList.get(0);
+                UserInfoPageParam param = new UserInfoPageParam();
+                param.setRoleId(roleDto.getId());
+                param.setOrgId(user.getOrgId());
+                param.setPageSize(10000);
+                param.setPageNum(1);
+                JSONResult<PageBean<UserInfoDTO>> userListJson = userInfoFeignClient.list(param);
+                if (JSONResult.SUCCESS.equals(userListJson.getCode())) {
+                    PageBean<UserInfoDTO> pageList = userListJson.getData();
+                    resList = pageList.getData();
+                }
+            }
+        }
+        return resList;
     }
 }
