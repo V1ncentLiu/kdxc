e65e32bd4fbd8b5361b16e2917d886611a1d16a7
YangBiao <yangbiao@kd.cn>
2019-03-07 15:25:42 +0800

    tijiao

--- src/main/java/com/kuaidao/manageweb/controller/buscustomer/BusinessMyCustomerController.java
+++ src/main/java/com/kuaidao/manageweb/controller/buscustomer/BusinessMyCustomerController.java
@@ -122,7 +122,7 @@ public class BusinessMyCustomerController {
         Date date2 =param.getReserveTime2();
         if(date1!=null && date2!=null ){
             if(date1.getTime()>date2.getTime()){
-                return new JSONResult().fail("-1","邀约来访时间，开始时间大于结束时间!");
+                return new JSONResult().fail("-1","邀约到访时间，开始时间大于结束时间!");
             }
         }
 
@@ -136,8 +136,8 @@ public class BusinessMyCustomerController {
 
         Date date5 = param.getCreateTime1();
         Date date6 = param.getCreateTime2();
-        if(date3!=null && date4!=null ){
-            if(date3.getTime()>date4.getTime()){
+        if(date5!=null && date6!=null ){
+            if(date5.getTime()>date6.getTime()){
                 return new JSONResult().fail("-1","提交邀约时间，开始时间大于结束时间!");
             }
         }
