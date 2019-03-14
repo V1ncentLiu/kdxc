1fb31bb5362f0ec6a405a6cfbf92ee7037165057
gaopengcheng <gaopengcheng@kd.cn>
2019-03-14 21:42:34 +0800

    待分发资源已分发资源

---
+++ src/main/java/com/kuaidao/manageweb/controller/clue/ExtendClueAgendaTaskController.java
@@ -0,0 +1,143 @@
+package com.kuaidao.manageweb.controller.clue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.kuaidao.aggregation.dto.clue.ClueAgendaTaskDTO;
+import com.kuaidao.aggregation.dto.clue.ClueAgendaTaskQueryDTO;
+import com.kuaidao.aggregation.dto.project.ProjectInfoDTO;
+import com.kuaidao.aggregation.dto.project.ProjectInfoPageParam;
+import com.kuaidao.common.constant.RoleCodeEnum;
+import com.kuaidao.common.entity.JSONResult;
+import com.kuaidao.common.entity.PageBean;
+import com.kuaidao.manageweb.feign.clue.ExtendClueFeignClient;
+import com.kuaidao.manageweb.feign.project.ProjectInfoFeignClient;
+import com.kuaidao.manageweb.feign.user.UserInfoFeignClient;
+import com.kuaidao.sys.dto.user.UserInfoDTO;
+import com.kuaidao.sys.dto.user.UserOrgRoleReq;
+
+@Controller
+@RequestMapping("/exetend/agendaTaskManager")
+public class ExtendClueAgendaTaskController {
+
+	@Autowired
+	private ExtendClueFeignClient extendClueFeignClient;
+
+	@Autowired
+	private ProjectInfoFeignClient projectInfoFeignClient;
+
+	@Autowired
+	private UserInfoFeignClient userInfoFeignClient;
+
+	/**
+	 * 初始化待审核列表
+	 * 
+	 * @param request
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("/waitDistributResource")
+	public String initWaitDistributResource(HttpServletRequest request, Model model) {
+		// 项目
+		ProjectInfoPageParam param = new ProjectInfoPageParam();
+		JSONResult<List<ProjectInfoDTO>> proJson = projectInfoFeignClient.listNoPage(param);
+		if (proJson.getCode().equals(JSONResult.SUCCESS)) {
+			request.setAttribute("proSelect", proJson.getData());
+		}
+
+		List<UserInfoDTO> userList = this.queryUserByRole();
+
+		request.setAttribute("userList", userList);
+
+		return "clue/waitDistributResource";
+	}
+
+	@RequestMapping("/queryPageAgendaTask")
+	@ResponseBody
+	public JSONResult<PageBean<ClueAgendaTaskDTO>> queryPageAgendaTask(HttpServletRequest request,
+			@RequestBody ClueAgendaTaskQueryDTO queryDto) {
+
+		return extendClueFeignClient.queryPageAgendaTask(queryDto);
+
+	}
+
+	/**
+	 * 查询所有资源专员
+	 * 
+	 * @param request
+	 * @return
+	 */
+
+	private List<UserInfoDTO> queryUserByRole() {
+
+		List<UserInfoDTO> userList = new ArrayList<UserInfoDTO>();
+
+		UserOrgRoleReq userRole = new UserOrgRoleReq();
+		userRole.setRoleCode(RoleCodeEnum.TGZXZJ.name());
+		JSONResult<List<UserInfoDTO>> userZxzjList = userInfoFeignClient.listByOrgAndRole(userRole);
+
+		if (JSONResult.SUCCESS.equals(userZxzjList.getCode()) && null != userZxzjList.getData()
+				&& userZxzjList.getData().size() > 0) {
+			userList.addAll(userZxzjList.getData());
+		}
+
+		userRole.setRoleCode(RoleCodeEnum.YHZG.name());
+		JSONResult<List<UserInfoDTO>> userYhZgList = userInfoFeignClient.listByOrgAndRole(userRole);
+
+		if (JSONResult.SUCCESS.equals(userYhZgList.getCode()) && null != userYhZgList.getData()
+				&& userYhZgList.getData().size() > 0) {
+			userList.addAll(userYhZgList.getData());
+		}
+		userRole.setRoleCode(RoleCodeEnum.TGYHWY.name());
+		JSONResult<List<UserInfoDTO>> userYhWyList = userInfoFeignClient.listByOrgAndRole(userRole);
+
+		if (JSONResult.SUCCESS.equals(userYhWyList.getCode()) && null != userYhWyList.getData()
+				&& userYhWyList.getData().size() > 0) {
+			userList.addAll(userYhWyList.getData());
+		}
+		userRole.setRoleCode(RoleCodeEnum.TGKF.name());
+		JSONResult<List<UserInfoDTO>> userKfList = userInfoFeignClient.listByOrgAndRole(userRole);
+
+		if (JSONResult.SUCCESS.equals(userKfList.getCode()) && null != userKfList.getData()
+				&& userKfList.getData().size() > 0) {
+			userList.addAll(userKfList.getData());
+		}
+
+		userRole.setRoleCode(RoleCodeEnum.KFZG.name());
+		JSONResult<List<UserInfoDTO>> userKfZgList = userInfoFeignClient.listByOrgAndRole(userRole);
+
+		if (JSONResult.SUCCESS.equals(userKfZgList.getCode()) && null != userKfZgList.getData()
+				&& userKfZgList.getData().size() > 0) {
+			userList.addAll(userKfZgList.getData());
+		}
+
+		userRole.setRoleCode(RoleCodeEnum.TGNQWY.name());
+		JSONResult<List<UserInfoDTO>> userKNqWyList = userInfoFeignClient.listByOrgAndRole(userRole);
+
+		if (JSONResult.SUCCESS.equals(userKNqWyList.getCode()) && null != userKNqWyList.getData()
+				&& userKNqWyList.getData().size() > 0) {
+			userList.addAll(userKNqWyList.getData());
+		}
+
+		userRole.setRoleCode(RoleCodeEnum.NQZG.name());
+		JSONResult<List<UserInfoDTO>> userNqZgList = userInfoFeignClient.listByOrgAndRole(userRole);
+
+		if (JSONResult.SUCCESS.equals(userNqZgList.getCode()) && null != userNqZgList.getData()
+				&& userNqZgList.getData().size() > 0) {
+			userList.addAll(userNqZgList.getData());
+		}
+
+		return userList;
+
+	}
+
+}
