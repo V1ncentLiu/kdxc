4c30a1c3806353d4dade30c2b47956124cd74f84
YangBiao <yangbiao@kd.cn>
2019-03-19 14:14:43 +0800

    tijiao

--- src/main/java/com/kuaidao/manageweb/controller/phonetraffic/PhoneTrafficController.java
+++ src/main/java/com/kuaidao/manageweb/controller/phonetraffic/PhoneTrafficController.java
@@ -2,20 +2,36 @@ package com.kuaidao.manageweb.controller.phonetraffic;
 
 import com.kuaidao.aggregation.dto.busmycustomer.BusMyCustomerRespDTO;
 import com.kuaidao.aggregation.dto.busmycustomer.MyCustomerParamDTO;
+import com.kuaidao.aggregation.dto.call.CallRecordReqDTO;
+import com.kuaidao.aggregation.dto.call.CallRecordRespDTO;
+import com.kuaidao.aggregation.dto.circulation.CirculationReqDTO;
+import com.kuaidao.aggregation.dto.circulation.CirculationRespDTO;
 import com.kuaidao.aggregation.dto.clue.AllocationClueReq;
+import com.kuaidao.aggregation.dto.clue.ClueDTO;
+import com.kuaidao.aggregation.dto.clue.ClueFileDTO;
+import com.kuaidao.aggregation.dto.clue.ClueQueryDTO;
 import com.kuaidao.aggregation.dto.phonetraffic.PhoneTrafficParamDTO;
 import com.kuaidao.aggregation.dto.phonetraffic.PhoneTrafficRespDTO;
+import com.kuaidao.aggregation.dto.project.ProjectInfoDTO;
+import com.kuaidao.aggregation.dto.project.ProjectInfoPageParam;
+import com.kuaidao.aggregation.dto.tracking.TrackingReqDTO;
+import com.kuaidao.aggregation.dto.tracking.TrackingRespDTO;
 import com.kuaidao.common.entity.JSONResult;
 import com.kuaidao.common.entity.PageBean;
 import com.kuaidao.common.util.CommonUtil;
 import com.kuaidao.manageweb.config.LogRecord;
 import com.kuaidao.manageweb.constant.MenuEnum;
 import com.kuaidao.manageweb.feign.assignrule.InfoAssignFeignClient;
+import com.kuaidao.manageweb.feign.call.CallRecordFeign;
+import com.kuaidao.manageweb.feign.circulation.CirculationFeignClient;
 import com.kuaidao.manageweb.feign.clue.ClueBasicFeignClient;
+import com.kuaidao.manageweb.feign.clue.MyCustomerFeignClient;
 import com.kuaidao.manageweb.feign.customfield.CustomFieldFeignClient;
 import com.kuaidao.manageweb.feign.dictionary.DictionaryItemFeignClient;
 import com.kuaidao.manageweb.feign.organization.OrganizationFeignClient;
 import com.kuaidao.manageweb.feign.phonetraffic.PhoneTrafficFeignClient;
+import com.kuaidao.manageweb.feign.project.ProjectInfoFeignClient;
+import com.kuaidao.manageweb.feign.tracking.TrackingFeignClient;
 import com.kuaidao.manageweb.feign.user.UserInfoFeignClient;
 import com.kuaidao.manageweb.util.CommUtil;
 import com.kuaidao.sys.dto.customfield.CustomFieldQueryDTO;
@@ -27,15 +43,14 @@ import com.kuaidao.sys.dto.user.UserInfoDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -62,6 +77,24 @@ public class PhoneTrafficController {
     private CustomFieldFeignClient customFieldFeignClient;
     @Autowired
     PhoneTrafficFeignClient phoneTrafficFeignClient;
+
+    @Autowired
+    private ProjectInfoFeignClient projectInfoFeignClient;
+
+    @Autowired
+    private CallRecordFeign callRecordFeign;
+
+    @Autowired
+    private TrackingFeignClient trackingFeignClient;
+
+    @Autowired
+    private CirculationFeignClient circulationFeignClient;
+
+    @Autowired
+    private MyCustomerFeignClient myCustomerFeignClient;
+
+    @Value("${oss.url.directUpload}")
+    private String ossUrl;
 
     @RequestMapping("/listPage")
     public String listPage(HttpServletRequest request){
@@ -142,7 +175,84 @@ public class PhoneTrafficController {
     /**
      * 跳转 编辑资源页面
      */
-    public String toEditPage(){
+    @RequestMapping("/toEditPage")
+    public String toEditPage(HttpServletRequest request, @RequestParam String clueId){
+        CallRecordReqDTO call = new CallRecordReqDTO();
+        call.setClueId(clueId);
+        JSONResult<List<CallRecordRespDTO>> callRecord = callRecordFeign.listTmCallReacordByParamsNoPage(call);
+        // 资源通话记录
+        if (callRecord != null && JSONResult.SUCCESS.equals(callRecord.getCode()) && callRecord.getData() != null) {
+
+            request.setAttribute("callRecord", callRecord.getData());
+        }
+        ClueQueryDTO queryDTO = new ClueQueryDTO();
+
+        queryDTO.setClueId(new Long(clueId));
+
+        request.setAttribute("clueId", clueId);
+
+        request.setAttribute("ossUrl", ossUrl);
+
+        JSONResult<ClueDTO> clueInfo = myCustomerFeignClient.findClueInfo(queryDTO);
+
+        // 维护的资源数据
+        if (clueInfo != null && JSONResult.SUCCESS.equals(clueInfo.getCode()) && clueInfo.getData() != null) {
+
+            if (null != clueInfo.getData().getClueCustomer()) {
+                request.setAttribute("customer", clueInfo.getData().getClueCustomer());
+            } else {
+                request.setAttribute("customer", new ArrayList());
+            }
+            if (null != clueInfo.getData().getClueBasic()) {
+                request.setAttribute("base", clueInfo.getData().getClueBasic());
+            } else {
+                request.setAttribute("customer", new ArrayList());
+            }
+            if (null != clueInfo.getData().getClueIntention()) {
+                request.setAttribute("intention", clueInfo.getData().getClueIntention());
+            } else {
+                request.setAttribute("customer", new ArrayList());
+            }
+        }
+        // 获取资源跟进记录数据
+        TrackingReqDTO dto = new TrackingReqDTO();
+        dto.setClueId(new Long(clueId));
+        JSONResult<List<TrackingRespDTO>> trackingList = trackingFeignClient.queryList(dto);
+        if (trackingList != null && trackingList.SUCCESS.equals(trackingList.getCode())
+                && trackingList.getData() != null) {
+            request.setAttribute("trackingList", trackingList.getData());
+        } else {
+            request.setAttribute("trackingList", new ArrayList());
+        }
+
+        // 获取资源流转数据
+        CirculationReqDTO circDto = new CirculationReqDTO();
+        circDto.setClueId(new Long(clueId));
+        JSONResult<List<CirculationRespDTO>> circulationList = circulationFeignClient.queryList(circDto);
+        if (circulationList != null && circulationList.SUCCESS.equals(circulationList.getCode())
+                && circulationList.getData() != null) {
+            request.setAttribute("circulationList", circulationList.getData());
+        } else {
+            request.setAttribute("circulationList", new ArrayList());
+        }
+        // 项目
+        ProjectInfoPageParam param = new ProjectInfoPageParam();
+        JSONResult<List<ProjectInfoDTO>> proJson = projectInfoFeignClient.listNoPage(param);
+        if (proJson.getCode().equals(JSONResult.SUCCESS)) {
+            request.setAttribute("proSelect", proJson.getData());
+        } else {
+            request.setAttribute("proSelect", new ArrayList());
+        }
+
+        // 获取已上传的文件数据
+        ClueQueryDTO fileDto = new ClueQueryDTO();
+        fileDto.setClueId(new Long(clueId));
+        JSONResult<List<ClueFileDTO>> clueFileList = myCustomerFeignClient.findClueFile(fileDto);
+        if (clueFileList != null && clueFileList.SUCCESS.equals(clueFileList.getCode())
+                && clueFileList.getData() != null) {
+            request.setAttribute("clueFileList", clueFileList.getData());
+        }
+
         return "/phonetraffic/editCustomerMaintenance";
     }
 }
