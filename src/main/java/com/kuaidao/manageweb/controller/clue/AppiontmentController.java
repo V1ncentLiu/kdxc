6c03d2b6bb2c83135e7552a3845167be73a800fc
chenchengxue <chenchengxue@kuaidaoapp.com>
2019-03-19 11:18:22 +0800

    电销控制台

--- src/main/java/com/kuaidao/manageweb/controller/clue/AppiontmentController.java
+++ src/main/java/com/kuaidao/manageweb/controller/clue/AppiontmentController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import com.kuaidao.aggregation.dto.TeleConsoleReqDTO;
 import com.kuaidao.aggregation.dto.clue.AppiontmentCancelDTO;
 import com.kuaidao.aggregation.dto.clue.ClueAppiontmentDTO;
 import com.kuaidao.aggregation.dto.clue.ClueAppiontmentPageParam;
