2c98c3acf5453bc95b7b125afcb0cf6e88b3bd8f
zhangxingyu <408744797@qq.com>
2019-04-10 21:11:49 +0800

    tijaio

---
+++ src/main/java/com/kuaidao/manageweb/feign/financing/RefundRebateFeignClient.java
@@ -0,0 +1,78 @@
+package com.kuaidao.manageweb.feign.financing;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import com.kuaidao.aggregation.dto.financing.ApplyRefundRebateReq;
+import com.kuaidao.aggregation.dto.financing.RefundRebateListDTO;
+import com.kuaidao.aggregation.dto.financing.RefundRebatePageParam;
+import com.kuaidao.common.constant.SysErrorCodeEnum;
+import com.kuaidao.common.entity.JSONResult;
+import com.kuaidao.common.entity.PageBean;
+
+/**
+ * 餐饮公司退返款
+ * 
+ * @author: zxy
+ * @date: 2019年1月4日
+ * @version V1.0
+ */
+@FeignClient(name = "aggregation-service", path = "/aggregation/refundRebate",
+        fallback = RefundRebateFeignClient.HystrixClientFallback.class)
+public interface RefundRebateFeignClient {
+
+    /**
+     * 餐饮公司退返款列表
+     * 
+     * @param menuDTO
+     * @return
+     */
+    @PostMapping("/list")
+    public JSONResult<PageBean<RefundRebateListDTO>> list(@RequestBody RefundRebatePageParam param);
+
+
+    /**
+     * 餐饮公司申请退返款
+     * 
+     * @param idEntity
+     * @return
+     */
+    @PostMapping("/applyRefundRebate")
+    public JSONResult<Long> applyRefundRebate(@RequestBody ApplyRefundRebateReq req);
+
+
+
+    @Component
+    static class HystrixClientFallback implements RefundRebateFeignClient {
+
+        private static Logger logger = LoggerFactory.getLogger(RefundRebateFeignClient.class);
+
+
+        private JSONResult fallBackError(String name) {
+            logger.error(name + "接口调用失败：无法获取目标服务");
+            return new JSONResult().fail(SysErrorCodeEnum.ERR_REST_FAIL.getCode(),
+                    SysErrorCodeEnum.ERR_REST_FAIL.getMessage());
+        }
+
+
+
+        @Override
+        public JSONResult<Long> applyRefundRebate(@RequestBody ApplyRefundRebateReq req) {
+            return fallBackError("餐饮公司申请退返款");
+        }
+
+
+        @Override
+        public JSONResult<PageBean<RefundRebateListDTO>> list(
+                @RequestBody RefundRebatePageParam param) {
+            return fallBackError("餐饮公司退返款列表");
+        }
+
+
+    }
+
+
+}
