436acef394909dc733bcdf6209682d9885febd70
chenchengxue <chenchengxue@kuaidaoapp.com>
2019-04-11 14:05:51 +0800

    财务管理开发

--- src/main/java/com/kuaidao/manageweb/controller/financing/RefundController.java
+++ src/main/java/com/kuaidao/manageweb/controller/financing/RefundController.java
@@ -1,7 +1,30 @@
 package com.kuaidao.manageweb.controller.financing;
 
+import java.util.List;
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.kuaidao.aggregation.constant.AggregationConstant;
+import com.kuaidao.aggregation.dto.financing.RefundAndImgRespDTO;
+import com.kuaidao.aggregation.dto.financing.RefundImgRespDTO;
+import com.kuaidao.aggregation.dto.financing.RefundQueryDTO;
+import com.kuaidao.aggregation.dto.financing.RefundRespDTO;
+import com.kuaidao.aggregation.dto.financing.RefundUpdateDTO;
+import com.kuaidao.common.constant.RoleCodeEnum;
+import com.kuaidao.common.constant.SysErrorCodeEnum;
+import com.kuaidao.common.entity.IdEntityLong;
+import com.kuaidao.common.entity.IdListLongReq;
+import com.kuaidao.common.entity.JSONResult;
+import com.kuaidao.common.entity.PageBean;
+import com.kuaidao.common.util.CommonUtil;
+import com.kuaidao.manageweb.feign.financing.RefundFeignClient;
+import com.kuaidao.manageweb.util.CommUtil;
+import com.kuaidao.sys.dto.role.RoleInfoDTO;
+import com.kuaidao.sys.dto.user.UserInfoDTO;
 
 /**
  *  退返款
@@ -12,7 +35,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/financing/refund")
 @Controller
 public class RefundController {
-    
+
+    @Autowired
+    RefundFeignClient refundFeignClient;
+
     /**
      * 退款申请页面
      * @return
@@ -21,6 +47,7 @@ public class RefundController {
     public String refundApplyPage() {
         return "financing/refundApplyPage";
     }
+
     /**
      * 退款确认页面
      * @return
@@ -29,7 +56,7 @@ public class RefundController {
     public String refundConfirmPage() {
         return "financing/refundConfirmPage";
     }
-    
+
     /***
      * 返款申请页面
      * @return
@@ -38,6 +65,7 @@ public class RefundController {
     public String rebateApplayPage() {
         return "financing/rebateApplayPage";
     }
+
     /***
      * 返款确认页面页面
      * @return
@@ -45,6 +73,260 @@ public class RefundController {
     @RequestMapping("/rebateConfirmPage")
     public String rebateConfirmPage() {
         return "financing/rebateConfirmPage";
+    }
+
+
+    /***
+     * 退款申请列表
+     * @param queryDTO
+     * @return
+     */
+    @PostMapping("/listRefundApplay")
+    @ResponseBody
+    public JSONResult<PageBean<RefundRespDTO>> listRefundApplay(
+            @RequestBody RefundQueryDTO queryDTO) {
+        UserInfoDTO curLoginUser = CommUtil.getCurLoginUser();
+        queryDTO.setCurUser(curLoginUser.getId());
+        queryDTO.setType(AggregationConstant.REFOUND_REBATE_TYPE.REFOUND_TYPE);
+        return refundFeignClient.listRefundApply(queryDTO);
+    }
+    
+    
+    /**
+     * 标记退款结束
+     * @return
+     */
+    @PostMapping("/updateRefundInfo")
+    @ResponseBody
+    public JSONResult<Boolean> updateRefundInfo(@RequestBody RefundUpdateDTO refundUpdateDTO){
+       List<Long> idList = refundUpdateDTO.getIdList();
+       if(CollectionUtils.isEmpty(idList)) {
+           return CommonUtil.getParamIllegalJSONResult();
+       }
+       refundUpdateDTO.setStatus(AggregationConstant.REFOUND_REBATE_STATUS.STATUS_5);
+       return refundFeignClient.updateRefundInfo(refundUpdateDTO);
+    }
+    
+    
+    /***
+     * 退款确认列表
+     * @param queryDTO
+     * @return
+     */
+    @PostMapping("/listRefundConfirm")
+    @ResponseBody
+    public JSONResult<PageBean<RefundRespDTO>> listRefundConfirm(
+            @RequestBody RefundQueryDTO queryDTO) {
+        UserInfoDTO curLoginUser = CommUtil.getCurLoginUser();
+        queryDTO.setCurUser(curLoginUser.getId());
+        queryDTO.setType(AggregationConstant.REFOUND_REBATE_TYPE.REFOUND_TYPE);
+        
+        List<RoleInfoDTO> roleList = curLoginUser.getRoleList();
+        RoleInfoDTO roleInfoDTO = roleList.get(0);
+        String roleCode = roleInfoDTO.getRoleCode();
+        if (RoleCodeEnum.QDSJCW.name().equals(roleCode)){
+            queryDTO.setRoleCode(roleCode);
+        }else if(RoleCodeEnum.SJHZCW.name().equals(roleCode)) {
+            queryDTO.setRoleCode(roleCode);
+        }else {
+            return new JSONResult().fail(SysErrorCodeEnum.ERR_NOTEXISTS_DATA.getCode(), "角色没有权限");
+        }
+        return refundFeignClient.listRefundApply(queryDTO);
+    }
+    /**
+     * 更新退款确认
+     * @param result
+     * @return
+     */
+    @PostMapping("/updateRefundConfirm")
+    @ResponseBody
+    public JSONResult<Boolean> updateRefundConfirm(@RequestBody RefundUpdateDTO refundUpdateDTO) {
+        List<Long> idList = refundUpdateDTO.getIdList();
+        if(CollectionUtils.isEmpty(idList)) {
+            return CommonUtil.getParamIllegalJSONResult();
+        }
+        refundUpdateDTO.setStatus(AggregationConstant.REFOUND_REBATE_STATUS.STATUS_3);
+        return refundFeignClient.updateRefundInfo(refundUpdateDTO);
+    }
+    
+    /**
+     * 驳回退款
+     * @param result
+     * @return
+     */
+    @PostMapping("/rejectRefund")
+    @ResponseBody
+    public JSONResult<Boolean> rejectRefund(@RequestBody RefundUpdateDTO refundUpdateDTO) {
+        List<Long> idList = refundUpdateDTO.getIdList();
+        if(CollectionUtils.isEmpty(idList)) {
+            return CommonUtil.getParamIllegalJSONResult();
+        }
+        refundUpdateDTO.setStatus(AggregationConstant.REFOUND_REBATE_STATUS.STATUS_2);
+        return refundFeignClient.updateRefundInfo(refundUpdateDTO);
+    }
+    
+    /**
+     * 标记退款
+     * @param result
+     * @return
+     */
+    @PostMapping("/markRefund")
+    @ResponseBody
+    public JSONResult markRefund(@RequestBody RefundUpdateDTO refundUpdateDTO) {
+        List<Long> idList = refundUpdateDTO.getIdList();
+        if(CollectionUtils.isEmpty(idList)) {
+            return CommonUtil.getParamIllegalJSONResult();
+        }
+        refundUpdateDTO.setStatus(AggregationConstant.REFOUND_REBATE_STATUS.STATUS_4);
+        return refundFeignClient.updateRefundInfo(refundUpdateDTO);
+    }
+
+
+    /***
+     * 返款申请列表
+     * @param queryDTO
+     * @return
+     */
+    @PostMapping("/listRebateApplay")
+    @ResponseBody
+    public JSONResult<PageBean<RefundRespDTO>> listRebateApplay(
+            @RequestBody RefundQueryDTO queryDTO) {
+        UserInfoDTO curLoginUser = CommUtil.getCurLoginUser();
+        queryDTO.setCurUser(curLoginUser.getId());
+        queryDTO.setType(AggregationConstant.REFOUND_REBATE_TYPE.REBATE_TYPE);
+        return refundFeignClient.listRefundApply(queryDTO);
+    }
+    
+    /***
+     * 返款确认列表
+     * @param queryDTO
+     * @return
+     */
+    @PostMapping("/listRebateConfirm")
+    @ResponseBody
+    public JSONResult<PageBean<RefundRespDTO>> listRebateConfirm(
+            @RequestBody RefundQueryDTO queryDTO) {
+        UserInfoDTO curLoginUser = CommUtil.getCurLoginUser();
+        queryDTO.setCurUser(curLoginUser.getId());
+        queryDTO.setType(AggregationConstant.REFOUND_REBATE_TYPE.REBATE_TYPE);
+        
+        List<RoleInfoDTO> roleList = curLoginUser.getRoleList();
+        RoleInfoDTO roleInfoDTO = roleList.get(0);
+        String roleCode = roleInfoDTO.getRoleCode();
+       if (RoleCodeEnum.QDSJCW.name().equals(roleCode)){
+           queryDTO.setRoleCode(roleCode);
+       }else if(RoleCodeEnum.SJHZCW.name().equals(roleCode)) {
+           queryDTO.setRoleCode(roleCode);
+       }else {
+           return new JSONResult().fail(SysErrorCodeEnum.ERR_NOTEXISTS_DATA.getCode(), "角色没有权限");
+       }
+        return refundFeignClient.listRefundApply(queryDTO);
+    }
+    
+    
+    /**
+     * 标记返款结束
+     * @return
+     */
+    @PostMapping("/updateRebateInfo")
+    @ResponseBody
+    public JSONResult<Boolean> updateRebateInfo(@RequestBody RefundUpdateDTO refundUpdateDTO){
+       List<Long> idList = refundUpdateDTO.getIdList();
+       if(CollectionUtils.isEmpty(idList)) {
+           return CommonUtil.getParamIllegalJSONResult();
+       }
+       refundUpdateDTO.setStatus(AggregationConstant.REFOUND_REBATE_STATUS.STATUS_5);
+       return refundFeignClient.updateRefundInfo(refundUpdateDTO);
+    }
+    
+    /**
+     * 更新返款确认
+     * @param result
+     * @return
+     */
+    @PostMapping("/updateRebateConfirm")
+    @ResponseBody
+    public JSONResult<Boolean> updateRebateConfirm(@RequestBody RefundUpdateDTO refundUpdateDTO) {
+        List<Long> idList = refundUpdateDTO.getIdList();
+        if(CollectionUtils.isEmpty(idList)) {
+            return CommonUtil.getParamIllegalJSONResult();
+        }
+        refundUpdateDTO.setStatus(AggregationConstant.REFOUND_REBATE_STATUS.STATUS_3);
+        return refundFeignClient.updateRefundInfo(refundUpdateDTO);
+    }
+    
+    /**
+     * 驳回返款
+     * @param result
+     * @return
+     */
+    @PostMapping("/rejectRebate")
+    @ResponseBody
+    public JSONResult<Boolean> rejectRebate(@RequestBody RefundUpdateDTO refundUpdateDTO) {
+        List<Long> idList = refundUpdateDTO.getIdList();
+        if(CollectionUtils.isEmpty(idList)) {
+            return CommonUtil.getParamIllegalJSONResult();
+        }
+        refundUpdateDTO.setStatus(AggregationConstant.REFOUND_REBATE_STATUS.STATUS_2);
+        return refundFeignClient.updateRefundInfo(refundUpdateDTO);
+    }
+    
+    /**
+     * 标记返款
+     * @param result
+     * @return
+     */
+    @PostMapping("/markRebate")
+    @ResponseBody
+    public JSONResult markRebate(@RequestBody RefundUpdateDTO refundUpdateDTO) {
+        List<Long> idList = refundUpdateDTO.getIdList();
+        if(CollectionUtils.isEmpty(idList)) {
+            return CommonUtil.getParamIllegalJSONResult();
+        }
+        refundUpdateDTO.setStatus(AggregationConstant.REFOUND_REBATE_STATUS.STATUS_4);
+        return refundFeignClient.updateRefundInfo(refundUpdateDTO);
+    }
+    
+    /***
+     * 根据Id 查询图片列表
+     */
+    @PostMapping("/listImgById")
+    @ResponseBody
+    public JSONResult<List<RefundImgRespDTO>> listImgById(@RequestBody IdEntityLong idEntityLong){
+        if(idEntityLong.getId()==null) {
+            return CommonUtil.getParamIllegalJSONResult();
+        }
+        return refundFeignClient.listImgById(idEntityLong);
+    }
+    
+    /**
+     * 根据图片Id 删除 图片
+     * @param idListLongReq
+     * @return
+     */
+    @PostMapping("/deleteImgByIdList")
+    @ResponseBody
+    public JSONResult<Boolean> deleteImgByIdList(@RequestBody IdListLongReq idListLongReq){
+        List<Long> idList = idListLongReq.getIdList();
+        if(CollectionUtils.isEmpty(idList)) {
+            return CommonUtil.getParamIllegalJSONResult();
+        }
+        return  refundFeignClient.deleteImgByIdList(idListLongReq);
+    }
+    
+    /**
+     * 查询退返款详情 根据Id
+     * @param idEntityLong
+     * @return
+     */
+    @PostMapping("/queryRefundInfoById")
+    @ResponseBody
+    public JSONResult<RefundAndImgRespDTO> queryRefundInfoById(@RequestBody IdEntityLong idEntityLong){
+        Long id = idEntityLong.getId();
+        if(id == null) {
+            return CommonUtil.getParamIllegalJSONResult();
+        }
+        return refundFeignClient.queryRefundInfoById(idEntityLong);
     }
     
 
---
+++ src/main/java/com/kuaidao/manageweb/feign/financing/RefundFeignClient.java
@@ -0,0 +1,106 @@
+package com.kuaidao.manageweb.feign.financing;
+
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.kuaidao.aggregation.dto.financing.RefundAndImgRespDTO;
+import com.kuaidao.aggregation.dto.financing.RefundImgRespDTO;
+import com.kuaidao.aggregation.dto.financing.RefundQueryDTO;
+import com.kuaidao.aggregation.dto.financing.RefundRespDTO;
+import com.kuaidao.aggregation.dto.financing.RefundUpdateDTO;
+import com.kuaidao.common.constant.SysErrorCodeEnum;
+import com.kuaidao.common.entity.IdEntityLong;
+import com.kuaidao.common.entity.IdListLongReq;
+import com.kuaidao.common.entity.JSONResult;
+import com.kuaidao.common.entity.PageBean;
+
+/**
+ *  退返款 
+ * @author  Chen
+ * @date 2019年4月10日 下午7:35:14   
+ * @version V1.0
+ */
+@FeignClient(name = "aggregation-service-chen", path = "/aggregation/financing/refund", fallback = RefundFeignClient.HystrixClientFallback.class)
+public interface RefundFeignClient {
+    /**
+     * 退返款申请列表
+     * @param queryDTO
+     * @return
+     */
+    @PostMapping("/listRefundApply")
+    JSONResult<PageBean<RefundRespDTO>> listRefundApply(RefundQueryDTO queryDTO);
+
+    /**
+     * 更新退还款信息
+     * @param refundUpdateDTO
+     * @return
+     */
+    @PostMapping("/updateRefundInfo")
+    JSONResult<Boolean> updateRefundInfo(RefundUpdateDTO refundUpdateDTO);
+    
+    /**
+     * 根据退返款ID 查询 图片地址
+     * @param idEntityLong
+     * @return
+     */
+    @PostMapping("/listImgById")
+    JSONResult<List<RefundImgRespDTO>> listImgById(IdEntityLong idEntityLong);
+    
+    
+    /**
+     * 根据图片地址删除图片
+     * @param idListLongReq
+     * @return
+     */
+    @PostMapping("/deleteImgByIdList")
+    JSONResult<Boolean> deleteImgByIdList(IdListLongReq idListLongReq);
+    
+    /**
+     * 查询退返款详情 根据Id
+     * @param idEntityLong
+     * @return
+     */
+    @PostMapping("/queryRefundInfoById")
+    JSONResult<RefundAndImgRespDTO> queryRefundInfoById(IdEntityLong idEntityLong);
+    
+    @Component
+    static class HystrixClientFallback implements RefundFeignClient {
+
+        private static Logger logger = LoggerFactory.getLogger(RefundFeignClient.class);
+
+        private JSONResult fallBackError(String name) {
+            logger.error(name + "接口调用失败：无法获取目标服务");
+            return new JSONResult().fail(SysErrorCodeEnum.ERR_REST_FAIL.getCode(),
+                    SysErrorCodeEnum.ERR_REST_FAIL.getMessage());
+        }
+
+        @Override
+        public JSONResult<PageBean<RefundRespDTO>> listRefundApply(RefundQueryDTO queryDTO) {
+            return fallBackError("查询退返款申请列表");
+        }
+
+        @Override
+        public JSONResult<Boolean> updateRefundInfo(RefundUpdateDTO refundUpdateDTO) {
+            return fallBackError("更新退返款信息");
+        }
+
+        @Override
+        public JSONResult<List<RefundImgRespDTO>> listImgById(IdEntityLong idEntityLong) {
+            return fallBackError("根据退返款ID 查询 图片地址");
+        }
+
+        @Override
+        public JSONResult<Boolean> deleteImgByIdList(IdListLongReq idListLongReq) {
+            return fallBackError("根据图片地址删除图片");
+        }
+
+        @Override
+        public JSONResult<RefundAndImgRespDTO> queryRefundInfoById(IdEntityLong idEntityLong) {
+            return fallBackError("根据Id查询退返款详情");
+        }
+    }
+
+}
