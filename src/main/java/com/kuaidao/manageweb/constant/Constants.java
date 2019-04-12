b2cd9b4ce45de74315df94340cee6d9f5b2c82aa
jiangzhenxueAdministrator <1427692900@qq.com>
2019-04-12 20:33:52 +0800

    tijiao

--- src/main/java/com/kuaidao/manageweb/constant/Constants.java
+++ src/main/java/com/kuaidao/manageweb/constant/Constants.java
@@ -66,6 +66,8 @@ public class Constants {
     public static String USSM = "ussm";
     // 释放原因字典Code
     public static String RELEASE_REASON = "releaseReason";
+ // 释放原因字典Code
+    public static String GIVE_TYPE = "give_type";
 
 
 }
