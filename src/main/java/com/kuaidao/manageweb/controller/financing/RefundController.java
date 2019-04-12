ba44b23a8e6c0ed5ff23dc6c103ea7b7b94a93e1
chenchengxue <chenchengxue@kuaidaoapp.com>
2019-04-12 20:58:12 +0800

    财务管理开发

--- src/main/java/com/kuaidao/manageweb/controller/financing/RefundController.java
+++ src/main/java/com/kuaidao/manageweb/controller/financing/RefundController.java
@@ -1,7 +1,11 @@
 package com.kuaidao.manageweb.controller.financing;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.apache.commons.collections.CollectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,17 +19,27 @@ import com.kuaidao.aggregation.dto.financing.RefundInfoQueryDTO;
 import com.kuaidao.aggregation.dto.financing.RefundQueryDTO;
 import com.kuaidao.aggregation.dto.financing.RefundRespDTO;
 import com.kuaidao.aggregation.dto.financing.RefundUpdateDTO;
+import com.kuaidao.common.constant.OrgTypeConstant;
 import com.kuaidao.common.constant.RoleCodeEnum;
 import com.kuaidao.common.constant.SysErrorCodeEnum;
+import com.kuaidao.common.constant.SystemCodeConstant;
 import com.kuaidao.common.entity.IdEntityLong;
 import com.kuaidao.common.entity.IdListLongReq;
 import com.kuaidao.common.entity.JSONResult;
 import com.kuaidao.common.entity.PageBean;
 import com.kuaidao.common.util.CommonUtil;
+import com.kuaidao.manageweb.config.LogRecord;
+import com.kuaidao.manageweb.config.LogRecord.OperationType;
+import com.kuaidao.manageweb.constant.MenuEnum;
 import com.kuaidao.manageweb.feign.financing.RefundFeignClient;
+import com.kuaidao.manageweb.feign.organization.OrganizationFeignClient;
 import com.kuaidao.manageweb.util.CommUtil;
+import com.kuaidao.sys.dto.organization.OrganizationDTO;
+import com.kuaidao.sys.dto.organization.OrganizationQueryDTO;
+import com.kuaidao.sys.dto.organization.OrganizationRespDTO;
 import com.kuaidao.sys.dto.role.RoleInfoDTO;
 import com.kuaidao.sys.dto.user.UserInfoDTO;
+import com.kuaidao.sys.dto.user.UserOrgRoleReq;
 
 /**
  *  退返款
@@ -36,9 +50,15 @@ import com.kuaidao.sys.dto.user.UserInfoDTO;
 @RequestMapping("/financing/refund")
 @Controller
 public class RefundController {
+    
+    private static Logger logger = LoggerFactory.getLogger(RefundController.class);
 
     @Autowired
     RefundFeignClient refundFeignClient;
+    
+    @Autowired
+    OrganizationFeignClient organizationFeignClient;
+
 
     /**
      * 退款申请页面
@@ -89,10 +109,111 @@ public class RefundController {
         UserInfoDTO curLoginUser = CommUtil.getCurLoginUser();
         queryDTO.setCurUser(curLoginUser.getId());
         queryDTO.setType(AggregationConstant.REFOUND_REBATE_TYPE.REFOUND_TYPE);
+        //处理商务参数
+        handleBusinessReqParam(queryDTO);
+        //处理电销参数
+        handleTeleReqParam(queryDTO);
         return refundFeignClient.listRefundApply(queryDTO);
     }
     
     
+    /**
+     * 处理电销参数
+     * @param queryDTO
+     */
+     private void handleTeleReqParam(RefundQueryDTO queryDTO) {
+        Long teleId = queryDTO.getTeleId();
+        if(teleId!=null) {
+            List<Long> teleIdList = new ArrayList<>();
+            teleIdList.add(teleId);
+            queryDTO.setTeleIdList(teleIdList);
+            return;
+         }
+        Long teleGroupId = queryDTO.getTeleGroupId();
+        if(teleGroupId!=null) {
+            List<Long> teleGroupIdList = new ArrayList<>();
+            teleGroupIdList.add(teleGroupId);
+            queryDTO.setTeleGroupIdList(teleGroupIdList);
+            return ;
+        }
+        
+        Long teleDeptId = queryDTO.getTeleDeptId();
+        if(teleDeptId!=null) {
+            OrganizationQueryDTO busGroupReqDTO = new OrganizationQueryDTO();
+            busGroupReqDTO.setParentId(teleDeptId); 
+            busGroupReqDTO.setSystemCode(SystemCodeConstant.HUI_JU);
+            busGroupReqDTO.setOrgType(OrgTypeConstant.DXZ);
+            JSONResult<List<OrganizationRespDTO>> orgJr = organizationFeignClient.queryOrgByParam(busGroupReqDTO);
+            if (orgJr == null || !JSONResult.SUCCESS.equals(orgJr.getCode())) {
+                logger.error("refund query org list res{{}}", orgJr);
+            }else {
+                List<OrganizationRespDTO> data = orgJr.getData();
+                if(CollectionUtils.isNotEmpty(data)) {
+                    List<Long> telGroupList = data.parallelStream().map(OrganizationRespDTO::getId).collect(Collectors.toList());
+                    queryDTO.setBusGroupIdList(telGroupList);
+                }
+            }
+            
+        }
+    }
+     
+
+
+    //处理商务参数
+    private void handleBusinessReqParam(RefundQueryDTO queryDTO) {
+       Long busManagerId = queryDTO.getBusManagerId();
+       if(busManagerId!=null) {
+           List<Long> busManagerIdList = new ArrayList<Long>();
+           busManagerIdList.add(busManagerId);
+           queryDTO.setBusManagerIdList(busManagerIdList);
+           return;
+       }
+       
+       Long busGroupId = queryDTO.getBusGroupId();
+       if(busGroupId!=null) {
+           List<Long> busGroupIdList = new ArrayList<Long>();
+           busGroupIdList.add(busGroupId);
+           queryDTO.setBusGroupIdList(busGroupIdList);
+           return;
+       }
+       
+       Long busAreaId = queryDTO.getBusAreaId();
+       if(busAreaId!=null) {
+           // 商务小组
+           List<OrganizationDTO> businessGroupList = getBusinessGroupList(busAreaId, OrgTypeConstant.SWZ);
+           if(CollectionUtils.isNotEmpty(businessGroupList)) {
+               List<Long> busGroupIdList = businessGroupList.parallelStream().map(OrganizationDTO::getId).collect(Collectors.toList());
+               queryDTO.setBusGroupIdList(busGroupIdList);
+           }
+         
+           return;
+       }
+       
+    }
+    
+    /**
+     * 获取商务组
+     * 
+     * @param orgId
+     * @param orgType
+     * @return
+     */
+    private List<OrganizationDTO> getBusinessGroupList(Long orgId, Integer orgType) {
+        OrganizationQueryDTO busGroupReqDTO = new OrganizationQueryDTO();
+        busGroupReqDTO.setSystemCode(SystemCodeConstant.HUI_JU);
+        busGroupReqDTO.setParentId(orgId);
+        busGroupReqDTO.setOrgType(orgType);
+        JSONResult<List<OrganizationDTO>> orgJr =
+                organizationFeignClient.listDescenDantByParentId(busGroupReqDTO);
+        if (orgJr == null || !JSONResult.SUCCESS.equals(orgJr.getCode())) {
+            logger.error("query org list res{{}}", orgJr);
+            return null;
+        }
+        return orgJr.getData();
+
+    }
+    
+
     /**
      * 查询退款详情 根据Id
      * @param idEntityLong
@@ -116,6 +237,8 @@ public class RefundController {
      */
     @PostMapping("/updateRefundInfo")
     @ResponseBody
+    @LogRecord(operationType = OperationType.UPDATE, description = "标记退款结束",
+    menuName = MenuEnum.REFUNDAPPLYLIST)
     public JSONResult<Boolean> updateRefundInfo(@RequestBody RefundUpdateDTO refundUpdateDTO){
        List<Long> idList = refundUpdateDTO.getIdList();
        if(CollectionUtils.isEmpty(idList)) {
@@ -159,6 +282,8 @@ public class RefundController {
      */
     @PostMapping("/updateRefundConfirm")
     @ResponseBody
+    @LogRecord(operationType = OperationType.UPDATE, description = "退款确认",
+    menuName = MenuEnum.REFUNDCONFIRM)
     public JSONResult<Boolean> updateRefundConfirm(@RequestBody RefundUpdateDTO refundUpdateDTO) {
         List<Long> idList = refundUpdateDTO.getIdList();
         if(CollectionUtils.isEmpty(idList)) {
@@ -176,6 +301,8 @@ public class RefundController {
      */
     @PostMapping("/rejectRefund")
     @ResponseBody
+    @LogRecord(operationType = OperationType.UPDATE, description = "驳回退款",
+    menuName = MenuEnum.REFUNDCONFIRM)
     public JSONResult<Boolean> rejectRefund(@RequestBody RefundUpdateDTO refundUpdateDTO) {
         List<Long> idList = refundUpdateDTO.getIdList();
         if(CollectionUtils.isEmpty(idList)) {
@@ -193,6 +320,8 @@ public class RefundController {
      */
     @PostMapping("/markRefund")
     @ResponseBody
+    @LogRecord(operationType = OperationType.UPDATE, description = "标记退款",
+    menuName = MenuEnum.REFUNDCONFIRM)
     public JSONResult markRefund(@RequestBody RefundUpdateDTO refundUpdateDTO) {
         List<Long> idList = refundUpdateDTO.getIdList();
         if(CollectionUtils.isEmpty(idList)) {
@@ -252,6 +381,8 @@ public class RefundController {
      */
     @PostMapping("/updateRebateInfo")
     @ResponseBody
+    @LogRecord(operationType = OperationType.UPDATE, description = "标记返款结束",
+    menuName = MenuEnum.REBATEAPPLYLIST)
     public JSONResult<Boolean> updateRebateInfo(@RequestBody RefundUpdateDTO refundUpdateDTO){
        List<Long> idList = refundUpdateDTO.getIdList();
        if(CollectionUtils.isEmpty(idList)) {
@@ -269,6 +400,8 @@ public class RefundController {
      */
     @PostMapping("/updateRebateConfirm")
     @ResponseBody
+    @LogRecord(operationType = OperationType.UPDATE, description = "返款确认",
+    menuName = MenuEnum.REBATECONFIRM)
     public JSONResult<Boolean> updateRebateConfirm(@RequestBody RefundUpdateDTO refundUpdateDTO) {
         List<Long> idList = refundUpdateDTO.getIdList();
         if(CollectionUtils.isEmpty(idList)) {
@@ -286,6 +419,8 @@ public class RefundController {
      */
     @PostMapping("/rejectRebate")
     @ResponseBody
+    @LogRecord(operationType = OperationType.UPDATE, description = "驳回返款",
+    menuName = MenuEnum.REBATECONFIRM)
     public JSONResult<Boolean> rejectRebate(@RequestBody RefundUpdateDTO refundUpdateDTO) {
         List<Long> idList = refundUpdateDTO.getIdList();
         if(CollectionUtils.isEmpty(idList)) {
@@ -303,6 +438,8 @@ public class RefundController {
      */
     @PostMapping("/markRebate")
     @ResponseBody
+    @LogRecord(operationType = OperationType.UPDATE, description = "标记返款",
+    menuName = MenuEnum.REBATECONFIRM)
     public JSONResult markRebate(@RequestBody RefundUpdateDTO refundUpdateDTO) {
         List<Long> idList = refundUpdateDTO.getIdList();
         if(CollectionUtils.isEmpty(idList)) {
@@ -332,6 +469,8 @@ public class RefundController {
      */
     @PostMapping("/deleteImgByIdList")
     @ResponseBody
+    @LogRecord(operationType = OperationType.UPDATE, description = "删除图片",
+    menuName = MenuEnum.REBATECONFIRM)
     public JSONResult<Boolean> deleteImgByIdList(@RequestBody IdListLongReq idListLongReq){
         List<Long> idList = idListLongReq.getIdList();
         if(CollectionUtils.isEmpty(idList)) {
