ba2b16327d6ea1c96af496440c30cf121b803039
zhangxingyu <408744797@qq.com>
2019-04-23 21:46:18 +0800

    tijaio

--- src/main/java/com/kuaidao/manageweb/feign/clue/ExtendClueFeignClient.java
+++ src/main/java/com/kuaidao/manageweb/feign/clue/ExtendClueFeignClient.java
@@ -7,77 +7,112 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import com.kuaidao.aggregation.dto.clue.ClueAgendaTaskDTO;
 import com.kuaidao.aggregation.dto.clue.ClueAgendaTaskQueryDTO;
 import com.kuaidao.aggregation.dto.clue.ClueDistributionedTaskDTO;
 import com.kuaidao.aggregation.dto.clue.ClueDistributionedTaskQueryDTO;
+import com.kuaidao.aggregation.dto.clue.PushClueReq;
 import com.kuaidao.common.constant.SysErrorCodeEnum;
 import com.kuaidao.common.entity.IdListLongReq;
 import com.kuaidao.common.entity.JSONResult;
 import com.kuaidao.common.entity.PageBean;
 
-@FeignClient(name = "aggregation-service", path = "/aggregation/extend/clueManager", fallback = ExtendClueFeignClient.HystrixClientFallback.class)
+@FeignClient(name = "aggregation-service-zhang", path = "/aggregation/extend/clueManager",
+        fallback = ExtendClueFeignClient.HystrixClientFallback.class)
 public interface ExtendClueFeignClient {
 
-	/**
-	 * 待审核数据分页查询
-	 * 
-	 * @param queryDto
-	 * @return
-	 */
-	@RequestMapping(method = RequestMethod.POST, value = "/queryPageAgendaTask")
-	public JSONResult<PageBean<ClueAgendaTaskDTO>> queryPageAgendaTask(@RequestBody ClueAgendaTaskQueryDTO queryDto);
+    /**
+     * 待审核数据分页查询
+     * 
+     * @param queryDto
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.POST, value = "/queryPageAgendaTask")
+    public JSONResult<PageBean<ClueAgendaTaskDTO>> queryPageAgendaTask(
+            @RequestBody ClueAgendaTaskQueryDTO queryDto);
 
-	/**
-	 * 已审核列表查询
-	 * 
-	 * @param queryDto
-	 * @return
-	 */
-	@RequestMapping(method = RequestMethod.POST, value = "/queryPageDistributionedTask")
-	public JSONResult<PageBean<ClueDistributionedTaskDTO>> queryPageDistributionedTask(
-			@RequestBody ClueDistributionedTaskQueryDTO queryDto);
+    /**
+     * 已审核列表查询
+     * 
+     * @param queryDto
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.POST, value = "/queryPageDistributionedTask")
+    public JSONResult<PageBean<ClueDistributionedTaskDTO>> queryPageDistributionedTask(
+            @RequestBody ClueDistributionedTaskQueryDTO queryDto);
 
-	/**
-	 * 已审核列表查询
-	 * 
-	 * @param queryDto
-	 * @return
-	 */
-	@RequestMapping(method = RequestMethod.POST, value = "/autoAllocationTask")
-	public JSONResult<Integer> autoAllocationTask(@RequestBody IdListLongReq list);
+    /**
+     * 已审核列表查询
+     * 
+     * @param queryDto
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.POST, value = "/autoAllocationTask")
+    public JSONResult<Integer> autoAllocationTask(@RequestBody IdListLongReq list);
 
-	@Component
-	static class HystrixClientFallback implements ExtendClueFeignClient {
+    /**
+     * 新建线索
+     * 
+     * @param queryDto
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.POST, value = "/createClue")
+    public JSONResult<String> createClue(@RequestBody PushClueReq pushClueReq);
 
-		private static Logger logger = LoggerFactory.getLogger(ExtendClueFeignClient.class);
+    /**
+     * 编辑线索
+     * 
+     * @param queryDto
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.POST, value = "/updateClue")
+    public JSONResult<String> updateClue(@RequestBody PushClueReq pushClueReq);
 
-		private JSONResult fallBackError(String name) {
-			logger.error(name + "接口调用失败：无法获取目标服务");
-			return new JSONResult().fail(SysErrorCodeEnum.ERR_REST_FAIL.getCode(),
-					SysErrorCodeEnum.ERR_REST_FAIL.getMessage());
-		}
+    @Component
+    static class HystrixClientFallback implements ExtendClueFeignClient {
 
-		@Override
-		public JSONResult<PageBean<ClueAgendaTaskDTO>> queryPageAgendaTask(ClueAgendaTaskQueryDTO queryDto) {
-			// TODO Auto-generated method stub
-			return fallBackError("查询待审核数据");
-		}
+        private static Logger logger = LoggerFactory.getLogger(ExtendClueFeignClient.class);
 
-		@Override
-		public JSONResult<PageBean<ClueDistributionedTaskDTO>> queryPageDistributionedTask(
-				ClueDistributionedTaskQueryDTO queryDto) {
-			// TODO Auto-generated method stub
-			return fallBackError("查询已审核业务");
-		}
+        private JSONResult fallBackError(String name) {
+            logger.error(name + "接口调用失败：无法获取目标服务");
+            return new JSONResult().fail(SysErrorCodeEnum.ERR_REST_FAIL.getCode(),
+                    SysErrorCodeEnum.ERR_REST_FAIL.getMessage());
+        }
 
-		@Override
-		public JSONResult<Integer> autoAllocationTask(IdListLongReq list) {
-			// TODO Auto-generated method stub
-			return fallBackError("自动分配任务");
-		}
+        @Override
+        public JSONResult<PageBean<ClueAgendaTaskDTO>> queryPageAgendaTask(
+                ClueAgendaTaskQueryDTO queryDto) {
+            // TODO Auto-generated method stub
+            return fallBackError("查询待审核数据");
+        }
 
-	}
+        @Override
+        public JSONResult<PageBean<ClueDistributionedTaskDTO>> queryPageDistributionedTask(
+                ClueDistributionedTaskQueryDTO queryDto) {
+            // TODO Auto-generated method stub
+            return fallBackError("查询已审核业务");
+        }
+
+        @Override
+        public JSONResult<Integer> autoAllocationTask(IdListLongReq list) {
+            // TODO Auto-generated method stub
+            return fallBackError("自动分配任务");
+        }
+
+        @Override
+        public JSONResult<String> createClue(@RequestBody PushClueReq pushClueReq) {
+            // TODO Auto-generated method stub
+            return fallBackError("新建资源");
+        }
+
+        @Override
+        public JSONResult<String> updateClue(@RequestBody PushClueReq pushClueReq) {
+            // TODO Auto-generated method stub
+            return fallBackError("编辑资源");
+        }
+
+    }
+
+
 
 }
