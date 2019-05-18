f9d56119d5e7ddf10563f0872c2d28e59e9418e4
chenchengxue <chenchengxue@kuaidaoapp.com>
2019-05-18 18:01:25 +0800

    通话时长开发

---
+++ src/main/java/com/kuaidao/manageweb/feign/statistics/TeleTalkTimeFeignClient.java
@@ -0,0 +1,91 @@
+package com.kuaidao.manageweb.feign.statistics;
+
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.kuaidao.common.constant.SysErrorCodeEnum;
+import com.kuaidao.common.entity.JSONResult;
+import com.kuaidao.common.entity.PageBean;
+import com.kuaidao.stastics.dto.callrecord.TeleSaleTalkTimeQueryDTO;
+import com.kuaidao.stastics.dto.callrecord.TeleTalkTimeRespDTO;
+
+@FeignClient(name = "statstics-service-chen", path = "/statstics//callrecord/teleSaleTalkTime", fallback = TeleTalkTimeFeignClient.HystrixClientFallback.class)
+public interface TeleTalkTimeFeignClient {
+
+    /***
+     * 昨日 七天 电销组通话时长
+    * @param teleSaleTalkTimeQueryDTO
+    * @return
+     */
+    @PostMapping("/listTeleGroupTalkTime")
+    JSONResult<PageBean<TeleTalkTimeRespDTO>> listTeleGroupTalkTime(@RequestBody TeleSaleTalkTimeQueryDTO teleSaleTalkTimeQueryDTO);
+    
+    /**
+     *  昨日 七天 电销组通话时长
+    * @param teleSaleTalkTimeQueryDTO
+    * @return
+     */
+    @PostMapping("/listTeleGroupTalkTimeNoPage")
+    public JSONResult<List<TeleTalkTimeRespDTO>> listTeleGroupTalkTimeNoPage(@RequestBody TeleSaleTalkTimeQueryDTO teleSaleTalkTimeQueryDTO);
+    
+
+    /**
+     * 昨日 七天 电销顾问 通话时长表
+    * @param teleSaleTalkTimeQueryDTO
+    * @return
+     */
+    @PostMapping("/listTeleSaleTalkTimeNoPage")
+    public JSONResult<List<TeleTalkTimeRespDTO>> listTeleSaleTalkTimeNoPage(@RequestBody TeleSaleTalkTimeQueryDTO teleSaleTalkTimeQueryDTO);
+    
+    
+    /**
+     * 昨日 七天 电销顾问 通话时长表
+    * @param teleSaleTalkTimeQueryDTO
+    * @return
+     */
+    @RequestMapping("/listTeleSaleTalkTime")
+    public JSONResult<PageBean<TeleTalkTimeRespDTO>> listTeleSaleTalkTime(@RequestBody TeleSaleTalkTimeQueryDTO teleSaleTalkTimeQueryDTO);
+    
+    @Component
+    class HystrixClientFallback implements TeleTalkTimeFeignClient {
+
+        private static Logger logger = LoggerFactory.getLogger(StatisticsFeignClient.class);
+
+        private JSONResult fallBackError(String name) {
+            logger.error(name + "接口调用失败：无法获取目标服务");
+            return new JSONResult().fail(SysErrorCodeEnum.ERR_REST_FAIL.getCode(),
+                    SysErrorCodeEnum.ERR_REST_FAIL.getMessage());
+        }
+
+        @Override
+        public JSONResult<PageBean<TeleTalkTimeRespDTO>> listTeleGroupTalkTime(
+                TeleSaleTalkTimeQueryDTO teleSaleTalkTimeQueryDTO) {
+            return fallBackError("昨日-七天-电销组通话时长");
+        }
+
+        @Override
+        public JSONResult<List<TeleTalkTimeRespDTO>> listTeleGroupTalkTimeNoPage(
+                TeleSaleTalkTimeQueryDTO teleSaleTalkTimeQueryDTO) {
+            return fallBackError("昨日-七天-电销组通话时长不分页");
+        }
+
+        @Override
+        public JSONResult<List<TeleTalkTimeRespDTO>> listTeleSaleTalkTimeNoPage(
+                TeleSaleTalkTimeQueryDTO teleSaleTalkTimeQueryDTO) {
+            return fallBackError("昨日 七天 销顾问 通话时长表");
+        }
+
+        @Override
+        public JSONResult<PageBean<TeleTalkTimeRespDTO>> listTeleSaleTalkTime(
+                TeleSaleTalkTimeQueryDTO teleSaleTalkTimeQueryDTO) {
+            return fallBackError("昨日-七天电销顾问 通话时长表");
+        }
+
+    }
+
+}
