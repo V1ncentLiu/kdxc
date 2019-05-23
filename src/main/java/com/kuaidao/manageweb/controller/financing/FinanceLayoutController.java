7aae1a9974a3f4e2766eb8c2b21bf4f9de8e8b7f
fanjd <fjd89@126.com>
2019-05-23 18:56:32 +0800

    格式化代码

--- src/main/java/com/kuaidao/manageweb/controller/financing/FinanceLayoutController.java
+++ src/main/java/com/kuaidao/manageweb/controller/financing/FinanceLayoutController.java
@@ -98,35 +98,32 @@ public class FinanceLayoutController {
         return financeLayoutFeignClient.getFinanceLayoutList(financeLayoutDTO);
     }
 
-    public List<UserInfoDTO> getUserList(){
+    public List<UserInfoDTO> getUserList() {
         UserOrgRoleReq userOrgRoleReq = new UserOrgRoleReq();
         userOrgRoleReq.setRoleCode(RoleCodeEnum.CYCW.name());
-     // 商务小组
-        JSONResult<List<UserInfoDTO>> cycwList =
-        		userInfoFeignClient.listByOrgAndRole(userOrgRoleReq);
+        // 商务小组
+        JSONResult<List<UserInfoDTO>> cycwList = userInfoFeignClient.listByOrgAndRole(userOrgRoleReq);
         userOrgRoleReq.setRoleCode(RoleCodeEnum.QDSJCW.name());
         // 商务小组
-        JSONResult<List<UserInfoDTO>> qdsjcwList =
-           		userInfoFeignClient.listByOrgAndRole(userOrgRoleReq);
+        JSONResult<List<UserInfoDTO>> qdsjcwList = userInfoFeignClient.listByOrgAndRole(userOrgRoleReq);
         userOrgRoleReq.setRoleCode(RoleCodeEnum.SJHZCW.name());
-           // 商务小组
-        JSONResult<List<UserInfoDTO>> shgzcwList =
-      		userInfoFeignClient.listByOrgAndRole(userOrgRoleReq);
+        // 商务小组
+        JSONResult<List<UserInfoDTO>> shgzcwList = userInfoFeignClient.listByOrgAndRole(userOrgRoleReq);
         List<UserInfoDTO> userList = new ArrayList<>();
-        if(JSONResult.SUCCESS.equals(cycwList.getCode()) && cycwList.getData() !=null && cycwList.getData().size()>0) {
-        	for (UserInfoDTO userInfoDTO : cycwList.getData()) {
-        		userList.add(userInfoDTO);
-			}
+        if (JSONResult.SUCCESS.equals(cycwList.getCode()) && cycwList.getData() != null && cycwList.getData().size() > 0) {
+            for (UserInfoDTO userInfoDTO : cycwList.getData()) {
+                userList.add(userInfoDTO);
+            }
         }
-        if(JSONResult.SUCCESS.equals(qdsjcwList.getCode()) && qdsjcwList.getData() !=null && qdsjcwList.getData().size()>0) {
-        	for (UserInfoDTO userInfoDTO : qdsjcwList.getData()) {
-        		userList.add(userInfoDTO);
-			}
+        if (JSONResult.SUCCESS.equals(qdsjcwList.getCode()) && qdsjcwList.getData() != null && qdsjcwList.getData().size() > 0) {
+            for (UserInfoDTO userInfoDTO : qdsjcwList.getData()) {
+                userList.add(userInfoDTO);
+            }
         }
-        if(JSONResult.SUCCESS.equals(shgzcwList.getCode()) && shgzcwList.getData() !=null && shgzcwList.getData().size()>0) {
-        	for (UserInfoDTO userInfoDTO : shgzcwList.getData()) {
-        		userList.add(userInfoDTO);
-			}
+        if (JSONResult.SUCCESS.equals(shgzcwList.getCode()) && shgzcwList.getData() != null && shgzcwList.getData().size() > 0) {
+            for (UserInfoDTO userInfoDTO : shgzcwList.getData()) {
+                userList.add(userInfoDTO);
+            }
         }
         return userList;
     }
