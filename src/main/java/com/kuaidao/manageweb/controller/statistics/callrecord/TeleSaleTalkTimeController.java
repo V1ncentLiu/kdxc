64dd95051d901a81ce176e6f08ac79f02fc9a81d
chenchengxue <chenchengxue@kuaidaoapp.com>
2019-05-30 11:24:47 +0800

    删除引进多余的包

--- src/main/java/com/kuaidao/manageweb/controller/statistics/callrecord/TeleSaleTalkTimeController.java
+++ src/main/java/com/kuaidao/manageweb/controller/statistics/callrecord/TeleSaleTalkTimeController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.itextpdf.text.pdf.PdfStructTreeController.returnType;
 import com.kuaidao.common.constant.OrgTypeConstant;
 import com.kuaidao.common.constant.RoleCodeEnum;
 import com.kuaidao.common.constant.SystemCodeConstant;
