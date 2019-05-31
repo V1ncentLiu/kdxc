3713d697222af6a731da4bd8a061cdd8a2c83ad0
weiyanping <weiyanping@kuaidaoapp.com>
2019-05-31 13:05:45 +0800

    表头

--- src/main/java/com/kuaidao/manageweb/controller/statistics/teleSaleTracking/TeleSaleTrackingController.java
+++ src/main/java/com/kuaidao/manageweb/controller/statistics/teleSaleTracking/TeleSaleTrackingController.java
@@ -74,9 +74,9 @@ public class TeleSaleTrackingController {
             }
         }
         if(null != trackingQueryDto.getCusLevelList() && trackingQueryDto.getCusLevelList().size() > 0){
-            return teleSaleTrackingFeignClient.getRecordByGroupLevelPage(trackingQueryDto);
+            return  teleSaleTrackingFeignClient.getRecordByGroupLevelPage(trackingQueryDto);
         }else{
-            return teleSaleTrackingFeignClient.getRecordByGroupPage(trackingQueryDto);
+            return  teleSaleTrackingFeignClient.getRecordByGroupPage(trackingQueryDto);
         }
     }
 
@@ -202,13 +202,12 @@ public class TeleSaleTrackingController {
             list = teleSaleTrackingFeignClient.getRecordByGroupLevelUserId(trackingQueryDto);
         }
         List<List<Object>> dataList = new ArrayList<List<Object>>();
-        dataList.add(getHeadTitleList());
+        dataList.add(getHeadSumTitleList());
         List<TeleSaleTrackingDto> orderList = list.getData();
         for(int i = 0; i<orderList.size(); i++){
             TeleSaleTrackingDto ra = orderList.get(i);
             List<Object> curList = new ArrayList<>();
             curList.add(i + 1);
-            curList.add(ra.getDateId());
             curList.add(ra.getOrgName());
             curList.add(ra.getUserName());
             curList.add(getCusLeveName(ra.getCusLevel()));
@@ -451,6 +450,19 @@ public class TeleSaleTrackingController {
         List<Object> headTitleList = new ArrayList<>();
         headTitleList.add("序号");
         headTitleList.add("日期");
+        headTitleList.add("电销组");
+        headTitleList.add("电销顾问");
+        headTitleList.add("客户级别");
+        headTitleList.add("资源数");
+        headTitleList.add("回访次数");
+        headTitleList.add("回访资源数");
+        headTitleList.add("人均天回访次数");
+        return headTitleList;
+    }
+
+    private List<Object> getHeadSumTitleList() {
+        List<Object> headTitleList = new ArrayList<>();
+        headTitleList.add("序号");
         headTitleList.add("电销组");
         headTitleList.add("电销顾问");
         headTitleList.add("客户级别");
