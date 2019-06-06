7681069e5dfc077fec2a34fb79f7fd4d91b83ee3
zhangxingyu <408744797@qq.com>
2019-06-06 09:55:10 +0800

    tijiao

--- src/main/java/com/kuaidao/manageweb/controller/rule/NotOptRuleController.java
+++ src/main/java/com/kuaidao/manageweb/controller/rule/NotOptRuleController.java
@@ -96,7 +96,7 @@ public class NotOptRuleController {
         // 查询字典媒介集合
         request.setAttribute("mediumList", getDictionaryByCode(Constants.MEDIUM));
         // 当前人员id
-        request.setAttribute("userId", user.getId());
+        request.setAttribute("userId", user.getId() + "");
         return "rule/notOptRuleManagerPage";
     }
 
