da638927196353462c524f241c52291ee835a295
fanjd <fjd89@126.com>
2019-06-17 16:30:31 +0800

    增加clueRelate Fegin

---
+++ src/main/java/com/kuaidao/manageweb/feign/clue/ClueRelateFeignClient.java
@@ -0,0 +1,62 @@
+package com.kuaidao.manageweb.feign.clue;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.kuaidao.aggregation.dto.clue.ClueRelateReq;
+import com.kuaidao.common.constant.SysErrorCodeEnum;
+import com.kuaidao.common.entity.JSONResult;
+
+/**
+ * 线索-所属组织
+ * 
+ * @author: fanjd
+ * @date: 2019年6月17日
+ * @version V1.0
+ */
+@FeignClient(name = "aggregation-service", path = "/aggregation/clue/clueRelate", fallback = ClueRelateFeignClient.HystrixClientFallback.class)
+public interface ClueRelateFeignClient {
+
+    /**
+     * 根据创业顾问id更新所在电销组组织信息
+     * 
+     * @author: Fanjd
+     * @param clueRelateReq
+     * @return:
+     * @Date: 2019/6/17 16:12
+     * @since: 1.0.0
+     **/
+    @PostMapping("/updateClueRelateByTeleSaleId")
+
+    JSONResult<String> updateClueRelateByTeleSaleId(@RequestBody ClueRelateReq clueRelateReq);
+
+
+
+    @Component
+    class HystrixClientFallback implements ClueRelateFeignClient {
+
+        private static Logger logger = LoggerFactory.getLogger(ClueRelateFeignClient.class);
+
+
+        private JSONResult fallBackError(String name) {
+            logger.error(name + "接口调用失败：无法获取目标服务");
+            return new JSONResult().fail(SysErrorCodeEnum.ERR_REST_FAIL.getCode(), SysErrorCodeEnum.ERR_REST_FAIL.getMessage());
+        }
+
+
+        @Override
+        public JSONResult<String> updateClueRelateByTeleSaleId(@RequestBody ClueRelateReq clueRelateReq) {
+            return fallBackError("根据创业顾问id更新所在电销组组织信息失败");
+        }
+
+
+
+    }
+
+
+
+}
