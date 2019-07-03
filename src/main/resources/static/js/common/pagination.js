ac8d44b89e33e6dbc96cc123f353364e427e0ff1
lisashare <lisa_zhu2012@foxmail.com>
2019-07-03 14:59:08 +0800

    报表模块分页优化

--- src/main/resources/static/js/common/pagination.js
+++ src/main/resources/static/js/common/pagination.js
@@ -17,44 +17,35 @@ Vue.component('table-pagination',{
       },
     },
     computed: {
-       total() {
-          return this.pager.total;
-       },
-        initBack() {
-             return this.pager.total / this.pager.pageSize < this.pager.currentPage;
-        }
+      total() {
+        return this.pager.total;
+      },
+      initBack() {
+        return this.pager.total / this.pager.pageSize < this.pager.currentPage;
+      }
     }, 
-     watch: { 
+    watch: { 
        total() { 
          // 存在记录但未获取到数据时, 重新请求 
           if (this.initBack) { 
             // this.pager.currentPage -= 1; this.$emit('change');
            } 
        },
-     },
-
-    methods:{
-        handleSizeChange(val) {
-          debugger
-          console.log(val)
-             //下拉框  每页 10,20条切换 调用
-             this.pager.currentPage = 1;
-             this.pager.pageSize=val;
-             localStorage.setItem('allChangePageSize', val);
-             this.$emit('change');
-          },
-          handleCurrentChange(val) {
-              //点击 页码
-           this.pager.currentPage = val;
-           this.$emit('change');
-          },
     },
-    created(){
-        var localVal=parseInt(localStorage.getItem('allChangePageSize'));
-        if(localVal){
-            this.pager.pageSize = localVal;
-        }
-   }
+    methods:{
+      handleSizeChange(val) {
+        //下拉框  每页 10,20条切换 调用
+        this.pager.currentPage = 1;
+        this.pager.pageSize=val;
+        localStorage.setItem('allChangePageSize', val);
+        this.$emit('change');
+      },
+      handleCurrentChange(val) {
+        //点击 页码
+        this.pager.currentPage = val;
+        this.$emit('change');
+      },
+    }
 });
 
 
