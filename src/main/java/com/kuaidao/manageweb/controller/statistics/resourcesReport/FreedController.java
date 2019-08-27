85a15f24f65775ef20649c962376a3e577901aaa
guhuitao <guhuitao@kuaidaoapp.com>
2019-08-27 19:04:20 +0800

    资源驳回原因统计

--- src/main/java/com/kuaidao/manageweb/controller/statistics/resourcesReport/FreedController.java
+++ src/main/java/com/kuaidao/manageweb/controller/statistics/resourcesReport/FreedController.java
@@ -1,9 +1,39 @@
 package com.kuaidao.manageweb.controller.statistics.resourcesReport;
 
+import com.kuaidao.common.constant.OrgTypeConstant;
+import com.kuaidao.common.constant.RoleCodeEnum;
+import com.kuaidao.common.entity.IdEntity;
+import com.kuaidao.common.entity.JSONResult;
+import com.kuaidao.common.util.ExcelUtil;
+import com.kuaidao.manageweb.controller.statistics.busCostomerVisit.BusCustomerVisitController;
+import com.kuaidao.manageweb.feign.organization.OrganizationFeignClient;
+import com.kuaidao.manageweb.feign.statistics.resourceAllocation.StatisticsFreeFeignClient;
+import com.kuaidao.manageweb.util.CommUtil;
+import com.kuaidao.stastics.dto.bussCoustomerVisit.CustomerVisitDto;
+import com.kuaidao.stastics.dto.resourceFree.ResourceFreeDto;
+import com.kuaidao.stastics.dto.resourceFree.ResourceFreeQueryDto;
+import com.kuaidao.sys.dto.organization.OrganizationDTO;
+import com.kuaidao.sys.dto.organization.OrganizationQueryDTO;
+import com.kuaidao.sys.dto.organization.OrganizationRespDTO;
+import com.kuaidao.sys.dto.user.UserInfoDTO;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.net.URLEncoder;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author: guhuitao
@@ -14,7 +44,12 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/resourceFreed")
 public class FreedController {
 
+    private static Logger logger = LoggerFactory.getLogger(FreedController.class);
 
+    @Autowired
+    private StatisticsFreeFeignClient statisticsFreeFeignClient;
+    @Autowired
+    private OrganizationFeignClient organizationFeignClient;
     /**
      * 资源释放页面
      * @param request
@@ -22,7 +57,121 @@ public class FreedController {
      */
     @RequestMapping("/releasePie")
     public String freedView(HttpServletRequest request){
+        initOrg(request);
        return "reportResources/releasePie";
+    }
+
+    /**
+     * 查询资源释放原因数据
+     * @param dto
+     * @return
+     */
+    @RequestMapping("/queryList")
+    public @ResponseBody JSONResult<List<ResourceFreeDto>> queryList(@RequestBody ResourceFreeQueryDto dto){
+        initParams(dto);
+        return statisticsFreeFeignClient.queryList(dto);
+    }
+
+
+    @RequestMapping("/export")
+    public @ResponseBody void export(@RequestBody ResourceFreeQueryDto dto, HttpServletResponse response){
+        try {
+            initParams(dto);
+            JSONResult<List<ResourceFreeDto>> result = statisticsFreeFeignClient.queryList(dto);
+            if ("0".equals(result.getCode())) {
+                CustomerVisitDto[] dtos = result.getData().toArray(new CustomerVisitDto[0]);
+                String[] keys = {"name", "value"};
+                String[] hader = {"原因", "数量"};
+                Workbook wb = ExcelUtil.createWorkBook(dtos, keys, hader);
+                String name = MessageFormat.format("资源释放原因统计表_{0}_{1}.xlsx", "" + dto.getStartTime(), dto.getEndTime() + "");
+                response.addHeader("Content-Disposition",
+                        "attachment;filename=\"" + name + "\"");
+                response.addHeader("fileName", URLEncoder.encode(name, "utf-8"));
+                response.setContentType("application/octet-stream");
+                ServletOutputStream outputStream = response.getOutputStream();
+                wb.write(outputStream);
+                outputStream.close();
+            }
+        }catch (Exception e){
+            logger.error(e.getMessage(),e);
+        }
+    }
+
+    /**
+     * 初始化页面参数
+     * @param request
+     */
+    public void initOrg(HttpServletRequest request){
+        UserInfoDTO curLoginUser = CommUtil.getCurLoginUser();
+        String roleCode=curLoginUser.getRoleList().get(0).getRoleCode();
+
+        OrganizationQueryDTO queryDTO = new OrganizationQueryDTO();
+        //查询电销事业部
+        queryDTO.setOrgType(OrgTypeConstant.DZSYB);
+        if(RoleCodeEnum.DXZJL.name().equals(roleCode)){
+            queryDTO.setParentId(curLoginUser.getOrgId());
+        }else if(RoleCodeEnum.DXFZ.name().equals(roleCode)){
+            queryDTO.setId(curLoginUser.getOrgId());
+            request.setAttribute("deptId",curLoginUser.getOrgId());
+        }else if(RoleCodeEnum.DXZJ.name().equals(roleCode) || RoleCodeEnum.DXCYGW.name().equals(roleCode)){
+            IdEntity idEntity=new IdEntity();
+            idEntity.setId(curLoginUser.getOrgId().toString());
+            JSONResult<OrganizationDTO> jsonResult= organizationFeignClient.queryOrgById(idEntity);
+            queryDTO.setId(jsonResult.getData().getParentId());
+            request.setAttribute("deptId",jsonResult.getData().getParentId());
+            request.setAttribute("teleGroupId",curLoginUser.getOrgId());
+            if(RoleCodeEnum.DXCYGW.name().equals(roleCode)){
+                request.setAttribute("teleSaleId",curLoginUser.getId());
+            }
+        }else if(RoleCodeEnum.GLY.name().equals(roleCode)){
+            //管理员可以查看全部
+        }else{
+            //other 没权限
+            queryDTO.setId(-1l);
+        }
+        request.setAttribute("roleCode",roleCode);
+        JSONResult<List<OrganizationRespDTO>> queryOrgByParam =
+                organizationFeignClient.queryOrgByParam(queryDTO);
+        request.setAttribute("deptList",queryOrgByParam.getData());
+
+    }
+
+    /**
+     * 根据角色设置权限参数
+     * @param dto
+     */
+    public void initParams(ResourceFreeQueryDto dto){
+        UserInfoDTO curLoginUser = CommUtil.getCurLoginUser();
+        String roleCode=curLoginUser.getRoleList().get(0).getRoleCode();
+        if(RoleCodeEnum.DXZJL.name().equals(roleCode)){
+            if(null!=dto.getTeleDeptId()){
+                dto.setTeleDeptIds(new ArrayList<>(Arrays.asList(dto.getTeleDeptId())));
+            }else {
+                OrganizationQueryDTO queryDTO = new OrganizationQueryDTO();
+                //总监理查看下属所有事业部
+                queryDTO.setOrgType(OrgTypeConstant.DZSYB);
+                queryDTO.setParentId(curLoginUser.getOrgId());
+                JSONResult<List<OrganizationRespDTO>> result =
+                        organizationFeignClient.queryOrgByParam(queryDTO);
+                List<Long> ids = result.getData().stream().map(c -> c.getId()).collect(Collectors.toList());
+                dto.setTeleDeptIds(ids);
+            }
+        }else if(RoleCodeEnum.DXFZ.name().equals(roleCode)){
+            //副总查看当前事业部
+            dto.setTeleDeptIds(new ArrayList<>(Arrays.asList(curLoginUser.getOrgId())));
+        }else if(RoleCodeEnum.DXZJ.name().equals(roleCode)){
+            //总监查看上去组
+           dto.setTeleGroupId(curLoginUser.getOrgId());
+        }else if(RoleCodeEnum.DXCYGW.name().equals(roleCode)){
+            //顾问查看自己
+            dto.setTeleSaleId(curLoginUser.getId());
+        }else if(RoleCodeEnum.GLY.name().equals(roleCode)){
+            //管理员可以查看全部
+        }else{
+            //other
+            dto.setTeleSaleId(curLoginUser.getId());
+        }
+
     }
 
 }
---
+++ src/main/java/com/kuaidao/manageweb/feign/statistics/resourceAllocation/StatisticsFreeFeignClient.java
@@ -0,0 +1,49 @@
+package com.kuaidao.manageweb.feign.statistics.resourceAllocation;
+
+
+import com.kuaidao.common.constant.SysErrorCodeEnum;
+import com.kuaidao.common.entity.JSONResult;
+import com.kuaidao.common.entity.PageBean;
+import com.kuaidao.stastics.dto.resourceAllocation.ResourceAllocationDto;
+import com.kuaidao.stastics.dto.resourceAllocation.ResourceAllocationQueryDto;
+import com.kuaidao.stastics.dto.resourceFree.ResourceFreeDto;
+import com.kuaidao.stastics.dto.resourceFree.ResourceFreeQueryDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@FeignClient(name = "statstics-service3", path = "/statstics/resourceFree", fallback = StatisticsFreeFeignClient.HystrixClientFallback.class)
+public interface StatisticsFreeFeignClient {
+
+    /**
+     * 查询全部资源释放原因记录
+     * @param resourceFreeQueryDto
+     * @return
+     */
+    @PostMapping("/queryList")
+    JSONResult<List<ResourceFreeDto>> queryList(@RequestBody ResourceFreeQueryDto resourceFreeQueryDto);
+
+
+
+    @Component
+    class HystrixClientFallback implements StatisticsFreeFeignClient {
+
+        private static Logger logger = LoggerFactory.getLogger(StatisticsFreeFeignClient.class);
+
+        private JSONResult fallBackError(String name) {
+            logger.error(name + "接口调用失败：无法获取目标服务");
+            return new JSONResult().fail(SysErrorCodeEnum.ERR_REST_FAIL.getCode(),
+                    SysErrorCodeEnum.ERR_REST_FAIL.getMessage());
+        }
+
+        @Override
+        public JSONResult<List<ResourceFreeDto>> queryList(ResourceFreeQueryDto resourceFreeQueryDto) {
+            return fallBackError("查询资源释放原因失败");
+        }
+    }
+}
