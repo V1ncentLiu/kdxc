e4f2cc7720d56217d6c38be8cedcd153c6bd39a6
weiyanping <weiyanping@kuaidaoapp.com>
2019-08-30 10:41:03 +0800

    修改

--- src/main/java/com/kuaidao/manageweb/controller/statistics/unpaidFunds/UnpaidFundsController.java
+++ src/main/java/com/kuaidao/manageweb/controller/statistics/unpaidFunds/UnpaidFundsController.java
@@ -61,7 +61,7 @@ public class UnpaidFundsController {
      * 总监一级级页面跳转
      */
     @RequestMapping("/businessTotalArrearsTable")
-    public String businessVisitReceptionTable(Long userId,Long orgId,Long endTime,Long projectId,Integer days,HttpServletRequest request) {
+    public String businessVisitReceptionTable(Long userId,Long orgId,Long endTime,Long projectId,String days,HttpServletRequest request) {
         pageParams(userId,orgId,endTime,projectId,days,request);
         initAuth(null,request);
         return "reportformsBusiness/businessTotalArrearsTable";
@@ -70,7 +70,7 @@ public class UnpaidFundsController {
      * 总监二级页面跳转
      */
     @RequestMapping("/businessTotalArrearsTableTeam")
-    public String businessVisitReceptionTablePerson(Long userId,Long orgId,Long endTime,Long projectId,Integer days,HttpServletRequest request) {
+    public String businessVisitReceptionTablePerson(Long userId,Long orgId,Long endTime,Long projectId,String days,HttpServletRequest request) {
         pageParams(userId,orgId,endTime,projectId,days,request);
         initAuth(null,request);
         return "reportformsBusiness/businessTotalArrearsTableTeam";
@@ -203,7 +203,7 @@ public class UnpaidFundsController {
     /**
      *  返回页面携带参数
      */
-    private void pageParams(Long userId,Long orgId,Long endTime,Long projectId,Integer days,HttpServletRequest request){
+    private void pageParams(Long userId,Long orgId,Long endTime,Long projectId,String days,HttpServletRequest request){
         UnpaidFundsQueryDto unpaidFundsQueryDto = new UnpaidFundsQueryDto();
         unpaidFundsQueryDto.setOrgId(orgId);
         unpaidFundsQueryDto.setEndTime(endTime);
