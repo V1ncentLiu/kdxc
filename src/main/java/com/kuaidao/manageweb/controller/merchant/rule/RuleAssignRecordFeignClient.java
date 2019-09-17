10c0a39113fa7d46060f34565d855961c2d3d676
fanjd <fjd89@126.com>
2019-09-17 15:39:35 +0800

    删除fegin

---
+++ src/main/java/com/kuaidao/manageweb/controller/merchant/rule/RuleAssignRecordFeignClient.java
@@ -0,0 +1,11 @@
+package com.kuaidao.manageweb.controller.merchant.rule;
+
+/**
+ * TODO
+ *
+ * @author:fanjd
+ * @date:2019/9/17
+ * @since 1.0.0
+ */
+public interface RuleAssignRecordFeignClient {
+}
