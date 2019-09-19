3e21d87361b03330caa87860440c392589e1b194
xuyunfeng <xuyunfeng@kd.cn>
2019-09-19 19:04:50 +0800

    公有池列表无数据

--- src/main/java/com/kuaidao/manageweb/controller/merchant/pubcustomer/PubcustomerController.java
+++ src/main/java/com/kuaidao/manageweb/controller/merchant/pubcustomer/PubcustomerController.java
@@ -2,15 +2,15 @@ package com.kuaidao.manageweb.controller.merchant.pubcustomer;
 
 import com.kuaidao.common.entity.JSONResult;
 import com.kuaidao.common.entity.PageBean;
-import com.kuaidao.common.util.CommonUtil;
 import com.kuaidao.manageweb.feign.dictionary.DictionaryItemFeignClient;
-
 import com.kuaidao.manageweb.feign.merchant.publiccustomer.PubcustomerFeignClient;
+import com.kuaidao.manageweb.feign.merchant.user.MerchantUserInfoFeignClient;
 import com.kuaidao.manageweb.util.CommUtil;
 import com.kuaidao.merchant.dto.pubcusres.ClueQueryParamDTO;
 import com.kuaidao.merchant.dto.pubcusres.ClueReceiveRecordsDTO;
 import com.kuaidao.merchant.dto.pubcusres.PublicCustomerResourcesRespDTO;
 import com.kuaidao.sys.dto.user.UserInfoDTO;
+import com.kuaidao.sys.dto.user.UserInfoReq;
 import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -37,7 +37,8 @@ public class PubcustomerController {
 
     @Autowired
     private PubcustomerFeignClient pubcustomerFeignClient;
-
+    @Autowired
+    private MerchantUserInfoFeignClient merchantUserInfoFeignClient;
     /**
      * 分页查询
      */
@@ -66,6 +67,16 @@ public class PubcustomerController {
     @ResponseBody
     public JSONResult<PageBean<PublicCustomerResourcesRespDTO>> queryListPage(
             @RequestBody ClueQueryParamDTO dto) {
+       UserInfoDTO user = CommUtil.getCurLoginUser();
+       if(user.getUserType() != null && user.getUserType() ==2){
+         dto.setBussinessAccount(user.getId());
+       }else if(user.getUserType() != null && user.getUserType() ==3){
+         UserInfoReq req = new UserInfoReq();
+         req.setId(user.getParentId());
+         JSONResult<UserInfoReq> jsonResult = merchantUserInfoFeignClient.getMechantUserById(req);
+         UserInfoReq userDto = jsonResult.getData();
+         dto.setBussinessAccount(userDto.getId());
+       }
         return pubcustomerFeignClient.queryListPage(dto);
     }
 
