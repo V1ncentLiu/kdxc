5c0c6254167382257809d4ce712e6ca68556f2a1
yangbiao <923937157@qq.com>
2019-09-30 10:32:01 +0800

    第三批提交

--- src/main/java/com/kuaidao/manageweb/controller/merchant/resourcetrajectory/ResourceTrajectoryController.java
+++ src/main/java/com/kuaidao/manageweb/controller/merchant/resourcetrajectory/ResourceTrajectoryController.java
@@ -1,13 +1,27 @@
 package com.kuaidao.manageweb.controller.merchant.resourcetrajectory;
 
+import com.kuaidao.aggregation.dto.clue.ClueBasicDTO;
+import com.kuaidao.aggregation.dto.clue.ClueCustomerDTO;
+import com.kuaidao.aggregation.dto.clue.ClueDTO;
+import com.kuaidao.aggregation.dto.clue.ClueQueryDTO;
+import com.kuaidao.aggregation.dto.log.ImLogsDTO;
+import com.kuaidao.common.entity.IdEntityLong;
 import com.kuaidao.common.entity.JSONResult;
+import com.kuaidao.common.util.CommonUtil;
 import com.kuaidao.manageweb.controller.merchant.pubcustomer.PubcustomerController;
+import com.kuaidao.manageweb.feign.clue.ClueBasicFeignClient;
+import com.kuaidao.manageweb.feign.clue.ClueCustomerFeignClient;
+import com.kuaidao.manageweb.feign.clue.MyCustomerFeignClient;
+import com.kuaidao.manageweb.feign.log.ImLogMgtFeignClient;
 import com.kuaidao.merchant.dto.pubcusres.ClueReceiveRecordsDTO;
 import com.kuaidao.merchant.dto.resourcetrajectory.ResourceTrajectoryDTO;
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,6 +34,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/merchant/resourceTrajectory")
 public class ResourceTrajectoryController {
   private static Logger logger = LoggerFactory.getLogger(ResourceTrajectoryController.class);
+
+  @Autowired
+  ImLogMgtFeignClient imLogMgtFeignClient;
+
+  @Autowired
+  private MyCustomerFeignClient myCustomerFeignClient;
+
+
   /**
    * 页面跳转
    */
@@ -34,10 +56,58 @@ public class ResourceTrajectoryController {
 
   @ResponseBody
   @RequestMapping("/data")
-  public JSONResult<ResourceTrajectoryDTO> data() {
+  public JSONResult<ResourceTrajectoryDTO> data(@RequestBody IdEntityLong clueId) {
+
     ResourceTrajectoryDTO resourceTrajectory = new ResourceTrajectoryDTO();
 
+    // 查询聊天记录：
+    ImLogsDTO logReqDTO = new ImLogsDTO();
+    logReqDTO.setClueId(clueId.getId());
+    JSONResult<List<ImLogsDTO>> imLogs = imLogMgtFeignClient.queryIMLogRecord(logReqDTO);
+    if(CommonUtil.resultCheck(imLogs)){
+      resourceTrajectory.setChatList(imLogs.getData());
+    }
+    // 查询基础
+//    customerFeignClient.findcustomersByClueIds();
+    ClueQueryDTO queryDTO = new ClueQueryDTO();
+    queryDTO.setClueId(clueId.getId());
+    JSONResult<ClueDTO> clueInfo = myCustomerFeignClient.findClueInfo(queryDTO);
 
+    if(CommonUtil.resultCheck(clueInfo)){
+      ClueDTO data = clueInfo.getData();
+      ClueCustomerDTO clueCustomer = data.getClueCustomer();
+      if(clueCustomer!=null){
+        resourceTrajectory.setAge(clueCustomer.getAge());
+        resourceTrajectory.setCusName(clueCustomer.getCusName());
+        resourceTrajectory.setEmail(clueCustomer.getEmail());
+        resourceTrajectory.setPhone(clueCustomer.getPhone());
+        resourceTrajectory.setPhone2(clueCustomer.getPhone2());
+        resourceTrajectory.setPhone3(clueCustomer.getPhone3());
+        resourceTrajectory.setPhone4(clueCustomer.getPhone4());
+        resourceTrajectory.setPhone5(clueCustomer.getPhone5());
+        resourceTrajectory.setWechat(clueCustomer.getWechat());
+        resourceTrajectory.setQq(clueCustomer.getQq());
+        resourceTrajectory.setSex(clueCustomer.getSex()==1?"男":"女");
+      }
+      ClueBasicDTO clueBasic = data.getClueBasic();
+      if(clueBasic!=null){
+        // 客户详情信息
+        resourceTrajectory.setClueId(clueBasic.getId());
+        resourceTrajectory.setMessageTime(clueBasic.getMessageTime());
+        resourceTrajectory.setMessagePoint(clueBasic.getMessagePoint());
+        resourceTrajectory.setSearchWord(clueBasic.getSearchWord());
+        resourceTrajectory.setProjectName(clueBasic.getProjectName());
+//        设置图片
+//        resourceTrajectory.setImageUrl();
+//        设置跳转URL
+//        resourceTrajectory.setUrl();
+        resourceTrajectory.setSource(clueBasic.getSourceName());
+        resourceTrajectory.setCategory(clueBasic.getCategoryName());
+      }
+    }
+    // 设置地域
+
+    // 设置终端
     return new JSONResult().success(resourceTrajectory);
   }
 
