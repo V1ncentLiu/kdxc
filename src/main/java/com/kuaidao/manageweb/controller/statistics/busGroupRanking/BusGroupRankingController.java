cb95e8d78f3a578db8bd1be92566d19269290c12
weiyanping <weiyanping@kuaidaoapp.com>
2019-09-30 14:02:06 +0800

    页面修改

---
+++ src/main/java/com/kuaidao/manageweb/controller/statistics/busGroupRanking/BusGroupRankingController.java
@@ -0,0 +1,133 @@
+package com.kuaidao.manageweb.controller.statistics.busGroupRanking;
+
+
+import com.kuaidao.aggregation.dto.project.CompanyInfoDTO;
+import com.kuaidao.aggregation.dto.project.ProjectInfoDTO;
+import com.kuaidao.common.constant.OrgTypeConstant;
+import com.kuaidao.common.constant.RoleCodeEnum;
+import com.kuaidao.common.constant.SystemCodeConstant;
+import com.kuaidao.common.entity.JSONResult;
+import com.kuaidao.manageweb.controller.statistics.BaseStatisticsController;
+import com.kuaidao.manageweb.feign.organization.OrganizationFeignClient;
+import com.kuaidao.manageweb.feign.project.CompanyInfoFeignClient;
+import com.kuaidao.manageweb.feign.project.ProjectInfoFeignClient;
+import com.kuaidao.manageweb.feign.statistics.busGroupRanking.BusGroupRankingFeignClient;
+import com.kuaidao.manageweb.util.CommUtil;
+import com.kuaidao.stastics.dto.base.BaseBusQueryDto;
+import com.kuaidao.sys.dto.organization.OrganizationQueryDTO;
+import com.kuaidao.sys.dto.organization.OrganizationRespDTO;
+import com.kuaidao.sys.dto.user.UserInfoDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * / 商务报表 / 业绩报表 / 集团项目业绩表
+ */
+@Slf4j
+@RequestMapping("/busGroupRanking")
+@Controller
+public class BusGroupRankingController extends BaseStatisticsController {
+
+    @Autowired
+    private BusGroupRankingFeignClient busGroupRankingFeignClient;
+    @Autowired
+    private OrganizationFeignClient organizationFeignClient;
+    @Autowired
+    private ProjectInfoFeignClient projectInfoFeignClient;
+    @Autowired
+    private CompanyInfoFeignClient companyInfoFeignClient;
+
+
+    /**
+     * 一级页面跳转
+     */
+    @RequestMapping("/toGroupProjectPerformance")
+    public String toGroupProjectPerformance(HttpServletRequest request) {
+        //商务组
+        initOrgList(request);
+        //商务大区
+        initBugOrg(request);
+        return "reportBusPerformance/groupProjectPerformance";
+    }
+
+    /**
+     * 二级页面跳转
+     */
+    @RequestMapping("/toGroupProjectPerformanceDetail ")
+    public String toGroupProjectPerformanceDetail(HttpServletRequest request) {
+        //商务组
+        initOrgList(request);
+        //商务大区
+        initBugOrg(request);
+        return "reportBusPerformance/groupProjectPerformanceDetail";
+    }
+
+
+    /**
+     * 一级页面分页查询
+     */
+    @RequestMapping("/getOnePageList")
+    public JSONResult<Map<String,Object>> getOneBusGroupRankingPageList(@RequestBody BaseBusQueryDto baseBusQueryDto){
+        JSONResult<Map<String, Object>> oneBusGroupRankingPageList = busGroupRankingFeignClient.getOneBusGroupRankingPageList(baseBusQueryDto);
+        return oneBusGroupRankingPageList;
+    }
+    /**
+     * 一级页面查询全部
+     */
+    @RequestMapping("/getOneList")
+    public JSONResult<Map<String,Object>> getOneBusGroupRankingList(@RequestBody BaseBusQueryDto baseBusQueryDto){
+        return busGroupRankingFeignClient.getOneBusGroupRankingList(baseBusQueryDto);
+    }
+    /**
+     * 二级页面分页查询
+     */
+    @RequestMapping("/getTwoPageList")
+    public JSONResult<Map<String,Object>> getTwoBusGroupRankingPageList(@RequestBody BaseBusQueryDto baseBusQueryDto){
+        return busGroupRankingFeignClient.getTwoBusGroupRankingPageList(baseBusQueryDto);
+    }
+    /**
+     * 二级页面查询全部
+     */
+    @RequestMapping("/getTwoList")
+    public JSONResult<Map<String,Object>> getTwoBusGroupRankingList(@RequestBody BaseBusQueryDto baseBusQueryDto){
+        return busGroupRankingFeignClient.getTwoBusGroupRankingList(baseBusQueryDto);
+    }
+    private void initOrgList(HttpServletRequest request){
+        UserInfoDTO curLoginUser = CommUtil.getCurLoginUser();
+        //商务组
+        String roleCode=curLoginUser.getRoleList().get(0).getRoleCode();
+        OrganizationQueryDTO busGroupReqDTO = new OrganizationQueryDTO();
+        if(RoleCodeEnum.SWDQZJ.name().equals(roleCode)){
+            busGroupReqDTO.setParentId(curLoginUser.getOrgId());
+        }else if(RoleCodeEnum.SWZJ.name().equals(roleCode) || RoleCodeEnum.SWJL.name().equals(roleCode)){
+            busGroupReqDTO.setId(curLoginUser.getOrgId());
+        }else if(RoleCodeEnum.GLY.name().equals(roleCode)){
+            //管理员可以查看全部
+            log.info("管理员登录");
+        }else{
+            //other 没权限
+            busGroupReqDTO.setId(-1l);
+        }
+        busGroupReqDTO.setSystemCode(SystemCodeConstant.HUI_JU);
+        busGroupReqDTO.setOrgType(OrgTypeConstant.SWZ);
+        JSONResult<List<OrganizationRespDTO>> listJSONResult = organizationFeignClient.queryOrgByParam(busGroupReqDTO);
+        List<OrganizationRespDTO> data = listJSONResult.getData();
+        request.setAttribute("busGroupList",data);
+
+        // 查询所有项目
+        JSONResult<List<ProjectInfoDTO>> allProject = projectInfoFeignClient.allProject();
+        request.setAttribute("projectList", allProject.getData());
+
+        //餐饮集团
+        JSONResult<List<CompanyInfoDTO>> listNoPage = companyInfoFeignClient.getCompanyList();
+        request.setAttribute("companyList", listNoPage.getData());
+    }
+
+}
---
+++ src/main/java/com/kuaidao/manageweb/feign/statistics/busGroupRanking/BusGroupRankingFeignClient.java
@@ -0,0 +1,76 @@
+package com.kuaidao.manageweb.feign.statistics.busGroupRanking;
+
+import com.kuaidao.common.constant.SysErrorCodeEnum;
+import com.kuaidao.common.entity.JSONResult;
+import com.kuaidao.manageweb.feign.statistics.appointmentVisit.AppointmentVisitFeignClient;
+import com.kuaidao.stastics.dto.base.BaseBusQueryDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+/**
+ * 集团项目业绩表
+ */
+@FeignClient(name = "statstics-service-wyp", path = "/statstics/busGroupRanking", fallback = BusGroupRankingFeignClient.HystrixClientFallback.class)
+public interface BusGroupRankingFeignClient {
+
+
+    /**
+     * 一级页面分页查询
+     */
+    @RequestMapping("/getOnePageList")
+    JSONResult<Map<String,Object>> getOneBusGroupRankingPageList(@RequestBody BaseBusQueryDto baseBusQueryDto);
+    /**
+     * 一级页面查询全部
+     */
+    @RequestMapping("/getOneList")
+    JSONResult<Map<String,Object>> getOneBusGroupRankingList(@RequestBody BaseBusQueryDto baseBusQueryDto);
+    /**
+     * 二级页面分页查询
+     */
+    @RequestMapping("/getTwoPageList")
+    JSONResult<Map<String,Object>> getTwoBusGroupRankingPageList(@RequestBody BaseBusQueryDto baseBusQueryDto);
+    /**
+     * 二级页面查询全部
+     */
+    @RequestMapping("/getTwoList")
+    JSONResult<Map<String,Object>> getTwoBusGroupRankingList(@RequestBody BaseBusQueryDto baseBusQueryDto);
+
+
+    @Component
+    class HystrixClientFallback implements BusGroupRankingFeignClient {
+        private static Logger logger = LoggerFactory.getLogger(AppointmentVisitFeignClient.class);
+
+        private JSONResult fallBackError(String name) {
+            logger.error(name + "接口调用失败：无法获取目标服务");
+            return new JSONResult().fail(SysErrorCodeEnum.ERR_REST_FAIL.getCode(),
+                    SysErrorCodeEnum.ERR_REST_FAIL.getMessage());
+        }
+
+        @Override
+        public JSONResult<Map<String, Object>> getOneBusGroupRankingPageList(BaseBusQueryDto baseBusQueryDto) {
+            return fallBackError("集团项目业绩表分页查询一级页面");
+        }
+
+        @Override
+        public JSONResult<Map<String, Object>> getOneBusGroupRankingList(BaseBusQueryDto baseBusQueryDto) {
+            return fallBackError("集团项目业绩表查询全部一级页面");
+        }
+
+        @Override
+        public JSONResult<Map<String, Object>> getTwoBusGroupRankingPageList(BaseBusQueryDto baseBusQueryDto) {
+            return fallBackError("集团项目业绩表分页查询二级页面");
+        }
+
+        @Override
+        public JSONResult<Map<String, Object>> getTwoBusGroupRankingList(BaseBusQueryDto baseBusQueryDto) {
+            return fallBackError("集团项目业绩表查询全部二级页面");
+        }
+    }
+
+}
