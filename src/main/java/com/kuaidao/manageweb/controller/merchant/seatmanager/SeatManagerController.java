32dfc44bfebde04894e77fc3b570ba7b5dc4ae53
yangbiao <923937157@qq.com>
2019-10-04 10:22:33 +0800

    坐席管理

---
+++ src/main/java/com/kuaidao/manageweb/controller/merchant/seatmanager/SeatManagerController.java
@@ -0,0 +1,10 @@
+package com.kuaidao.manageweb.controller.merchant.seatmanager;
+
+/**
+ * @Auther: admin
+ * @Date: 2019/10/4 10:22
+ * @Description:
+ */
+public class SeatManagerController {
+
+}
