0c382c687dd2ddae6be030f8e1e747bb2f95a424
shiguopu <shiguopu@kd.cn>
2019-10-10 20:35:51 +0800

    云外呼

--- src/main/java/com/kuaidao/manageweb/controller/merchant/bussinesscall/CallPackageController.java
+++ src/main/java/com/kuaidao/manageweb/controller/merchant/bussinesscall/CallPackageController.java
@@ -3,7 +3,9 @@ package com.kuaidao.manageweb.controller.merchant.bussinesscall;
 import com.kuaidao.account.dto.call.*;
 import com.kuaidao.common.entity.JSONResult;
 import com.kuaidao.manageweb.feign.merchant.bussinesscall.CallPackageFeignClient;
+import com.kuaidao.sys.dto.user.UserInfoDTO;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -30,6 +32,8 @@ public class CallPackageController {
      **/
     @RequestMapping("/index")
     public String index(HttpServletRequest request) {
+        UserInfoDTO user = getUser();
+        request.setAttribute("userId", user.getId());
         return "merchant/cloudCall/cloudCall";
     }
 
@@ -69,5 +73,16 @@ public class CallPackageController {
     public JSONResult<CallBuyPackageRes> list(@RequestParam Long userId) {
         log.info("CallPackageBuyController,list,userId={}", userId);
         return callPackageFeignClient.list(userId);
+    }
+
+    /**
+     * 获取当前登录账号
+     *
+     * @return
+     */
+    private UserInfoDTO getUser() {
+        Object attribute = SecurityUtils.getSubject().getSession().getAttribute("user");
+        UserInfoDTO user = (UserInfoDTO) attribute;
+        return user;
     }
 }
