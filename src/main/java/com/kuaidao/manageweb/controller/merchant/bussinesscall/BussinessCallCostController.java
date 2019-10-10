f11f6c3c4be5074b1ebad02e5e099e78f6c8963f
xuyunfeng <xuyunfeng@kd.cn>
2019-10-10 18:57:13 +0800

    商家通话费用查询

--- src/main/java/com/kuaidao/manageweb/controller/merchant/bussinesscall/BussinessCallCostController.java
+++ src/main/java/com/kuaidao/manageweb/controller/merchant/bussinesscall/BussinessCallCostController.java
@@ -1,9 +1,15 @@
 package com.kuaidao.manageweb.controller.merchant.bussinesscall;
 
+import com.kuaidao.account.dto.call.MerchantCallCostDTO;
+import com.kuaidao.account.dto.call.MerchantCallCostReq;
+import com.kuaidao.common.constant.SysErrorCodeEnum;
+import com.kuaidao.common.entity.JSONResult;
+import com.kuaidao.common.entity.PageBean;
 import javax.servlet.http.HttpServletRequest;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @description: BussinessCallCostController
@@ -27,5 +33,24 @@ public class BussinessCallCostController {
   public String initBussinessCallCost(HttpServletRequest request) {
 
     return "merchant/bussinessCall/chargeRecord";
+  }
+
+  /**
+   * @Description 商家端商家通话费用列表查询
+   * @param req
+   * @Return com.kuaidao.common.entity.JSONResult<com.kuaidao.common.entity.PageBean<com.kuaidao.account.dto.call.MerchantCallCostDTO>>
+   * @Author xuyunfeng
+   * @Date 2019/10/10 17:08
+   **/
+  @RequestMapping("/getBussinessCallCostList")
+  @ResponseBody
+  public JSONResult<PageBean<MerchantCallCostDTO>> getBussinessCallCostList(MerchantCallCostReq req){
+    try {
+
+
+      return new JSONResult<PageBean<MerchantCallCostDTO>>().success(null);
+    }catch (Exception e){
+      return new JSONResult<PageBean<MerchantCallCostDTO>>().fail(SysErrorCodeEnum.ERR_SYSTEM.getCode(),"获取商家端商家通话费用异常");
+    }
   }
 }
--- src/main/java/com/kuaidao/manageweb/controller/merchant/managecall/ManageCallCostController.java
+++ src/main/java/com/kuaidao/manageweb/controller/merchant/managecall/ManageCallCostController.java
@@ -1,9 +1,15 @@
 package com.kuaidao.manageweb.controller.merchant.managecall;
 
+import com.kuaidao.account.dto.call.MerchantCallCostDTO;
+import com.kuaidao.account.dto.call.MerchantCallCostReq;
+import com.kuaidao.common.constant.SysErrorCodeEnum;
+import com.kuaidao.common.entity.JSONResult;
+import com.kuaidao.common.entity.PageBean;
 import javax.servlet.http.HttpServletRequest;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @description: BussinessCallCostController
@@ -27,5 +33,24 @@ public class ManageCallCostController {
   public String initManageCallCost(HttpServletRequest request) {
 
     return "merchant/manageCall/manageChargeRecord";
+  }
+
+  /**
+  * @Description 管理端商家通话费用查询
+  * @param req
+  * @Return com.kuaidao.common.entity.JSONResult<com.kuaidao.common.entity.PageBean<com.kuaidao.account.dto.call.MerchantCallCostDTO>>
+  * @Author xuyunfeng
+  * @Date 2019/10/10 17:47
+  **/
+  @RequestMapping("/getManageCallCostList")
+  @ResponseBody
+  public JSONResult<PageBean<MerchantCallCostDTO>> getManageCallCostList(MerchantCallCostReq req){
+    try {
+
+
+      return new JSONResult<PageBean<MerchantCallCostDTO>>().success(null);
+    }catch (Exception e){
+      return new JSONResult<PageBean<MerchantCallCostDTO>>().fail(SysErrorCodeEnum.ERR_SYSTEM.getCode(),"获取管理端商家通话费用异常");
+    }
   }
 }
