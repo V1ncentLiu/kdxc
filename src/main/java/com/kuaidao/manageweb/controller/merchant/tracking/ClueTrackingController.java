f826bc410a30597631caeb2879ddfe5f4379a755
fanjd <fjd89@126.com>
2019-10-10 15:01:06 +0800

    增加字段

--- src/main/java/com/kuaidao/manageweb/controller/merchant/tracking/ClueTrackingController.java
+++ src/main/java/com/kuaidao/manageweb/controller/merchant/tracking/ClueTrackingController.java
@@ -1,9 +1,9 @@
 package com.kuaidao.manageweb.controller.merchant.tracking;
 
 import java.util.Date;
-
 import javax.validation.Valid;
-
+import com.kuaidao.manageweb.feign.merchant.tracking.TrackingFeignClient;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,27 +11,23 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.kuaidao.common.entity.JSONResult;
 import com.kuaidao.common.util.CommonUtil;
 import com.kuaidao.merchant.dto.tracking.TrackingInsertOrUpdateDTO;
-import com.kuaidao.merchant.entity.tracking.ClueTracking;
-import com.kuaidao.merchant.service.tracking.IClueTrackingService;
-import com.kuaidao.merchant.util.IdUtil;
+
 
 /**
  * 跟进记录
  * 
  * @author fanjd
  */
+@Slf4j
 @RestController
 @RequestMapping("/tracking")
 public class ClueTrackingController {
 
-    private static Logger logger = LoggerFactory.getLogger(ClueTrackingController.class);
-
     @Autowired
-    private IClueTrackingService<ClueTracking, Long> clueTrackingService;
+    private TrackingFeignClient trackingFeignClient;
 
     /**
      * 新增
@@ -41,12 +37,7 @@ public class ClueTrackingController {
         if (result.hasErrors()) {
             return CommonUtil.validateParam(result);
         }
-        logger.info("插入一条跟进记录");
-        dto.setId(IdUtil.getUUID());
-        dto.setCreateTime(new Date());
-        boolean b = clueTrackingService.saveTracking(dto);
-
-        return CommonUtil.comResult(b);
+        return trackingFeignClient.saveTracking(dto);
     }
 
 }
--- src/main/java/com/kuaidao/manageweb/feign/merchant/tracking/TrackingFeignClient.java
+++ src/main/java/com/kuaidao/manageweb/feign/merchant/tracking/TrackingFeignClient.java
@@ -1,47 +1,41 @@
 package com.kuaidao.manageweb.feign.merchant.tracking;
 
 import java.util.List;
-
+import com.kuaidao.merchant.dto.tracking.TrackingInsertOrUpdateDTO;
+import com.kuaidao.merchant.dto.tracking.TrackingReqDTO;
+import com.kuaidao.merchant.dto.tracking.TrackingRespDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.kuaidao.aggregation.dto.tracking.TrackingInsertOrUpdateDTO;
-import com.kuaidao.aggregation.dto.tracking.TrackingReqDTO;
-import com.kuaidao.aggregation.dto.tracking.TrackingRespDTO;
 import com.kuaidao.common.constant.SysErrorCodeEnum;
-import com.kuaidao.common.entity.IdListLongReq;
 import com.kuaidao.common.entity.JSONResult;
 
 
 /**
  *
- * 功能描述: 
- *      资源跟进记录
- * @auther  yangbiao
- * @date: 2019/1/8 17:35
+ * 功能描述: 资源跟进记录
+ * 
+ * @author fanjd
+ * @date: 2019/10/10 17:35
  */
-@FeignClient(name = "aggregation-service",path="/aggregation/tracking",fallback = TrackingFeignClient.HystrixClientFallback.class)
+@FeignClient(name = "merchant-service", path = "/merchant/tracking", fallback = TrackingFeignClient.HystrixClientFallback.class)
 public interface TrackingFeignClient {
 
     @RequestMapping("/insert")
-    public JSONResult<Boolean> saveTracking(@RequestBody TrackingInsertOrUpdateDTO dto);
+    JSONResult<Boolean> saveTracking(@RequestBody TrackingInsertOrUpdateDTO dto);
 
     @RequestMapping("/update")
-    public JSONResult<Boolean>  updateTracking(@RequestBody TrackingInsertOrUpdateDTO dto);
-
-    @RequestMapping("/delete")
-    public JSONResult<Boolean>  deleteTracking(@RequestBody IdListLongReq idListReq);
+    JSONResult<Boolean> updateTracking(@RequestBody TrackingInsertOrUpdateDTO dto);
 
     @RequestMapping("/queryList")
-    public JSONResult<List<TrackingRespDTO>> queryList(@RequestBody TrackingReqDTO dto);
+    JSONResult<List<TrackingRespDTO>> queryList(@RequestBody TrackingReqDTO dto);
 
 
     @Component
-    static class HystrixClientFallback implements TrackingFeignClient {
+    class HystrixClientFallback implements TrackingFeignClient {
 
         private static Logger logger = LoggerFactory.getLogger(HystrixClientFallback.class);
 
@@ -49,8 +43,7 @@ public interface TrackingFeignClient {
         @SuppressWarnings("rawtypes")
         private JSONResult fallBackError(String name) {
             logger.error(name + "接口调用失败：无法获取目标服务");
-            return new JSONResult().fail(SysErrorCodeEnum.ERR_REST_FAIL.getCode(),
-                    SysErrorCodeEnum.ERR_REST_FAIL.getMessage());
+            return new JSONResult().fail(SysErrorCodeEnum.ERR_REST_FAIL.getCode(), SysErrorCodeEnum.ERR_REST_FAIL.getMessage());
         }
 
 
@@ -62,11 +55,6 @@ public interface TrackingFeignClient {
         @Override
         public JSONResult<Boolean> updateTracking(TrackingInsertOrUpdateDTO dto) {
             return fallBackError("资源跟进记录-更新");
-        }
-
-        @Override
-        public JSONResult<Boolean> deleteTracking(IdListLongReq idListReq) {
-            return fallBackError("资源跟进记录-删除");
         }
 
         @Override
