7991bdc6e4e308357ee195fa36cedc79363383a4
houjunliang <houjunliang@kd.cn>
2019-10-11 18:06:35 +0800

    头像图片

--- src/main/java/com/kuaidao/manageweb/feign/merchant/seatmanager/SeatManagerFeignClient.java
+++ src/main/java/com/kuaidao/manageweb/feign/merchant/seatmanager/SeatManagerFeignClient.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 /**
  * Created on: 2019-09-23-17:39
  */
-@FeignClient(name = "callcenter-service-hou", path = "/callcenter/seatManager",
+@FeignClient(name = "callcenter-service", path = "/callcenter/seatManager",
     fallback = SeatManagerFeignClient.HystrixClientFallback.class)
 public interface SeatManagerFeignClient {
 
