48ca0d3db7765a6e95ca47d977a3e624a03b0c31
weiyanping <weiyanping@kuaidaoapp.com>
2019-10-22 14:45:27 +0800

    资源释放领取统计表

--- src/main/java/com/kuaidao/manageweb/controller/statistics/resourcesReport/FreedReportController.java
+++ src/main/java/com/kuaidao/manageweb/controller/statistics/resourcesReport/FreedReportController.java
@@ -1,13 +1,29 @@
 package com.kuaidao.manageweb.controller.statistics.resourcesReport;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.kuaidao.common.constant.DicCodeEnum;
+import com.kuaidao.common.entity.JSONResult;
+import com.kuaidao.common.util.ExcelUtil;
 import com.kuaidao.manageweb.controller.statistics.BaseStatisticsController;
 import com.kuaidao.manageweb.feign.statistics.resourceFreeReceive.ResourceFreeReceiveFeignClient;
+import com.kuaidao.stastics.dto.resourceFreeReceive.ResourceFreeReceiveDto;
+import com.kuaidao.stastics.dto.resourceFreeReceive.ResourceFreeReceiveQueryDto;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author: guhuitao
@@ -20,6 +36,11 @@ public class FreedReportController extends BaseStatisticsController {
 
     @Autowired
     private ResourceFreeReceiveFeignClient resourceFreeReceiveFeignClient;
+
+    private static final Integer GROUP = 1;
+    private static final Integer PERSON = 2;
+    private static final Integer DAY = 3;
+
 
 
     /**
@@ -65,6 +86,178 @@ public class FreedReportController extends BaseStatisticsController {
     }
 
 
+    /**
+     * 组分页
+     */
+    @RequestMapping("/getGroupPageList")
+    @ResponseBody
+    public JSONResult<Map<String,Object>> getGroupPageList(@RequestBody ResourceFreeReceiveQueryDto resourceFreeReceiveQueryDto){
+        return resourceFreeReceiveFeignClient.getGroupPageList(resourceFreeReceiveQueryDto);
+    }
+
+    /**
+     * 组不分页
+     */
+    @RequestMapping("/exportGroupAllList")
+    public void exportGroupAllList(@RequestBody ResourceFreeReceiveQueryDto resourceFreeReceiveQueryDto,HttpServletResponse response) throws IOException {
+        List<List<Object>> dataList = new ArrayList<List<Object>>();
+        dataList.add(getResourceFreedTitleList(GROUP));
+        JSONResult<Map<String, Object>> result = resourceFreeReceiveFeignClient.getGroupAllList(resourceFreeReceiveQueryDto);
+        Map<String, Object> dataMap = result.getData();
+        String listTxt = JSONArray.toJSONString(dataMap.get("tableData"));
+        List<ResourceFreeReceiveDto> orderList = JSON.parseArray(listTxt, ResourceFreeReceiveDto.class);
+        String totalDataStr = JSON.toJSONString(dataMap.get("totalData"));
+        //合计
+        ResourceFreeReceiveDto sumReadd = JSON.parseObject(totalDataStr, ResourceFreeReceiveDto.class);
+        //添加合计头
+        addTotalExportData(sumReadd,dataList,GROUP);
+        buildList(dataList, orderList,GROUP);
+        XSSFWorkbook wbWorkbook = ExcelUtil.creat2007Excel(dataList);
+        String name = "资源释放领取统计表" +resourceFreeReceiveQueryDto.getStartTime()+"-"+resourceFreeReceiveQueryDto.getEndTime() + ".xlsx";
+        response.addHeader("Content-Disposition",
+                "attachment;filename=" + new String(name.getBytes("UTF-8"), "ISO8859-1"));
+        response.addHeader("fileName", URLEncoder.encode(name, "utf-8"));
+        response.setContentType("application/octet-stream");
+        ServletOutputStream outputStream = response.getOutputStream();
+        wbWorkbook.write(outputStream);
+        outputStream.close();
+    }
+
+    /**
+     *
+     * 二级页面查询人（不分页）
+     */
+    @RequestMapping("/exportPersonAllList")
+    public void  exportPersonAllList(ResourceFreeReceiveQueryDto resourceFreeReceiveQueryDto,HttpServletResponse response) throws IOException {
+        List<List<Object>> dataList = new ArrayList<List<Object>>();
+        dataList.add(getResourceFreedTitleList(PERSON));
+        JSONResult<Map<String, Object>> result = resourceFreeReceiveFeignClient.getPersonAllList(resourceFreeReceiveQueryDto);
+        Map<String, Object> dataMap = result.getData();
+        String listTxt = JSONArray.toJSONString(dataMap.get("tableData"));
+        List<ResourceFreeReceiveDto> orderList = JSON.parseArray(listTxt, ResourceFreeReceiveDto.class);
+        String totalDataStr = JSON.toJSONString(dataMap.get("totalData"));
+        //合计
+        ResourceFreeReceiveDto sumReadd = JSON.parseObject(totalDataStr, ResourceFreeReceiveDto.class);
+        //添加合计头
+        addTotalExportData(sumReadd,dataList,PERSON);
+        buildList(dataList, orderList,PERSON);
+        XSSFWorkbook wbWorkbook = ExcelUtil.creat2007Excel(dataList);
+        String name = "资源释放领取统计表" +resourceFreeReceiveQueryDto.getStartTime()+"-"+resourceFreeReceiveQueryDto.getEndTime() + ".xlsx";
+        response.addHeader("Content-Disposition",
+                "attachment;filename=" + new String(name.getBytes("UTF-8"), "ISO8859-1"));
+        response.addHeader("fileName", URLEncoder.encode(name, "utf-8"));
+        response.setContentType("application/octet-stream");
+        ServletOutputStream outputStream = response.getOutputStream();
+        wbWorkbook.write(outputStream);
+    }
+    /**
+     *
+     * 二级页面查询人（分页）
+     */
+    @RequestMapping("/getPersonPageList")
+    @ResponseBody
+    public JSONResult<Map<String, Object>> getPersonPageList(ResourceFreeReceiveQueryDto resourceFreeReceiveQueryDto){
+        return resourceFreeReceiveFeignClient.getPersonPageList(resourceFreeReceiveQueryDto);
+    }
+    /**
+     *
+     * 三级页面查询人+天（不分页）
+     */
+    @RequestMapping("/exportPersonDayAllList")
+    public void  exportPersonDayAllList(ResourceFreeReceiveQueryDto resourceFreeReceiveQueryDto,HttpServletResponse response) throws IOException {
+        List<List<Object>> dataList = new ArrayList<List<Object>>();
+        dataList.add(getResourceFreedTitleList(DAY));
+        JSONResult<Map<String, Object>> result = resourceFreeReceiveFeignClient.getPersonDayAllList(resourceFreeReceiveQueryDto);
+        Map<String, Object> dataMap = result.getData();
+        String listTxt = JSONArray.toJSONString(dataMap.get("tableData"));
+        List<ResourceFreeReceiveDto> orderList = JSON.parseArray(listTxt, ResourceFreeReceiveDto.class);
+        String totalDataStr = JSON.toJSONString(dataMap.get("totalData"));
+        //合计
+        ResourceFreeReceiveDto sumReadd = JSON.parseObject(totalDataStr, ResourceFreeReceiveDto.class);
+        //添加合计头
+        addTotalExportData(sumReadd,dataList,DAY);
+        buildList(dataList, orderList,DAY);
+        XSSFWorkbook wbWorkbook = ExcelUtil.creat2007Excel(dataList);
+        String name = "资源释放领取统计表" +resourceFreeReceiveQueryDto.getStartTime()+"-"+resourceFreeReceiveQueryDto.getEndTime() + ".xlsx";
+        response.addHeader("Content-Disposition",
+                "attachment;filename=" + new String(name.getBytes("UTF-8"), "ISO8859-1"));
+        response.addHeader("fileName", URLEncoder.encode(name, "utf-8"));
+        response.setContentType("application/octet-stream");
+        ServletOutputStream outputStream = response.getOutputStream();
+        wbWorkbook.write(outputStream);
+    }
+    /**
+     *
+     * 三级页面查询人+天（分页）
+     */
+    @RequestMapping("/getPersonDayPageList")
+    @ResponseBody
+    public JSONResult<Map<String, Object>> getPersonDayPageList(ResourceFreeReceiveQueryDto resourceFreeReceiveQueryDto){
+        return resourceFreeReceiveFeignClient.getPersonDayPageList(resourceFreeReceiveQueryDto);
+    }
+
+    private List<Object> getResourceFreedTitleList(Integer type) {
+        List<Object> headTitleList = new ArrayList<>();
+        headTitleList.add("序号");
+        if(type.equals(GROUP)){
+            headTitleList.add("电销组");
+        }
+        if(type.equals(PERSON)){
+            headTitleList.add("电销顾问");
+        }
+        if(type.equals(DAY)){
+            headTitleList.add("日期");
+        }
+        headTitleList.add("不跟进释放公共资源数");
+        headTitleList.add("其他释放公共资源数");
+        headTitleList.add("无效释放数");
+        headTitleList.add("总释放数");
+        if(type.equals(GROUP)){
+            headTitleList.add("总监领取数");
+        }
+        headTitleList.add("顾问领取数");
+        if(type.equals(GROUP)){
+            headTitleList.add("总领取数");
+        }
+        headTitleList.add("释放/领取比例");
+        return headTitleList;
+    }
+
+    private void addTotalExportData(ResourceFreeReceiveDto ra, List<List<Object>> dataList, Integer type) {
+        List<Object> curList = new ArrayList<>();
+        curList.add("");
+        curList.add("");
+        curList.add(ra.getUnfollowCount());
+        curList.add(ra.getOtherCount());
+        curList.add(ra.getUselessCount());
+        curList.add(ra.getTotalFreeCount());
+        if(type.equals(GROUP)){
+            curList.add(ra.getTeleManagerCount());
+        }
+        curList.add(ra.getTeleSaleCount());
+        if(type.equals(GROUP)){
+            curList.add(ra.getTotalReceiveCount());
+        }
+        curList.add(ra.getFreeRate());
+        dataList.add(curList);
+    }
+
+    private void buildList(List<List<Object>> dataList, List<ResourceFreeReceiveDto> orderList, Integer type) {
+        for(int i = 0; i<orderList.size(); i++){
+            ResourceFreeReceiveDto ra = orderList.get(i);
+            List<Object> curList = new ArrayList<>();
+            curList.add(i + 1);
+            curList.add(ra.getUnfollowCount());
+            curList.add(ra.getOtherCount());
+            curList.add(ra.getUselessCount());
+            curList.add(ra.getTotalFreeCount());
+            curList.add(ra.getTeleManagerCount());
+            curList.add(ra.getTeleSaleCount());
+            curList.add(ra.getTotalReceiveCount());
+            curList.add(ra.getFreeRate());
+            dataList.add(curList);
+        }
+    }
 
 
 }
