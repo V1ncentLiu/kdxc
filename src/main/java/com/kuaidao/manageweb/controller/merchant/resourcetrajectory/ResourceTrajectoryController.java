9d156b490f917915ba016a8c584250d675cbccbd
yangbiao <923937157@qq.com>
2019-10-23 17:43:23 +0800

    tijiao

--- src/main/java/com/kuaidao/manageweb/controller/merchant/resourcetrajectory/ResourceTrajectoryController.java
+++ src/main/java/com/kuaidao/manageweb/controller/merchant/resourcetrajectory/ResourceTrajectoryController.java
@@ -15,6 +15,10 @@ import com.kuaidao.manageweb.feign.clue.ClueBasicFeignClient;
 import com.kuaidao.manageweb.feign.clue.ClueCustomerFeignClient;
 import com.kuaidao.manageweb.feign.clue.MyCustomerFeignClient;
 import com.kuaidao.manageweb.feign.log.ImLogMgtFeignClient;
+import com.kuaidao.manageweb.feign.merchant.clue.ClueInfoDetailFeignClient;
+import com.kuaidao.manageweb.feign.merchant.clue.ClueManagementFeignClient;
+import com.kuaidao.merchant.dto.clue.ClueManagementDto;
+import com.kuaidao.merchant.dto.clue.ClueManagementParamDto;
 import com.kuaidao.merchant.dto.pubcusres.ClueReceiveRecordsDTO;
 import com.kuaidao.merchant.dto.resourcetrajectory.ResourceTrajectoryDTO;
 import java.util.List;
@@ -44,6 +48,9 @@ public class ResourceTrajectoryController {
 
   @Autowired
   private MyCustomerFeignClient myCustomerFeignClient;
+
+  @Autowired
+  private ClueManagementFeignClient clueManagementFeignClient;
 
 
   /**
@@ -80,6 +87,17 @@ public class ResourceTrajectoryController {
     ClueQueryDTO queryDTO = new ClueQueryDTO();
     queryDTO.setClueId(clueId.getId());
     JSONResult<ClueDTO> clueInfo = myCustomerFeignClient.findClueInfo(queryDTO);
+    // 查询当前资源是否来源于共有池
+    ClueManagementParamDto reqDto = new ClueManagementParamDto();
+    reqDto.setClueId(clueId.getId());
+    JSONResult<List<ClueManagementDto>> listJSONResult = clueManagementFeignClient
+        .listNoPage(reqDto);
+    Integer cpoolReceiveFlag = 0;
+    if(CommonUtil.resultCheck(listJSONResult)){
+      List<ClueManagementDto> data = listJSONResult.getData();
+      ClueManagementDto clueManagementDto = data.get(0);
+      cpoolReceiveFlag = clueManagementDto.getCpoolReceiveFlag();
+    }
 
     if(CommonUtil.resultCheck(clueInfo)){
       ClueDTO data = clueInfo.getData();
@@ -88,6 +106,11 @@ public class ResourceTrajectoryController {
         resourceTrajectory.setAge(clueCustomer.getAge());
         resourceTrajectory.setCusName(clueCustomer.getCusName());
         resourceTrajectory.setEmail(clueCustomer.getEmail());
+        if(cpoolReceiveFlag!=null&&cpoolReceiveFlag==1){
+          resourceTrajectory.setPhone("***");
+        }else{
+          resourceTrajectory.setPhone(clueCustomer.getPhone());
+        }
         resourceTrajectory.setPhone(clueCustomer.getPhone());
         resourceTrajectory.setPhone2(clueCustomer.getPhone2());
         resourceTrajectory.setPhone3(clueCustomer.getPhone3());
