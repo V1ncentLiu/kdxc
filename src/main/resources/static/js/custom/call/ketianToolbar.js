ecc6a454deee2a73bd476d7e8bdb44ec7c0c2a6a
chenchengxue <chenchengxue@kd.cn>
2019-11-01 14:20:55 +0800

    科天坐席登录开发

---
+++ src/main/resources/static/js/custom/call/ketianToolbar.js
@@ -0,0 +1,9 @@
+
+var KetianClientToolBar= window.NameSpace || {};
+KetianClientToolBar.ketianClient = new function (){
+	var self=this;
+	self.hangup=function(){
+        CtiAgentBar.hangup();
+	};
+
+};
