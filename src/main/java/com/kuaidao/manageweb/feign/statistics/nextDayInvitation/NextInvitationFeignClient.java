bd6c495f3ab945572afafdbfaf82252876fa7895
guhuitao <a@123456>
2019-11-25 13:47:52 +0800

    次日邀约统计表

---
+++ src/main/java/com/kuaidao/manageweb/feign/statistics/nextDayInvitation/NextInvitationFeignClient.java
@@ -0,0 +1,71 @@
+package com.kuaidao.manageweb.feign.statistics.nextDayInvitation;
+
+
+import com.kuaidao.common.constant.SysErrorCodeEnum;
+import com.kuaidao.common.entity.JSONResult;
+import com.kuaidao.manageweb.feign.statistics.invitation.InvitationFeignClient;
+import com.kuaidao.stastics.dto.base.BaseQueryDto;
+import com.kuaidao.stastics.dto.invitation.InvitationDto;
+import com.kuaidao.stastics.dto.invitation.NextInvitationDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Map;
+
+@FeignClient(name="statstics-service3",path = "/statstics/nextInvitation",fallback = NextInvitationFeignClient.HystrixClientFallback.class)
+public interface NextInvitationFeignClient {
+
+
+    @RequestMapping("/queryPage")
+    public JSONResult<Map<String,Object>> queryByPage(@RequestBody BaseQueryDto baseQueryDto);
+
+
+    @RequestMapping("/queryList")
+    public JSONResult<List<NextInvitationDto>> queryListByParams(@RequestBody BaseQueryDto baseQueryDto);
+
+
+    @RequestMapping("/queryDeptPage")
+    public JSONResult<Map<String,Object>> queryDeptPage(@RequestBody BaseQueryDto baseQueryDto);
+
+
+    @RequestMapping("/queryDeptList")
+    public JSONResult<List<NextInvitationDto>> queryDeptList(@RequestBody BaseQueryDto baseQueryDto);
+
+    @Component
+    class HystrixClientFallback implements NextInvitationFeignClient {
+
+        private static Logger logger = LoggerFactory.getLogger(HystrixClientFallback.class);
+
+        private JSONResult fallBackError(String name) {
+            logger.error(name + "接口调用失败：无法获取目标服务");
+            return new JSONResult().fail(SysErrorCodeEnum.ERR_REST_FAIL.getCode(),
+                    SysErrorCodeEnum.ERR_REST_FAIL.getMessage());
+        }
+
+        @Override
+        public JSONResult<Map<String, Object>> queryByPage(BaseQueryDto baseQueryDto) {
+            return fallBackError("次日邀约统计表-分页查询");
+        }
+
+        @Override
+        public JSONResult<List<NextInvitationDto>> queryListByParams(BaseQueryDto baseQueryDto) {
+            return fallBackError("次日邀约统计表-导出excel查询");
+        }
+
+        @Override
+        public JSONResult<Map<String, Object>> queryDeptPage(BaseQueryDto baseQueryDto) {
+            return fallBackError("次日邀约统计表-电销组分页查询");
+        }
+
+        @Override
+        public JSONResult<List<NextInvitationDto>> queryDeptList(BaseQueryDto baseQueryDto) {
+            return fallBackError("次日邀约统计表-电销组导出excel查询");
+        }
+    }
+
+}
