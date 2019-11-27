05c0c07c6e92b610e310acde579925b95bd65dfa
guhuitao <a@123456>
2019-11-27 17:05:13 +0800

    服务名修改

--- src/main/java/com/kuaidao/manageweb/feign/statistics/nextDayInvitation/NextInvitationFeignClient.java
+++ src/main/java/com/kuaidao/manageweb/feign/statistics/nextDayInvitation/NextInvitationFeignClient.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 import java.util.Map;
 
-@FeignClient(name="statstics-service3",path = "/statstics/nextInvitation",fallback = NextInvitationFeignClient.HystrixClientFallback.class)
+@FeignClient(name="statstics-service",path = "/statstics/nextInvitation",fallback = NextInvitationFeignClient.HystrixClientFallback.class)
 public interface NextInvitationFeignClient {
 
 
