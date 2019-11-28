82a5c964740c0142a9eb0bc2b5c124f1d2e2fbca
zhangxingyu <408744797@qq.com>
2019-11-28 13:53:04 +0800

    signCreateTime

--- src/main/java/com/kuaidao/manageweb/feign/paydetail/PayDetailFeignClient.java
+++ src/main/java/com/kuaidao/manageweb/feign/paydetail/PayDetailFeignClient.java
@@ -1,7 +1,6 @@
 package com.kuaidao.manageweb.feign.paydetail;
 
 import java.util.List;
-import com.kuaidao.aggregation.dto.sign.BusSignInsertOrUpdateDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.netflix.feign.FeignClient;
@@ -9,17 +8,25 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.kuaidao.aggregation.dto.paydetail.PayDetailInsertOrUpdateDTO;
+import com.kuaidao.aggregation.dto.paydetail.PayDetailListDTO;
+import com.kuaidao.aggregation.dto.paydetail.PayDetailPageParam;
 import com.kuaidao.aggregation.dto.paydetail.PayDetailReqDTO;
 import com.kuaidao.aggregation.dto.paydetail.PayDetailRespDTO;
+import com.kuaidao.aggregation.dto.sign.BusSignInsertOrUpdateDTO;
 import com.kuaidao.common.constant.SysErrorCodeEnum;
 import com.kuaidao.common.entity.JSONResult;
+import com.kuaidao.common.entity.PageBean;
 
-@FeignClient(name = "aggregation-service", path = "/aggregation/paydetail", fallback = PayDetailFeignClient.HystrixClientFallback.class)
+@FeignClient(name = "aggregation-service-zhang", path = "/aggregation/paydetail",
+        fallback = PayDetailFeignClient.HystrixClientFallback.class)
 public interface PayDetailFeignClient {
 
 
     @RequestMapping("/queryList")
     JSONResult<List<PayDetailRespDTO>> queryList(@RequestBody PayDetailReqDTO dto);
+
+    @RequestMapping("/list")
+    JSONResult<PageBean<PayDetailListDTO>> list(@RequestBody PayDetailPageParam req);
 
     @RequestMapping("/insert")
     JSONResult<Boolean> savePayDedail(@RequestBody PayDetailInsertOrUpdateDTO dto);
@@ -33,12 +40,18 @@ public interface PayDetailFeignClient {
 
         private JSONResult fallBackError(String name) {
             logger.error(name + "接口调用失败：无法获取目标服务");
-            return new JSONResult().fail(SysErrorCodeEnum.ERR_REST_FAIL.getCode(), SysErrorCodeEnum.ERR_REST_FAIL.getMessage());
+            return new JSONResult().fail(SysErrorCodeEnum.ERR_REST_FAIL.getCode(),
+                    SysErrorCodeEnum.ERR_REST_FAIL.getMessage());
         }
 
         @Override
         public JSONResult<List<PayDetailRespDTO>> queryList(PayDetailReqDTO dto) {
             return fallBackError("付款详情");
+        }
+
+        @Override
+        public JSONResult<PageBean<PayDetailListDTO>> list(@RequestBody PayDetailPageParam req) {
+            return fallBackError("付款信息列表");
         }
 
         @Override
