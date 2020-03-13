e2f4cb15f0f034a6ca9928b54df846c1aeb874c5
fanjd <fjd89@126.com>
2020-03-13 10:42:34 +0800

    增加超成本fegin

--- src/main/java/com/kuaidao/manageweb/feign/financing/OverCostFeignClient.java
+++ src/main/java/com/kuaidao/manageweb/feign/financing/OverCostFeignClient.java
@@ -1,94 +1,66 @@
 package com.kuaidao.manageweb.feign.financing;
 
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
-import com.kuaidao.aggregation.dto.financing.ApplyRefundRebateReq;
-import com.kuaidao.aggregation.dto.financing.RefundRebateListDTO;
-import com.kuaidao.aggregation.dto.financing.RefundRebatePageParam;
+import com.kuaidao.aggregation.dto.financing.FinanceOverCostReqDto;
 import com.kuaidao.common.constant.SysErrorCodeEnum;
 import com.kuaidao.common.entity.JSONResult;
-import com.kuaidao.common.entity.PageBean;
+import lombok.extern.slf4j.Slf4j;
 
 /**
- * 退返款申请
+ * 超成本
  * 
- * @author: zxy
- * @date: 2019年1月4日
- * @version V1.0
- */
-@FeignClient(name = "aggregation-service", path = "/aggregation/refundRebate",
-        fallback = OverCostFeignClient.HystrixClientFallback.class)
+ * @author: Fanjd
+ * @param
+ * @return:
+ * @Date: 2020/3/13 10:22
+ * @since: 1.0.0
+ **/
+@FeignClient(name = "aggregation-service", path = "/aggregation//financing/overCost", fallback = OverCostFeignClient.HystrixClientFallback.class)
 public interface OverCostFeignClient {
 
     /**
-     * 退返款申请列表
+     * 超成本申请确认
      * 
-     * @param menuDTO
+     * @param
      * @return
      */
-    @PostMapping("/list")
-    public JSONResult<PageBean<RefundRebateListDTO>> list(@RequestBody RefundRebatePageParam param);
+    @PostMapping("/confirm")
+    JSONResult<String> confirm(@RequestBody FinanceOverCostReqDto reqDto);
 
     /**
-     * 关联签约单列表
-     * 
-     * @param menuDTO
+     * 超成本申请驳回
+     *
+     * @param
      * @return
      */
-    @PostMapping("/rebatesignList")
-    public JSONResult<List<RefundRebateListDTO>> rebatesignList(
-            @RequestBody RefundRebatePageParam param);
-
-
-    /**
-     * 餐饮公司申请退返款
-     * 
-     * @param idEntity
-     * @return
-     */
-    @PostMapping("/applyRefundRebate")
-    public JSONResult<Long> applyRefundRebate(@RequestBody ApplyRefundRebateReq req);
-
-
+    @PostMapping("/reject")
+    JSONResult<String> reject(@RequestBody FinanceOverCostReqDto reqDto);
 
     @Component
+    @Slf4j
     static class HystrixClientFallback implements OverCostFeignClient {
-
-        private static Logger logger = LoggerFactory.getLogger(OverCostFeignClient.class);
 
 
         private JSONResult fallBackError(String name) {
-            logger.error(name + "接口调用失败：无法获取目标服务");
-            return new JSONResult().fail(SysErrorCodeEnum.ERR_REST_FAIL.getCode(),
-                    SysErrorCodeEnum.ERR_REST_FAIL.getMessage());
+            log.error(name + "接口调用失败：无法获取目标服务");
+            return new JSONResult().fail(SysErrorCodeEnum.ERR_REST_FAIL.getCode(), SysErrorCodeEnum.ERR_REST_FAIL.getMessage());
         }
 
 
 
         @Override
-        public JSONResult<Long> applyRefundRebate(@RequestBody ApplyRefundRebateReq req) {
-            return fallBackError("餐饮公司申请退返款");
-        }
-
-
-        @Override
-        public JSONResult<PageBean<RefundRebateListDTO>> list(
-                @RequestBody RefundRebatePageParam param) {
-            return fallBackError("退返款申请列表");
+        public JSONResult<String> confirm(@RequestBody FinanceOverCostReqDto reqDto) {
+            return fallBackError("超成本申请确认");
         }
 
         @Override
-        public JSONResult<List<RefundRebateListDTO>> rebatesignList(
-                @RequestBody RefundRebatePageParam param) {
-            return fallBackError("关联签约单");
+        public JSONResult<String> reject(@RequestBody FinanceOverCostReqDto reqDto) {
+            return fallBackError("超成本申请驳回");
         }
+
 
 
     }
