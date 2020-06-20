4f6f87f94d32de13477c2b014db8cba71eb0e8fe
fanjiandong <fjd89@126.com>
2020-06-20 15:24:34 +0800

    优化路径

--- src/main/java/com/kuaidao/manageweb/controller/clue/ClueCustomerTelLogController.java
+++ src/main/java/com/kuaidao/manageweb/controller/clue/ClueCustomerTelLogController.java
@@ -1,15 +1,9 @@
 package com.kuaidao.manageweb.controller.clue;
 
-import com.kuaidao.aggregation.dto.clue.ClueCustomerTelLogDto;
-import com.kuaidao.aggregation.dto.clue.ClueCustomerTelLogReqDto;
-import com.kuaidao.common.constant.OrgTypeConstant;
-import com.kuaidao.common.entity.JSONResult;
-import com.kuaidao.common.entity.PageBean;
-import com.kuaidao.manageweb.feign.clue.ClueCustomerTelLogFeignClient;
-import com.kuaidao.manageweb.feign.organization.OrganizationFeignClient;
-import com.kuaidao.sys.dto.organization.OrganizationQueryDTO;
-import com.kuaidao.sys.dto.organization.OrganizationRespDTO;
-import com.kuaidao.sys.dto.user.UserInfoDTO;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.shiro.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,25 +12,31 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
+import com.kuaidao.aggregation.dto.clue.TelCreatePhoneLogDto;
+import com.kuaidao.aggregation.dto.clue.TelCreatePhoneLogReqDto;
+import com.kuaidao.common.constant.OrgTypeConstant;
+import com.kuaidao.common.entity.JSONResult;
+import com.kuaidao.common.entity.PageBean;
+import com.kuaidao.manageweb.feign.clue.TelCreatePhoneLogFeignClient;
+import com.kuaidao.manageweb.feign.organization.OrganizationFeignClient;
+import com.kuaidao.sys.dto.organization.OrganizationQueryDTO;
+import com.kuaidao.sys.dto.organization.OrganizationRespDTO;
+import com.kuaidao.sys.dto.user.UserInfoDTO;
 
 /**
  * 新建手机号查询
- *
  * @author fanjd
  */
 @Controller
-@RequestMapping(value = "/clueCustomerTelLog")
+@RequestMapping(value = "/telCreatePhoneLog")
 public class ClueCustomerTelLogController {
     @Autowired
     private OrganizationFeignClient organizationFeignClient;
     @Autowired
-    private ClueCustomerTelLogFeignClient clueCustomerTelLogFeignClient;
+    private TelCreatePhoneLogFeignClient telCreatePhoneLogFeignClient;
 
     /***
      * 初始化列表
-     *
      * @return
      */
     @RequestMapping("/initPage")
@@ -44,8 +44,7 @@ public class ClueCustomerTelLogController {
         // 电销组
         OrganizationQueryDTO organizationQueryDTO = new OrganizationQueryDTO();
         organizationQueryDTO.setOrgType(OrgTypeConstant.DXZ);
-        JSONResult<List<OrganizationRespDTO>> queryOrgByParam =
-                organizationFeignClient.queryOrgByParam(organizationQueryDTO);
+        JSONResult<List<OrganizationRespDTO>> queryOrgByParam = organizationFeignClient.queryOrgByParam(organizationQueryDTO);
         request.setAttribute("teleGroupList", queryOrgByParam.getData());
         return "searchNewPhone/searchNewPhone";
     }
@@ -55,16 +54,15 @@ public class ClueCustomerTelLogController {
      */
     @ResponseBody
     @PostMapping("/queryPageList")
-    public JSONResult<PageBean<ClueCustomerTelLogDto>> queryPageList(@RequestBody ClueCustomerTelLogReqDto reqDTO) {
+    public JSONResult<PageBean<TelCreatePhoneLogDto>> queryPageList(@RequestBody TelCreatePhoneLogReqDto reqDTO) {
         UserInfoDTO user = getUser();
         reqDTO.setBusinessLine(user.getBusinessLine());
-        JSONResult<PageBean<ClueCustomerTelLogDto>> jsonResult = clueCustomerTelLogFeignClient.queryPageList(reqDTO);
+        JSONResult<PageBean<TelCreatePhoneLogDto>> jsonResult = telCreatePhoneLogFeignClient.queryPageList(reqDTO);
         return jsonResult;
     }
 
     /**
      * 获取当前登录账号
-     *
      * @param
      * @return
      */
