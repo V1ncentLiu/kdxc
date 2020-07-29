45a75eaec75daa1646a8b08cdb0927bf44df5fac
yangxin <yangxin@kuaidaoapp.com>
2020-07-29 13:54:57 +0800

    bug修改

--- src/main/resources/static/js/custom/homepage/index.js
+++ src/main/resources/static/js/custom/homepage/index.js
@@ -1500,21 +1500,20 @@ var homePageVM=new Vue({
       openConsolePageCm(){//点击控制台button 事件
           this.defaultActive= null;
           $('.menu').css("color","rgb(255, 255, 255)");
-          var dataUrl=""
-          if(user){
-            var roleCode=user.roleCode;
-            console.log(roleCode);
-            if(roleCode=="DXCYGW"){//电销顾问
-              this.isRoleCodeDX=true;//电销顾问
-              dataUrl = "/console/console/index?sourceType=1";
-            }else if(roleCode=="SWJL"){
-              this.isRoleCodeSW=true;//商务经理
-              dataUrl = "/console/console/index?sourceType=1";
-            }else{
-              dataUrl = "/console/console/index?type=1";
-            }
-          }
-          // var dataUrl = "/console/console/index?type=1";
+          // var dataUrl=""
+          // if(user){
+          //   var roleCode=user.roleCode;
+          //   console.log(roleCode);
+          //   if(roleCode=="DXCYGW"){//电销顾问
+          //     this.isRoleCodeDX=true;//电销顾问
+          //     dataUrl = "/console/console/index?sourceType=1";
+          //   }else if(roleCode=="SWJL"){
+          //     this.isRoleCodeSW=true;//商务经理
+          //     dataUrl = "/console/console/index?sourceType=1";
+          //   }else{
+          //     dataUrl = "/console/console/index?type=1";
+          //   }
+          // }
           // $("#iframeBox").attr({
           //   "src":dataUrl //设置ifream地址
           // });
