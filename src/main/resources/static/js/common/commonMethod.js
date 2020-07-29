4b02953f1aad7d27291985bfa9f15179bced4c38
yangxin <yangxin@kuaidaoapp.com>
2020-07-29 16:38:53 +0800

    餐盟去掉面包屑

--- src/main/resources/static/js/common/commonMethod.js
+++ src/main/resources/static/js/common/commonMethod.js
@@ -53,4 +53,11 @@ function getQueryString(name){//获取url地址栏参数的方法
     var r = window.location.search.substr(1).match(reg);
     if(r!=null)return  unescape(r[2]); return null;
 }
- 
+// 餐盟页面不显示面包屑
+var canment = window.localStorage.getItem('union');
+if(canment=="unionStorage"){
+    var elBreadcrumb=document.getElementsByClassName('elBreadcrumb');
+    if(elBreadcrumb.length!=0){
+        elBreadcrumb[0].style.display="none";
+    }
+}
