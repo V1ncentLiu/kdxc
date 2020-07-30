3585c770d6cb8d1ec1c53daf0bfd263b83382fc3
jxs <1157561068@qq.com>
2020-07-30 18:12:50 +0800

    权限加入

--- src/main/java/com/kuaidao/manageweb/controller/sys/setting/SysSettingController.java
+++ src/main/java/com/kuaidao/manageweb/controller/sys/setting/SysSettingController.java
@@ -7,6 +7,7 @@ import com.kuaidao.manageweb.feign.user.SysSettingFeignClient;
 import com.kuaidao.sys.dto.user.SysSettingDTO;
 import com.kuaidao.sys.dto.user.SysSettingReq;
 import org.apache.commons.lang.StringUtils;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,6 +28,7 @@ public class SysSettingController {
     private SysSettingFeignClient sysSettingFeignClient;
 
     /**系统跳转*/
+    @RequiresPermissions("sys:setting:view")
     @RequestMapping("/index")
     public String page() {
         return "sys/setting/index";
@@ -37,6 +39,7 @@ public class SysSettingController {
      * @param sysSettingReq
      * @return
      */
+    @RequiresPermissions("sys:setting:view")
     @RequestMapping("/querySysSettingByPage")
     public @ResponseBody JSONResult<PageBean<SysSettingDTO>> querySysSettingByPage(@RequestBody SysSettingReq sysSettingReq){
         return sysSettingFeignClient.querySysSettingByPage(sysSettingReq) ;
@@ -47,6 +50,7 @@ public class SysSettingController {
      * @param map
      * @return
      */
+    @RequiresPermissions("sys:setting:delete")
     @RequestMapping("/deleteSysSettingByCode")
     public @ResponseBody JSONResult<Boolean> deleteSysSettingByCode(@RequestBody Map<String,String> map) throws IllegalAccessException {
         String codes = map.get("codes");
@@ -61,6 +65,7 @@ public class SysSettingController {
      * @param sysSettingReq
      * @return
      */
+    @RequiresPermissions("sys:setting:view")
     @RequestMapping("/findSysSettingByCode")
     public @ResponseBody JSONResult<SysSettingDTO> findSysSettingByCode(@RequestBody SysSettingReq sysSettingReq){
         return sysSettingFeignClient.findSysSettingByCode(sysSettingReq) ;
@@ -72,6 +77,7 @@ public class SysSettingController {
      * @param sysSettingReq
      * @return
      */
+    @RequiresPermissions("sys:setting:edit")
     @RequestMapping("/updateSysSettingByCode")
     public @ResponseBody JSONResult<Boolean> updateSysSettingByCode(@RequestBody SysSettingReq sysSettingReq){
         if(null == sysSettingReq || StringUtils.isBlank(sysSettingReq.getCode())){
@@ -85,6 +91,7 @@ public class SysSettingController {
      * @param sysSettingReq
      * @return
      */
+    @RequiresPermissions("sys:setting:add")
     @RequestMapping("/addSysSetting")
     public @ResponseBody JSONResult<Boolean> addSysSetting(@RequestBody SysSettingReq sysSettingReq){
 
