f216a5e5db12ae51f5fd856e4711b017bf62e304
houjunliang <qwe123!@#>
2020-09-07 18:42:01 +0800

    本地提交

---
+++ src/main/resources/static/im/js/module/friend.js
@@ -0,0 +1,321 @@
+/*
+* 好友相关
+*/
+
+'use strict'
+
+YX.fn.friend = function () {
+    //添加好友
+    this.$addFriend = $('#addFriend')
+    this.$addFriendBox = $('#addFriendBox')
+    this.$addFriend.on('click', this.showAddFriend.bind(this))
+    this.$addFriendBox.delegate('.j-close', 'click', this.hideAddFriend.bind(this))
+    this.$addFriendBox.delegate('.j-search', 'click', this.searchFriend.bind(this))
+    this.$addFriendBox.delegate('.j-back', 'click', this.resetSearchFriend.bind(this))
+    this.$addFriendBox.delegate('.j-add', 'click', this.addFriend.bind(this))
+    this.$addFriendBox.delegate('.j-blacklist', 'click', this.rmBlacklist.bind(this))
+    this.$addFriendBox.delegate('.j-chat', 'click', this.beginChat.bind(this))
+    this.$addFriendBox.delegate('.j-account', 'keydown', this.inputAddFriend.bind(this))
+    //黑名单
+    this.$blacklist = $('#blacklist')
+    $("#showBlacklist").on('click', this.showBlacklist.bind(this))
+    this.$blacklist.delegate('.j-close', 'click', this.hideBlacklist.bind(this))
+    this.$blacklist.delegate('.items .j-rm', 'click', this.removeFromBlacklist.bind(this))
+    //我的手机
+    $("#myPhone").on('click', this.sendToMyPhone.bind(this))
+
+    $('#searchListInputFriends').on('input', this.searchListInputFriendsOnmouse.bind(this)())
+    $('#inputSearchImgFriends').on('click', this.inputSearchImgFriendsClick.bind(this))
+}
+
+YX.fn.searchListInputFriendsOnmouse = function () {
+    let timer = null
+    var that = this
+    return function () {
+        if (timer) clearTimeout(timer)
+        timer = setTimeout(() => {
+            if (!$('#searchListInputFriends').val()) {
+                $("#inputSearchImgFriends").attr('src', 'images/newImages/fada@3x.png');
+            } else {
+                $("#inputSearchImgFriends").attr('src', 'images/newImages/qx@3x.png');
+            }
+            that.buildFriends()
+        }, 800)
+    }
+}
+
+YX.fn.inputSearchImgFriendsClick = function () {
+    $("#searchListInputFriends").val('');
+    $("#inputSearchImgFriends").attr('src', 'images/newImages/fada@3x.png');
+    this.buildFriends()
+}
+
+
+/**
+ * 通讯录列表显示
+ * @param  {object} 数据对象
+ * @return {void}
+ */
+
+YX.fn.friendListConcat = function () {
+    var array = [
+        {
+            brandName: '本宫的茶',
+            clueId: '1111212121',    //线索ID
+            isSubmit: 1,
+            account: "p_93640717727557801",    //imId
+            time: '06-16'
+        },
+        {
+            brandName: '本宫的茶1',
+            clueId: '1111212121',    //线索ID
+            isSubmit: 1,
+            account: "houjunliang",    //imId
+            time: '06-16'
+        },
+        {
+            brandName: '本宫的茶1',
+            clueId: '1111212121',    //线索ID
+            isSubmit: 1,
+            account: "houjunliang111",    //imId
+            time: '06-16'
+        },
+        {
+            brandName: '本宫的茶2',
+            clueId: '1111212121',    //线索ID
+            isSubmit: 1,
+            account: "weichen9694@126.com",    //imId
+            time: '06-16'
+        },
+        {
+            brandName: '本宫的茶2',
+            clueId: '1111212121',    //线索ID
+            isSubmit: 1,
+            account: "houjunliang222",    //imId
+            time: '06-16'
+        },{
+            brandName: '本宫的茶2',
+            clueId: '1111212121',    //线索ID
+            isSubmit: 1,
+            account: "houjunliang333",    //imId
+            time: '06-16'
+        },{
+            brandName: '本宫的茶2',
+            clueId: '1111212121',    //线索ID
+            isSubmit: 1,
+            account: "houjunliang444",    //imId
+            time: '06-16'  //imId
+          },{
+            brandName: '本宫的茶2',
+            clueId: '1111212121',    //线索ID
+            isSubmit: 1,
+            account: "houjunliang555",    //imId
+            time: '06-16'  //imId
+          },{
+            brandName: '本宫的茶2',
+            clueId: '1111212121',    //线索ID
+            isSubmit: 1,
+            account: "18501903418",    //imId
+            time: '06-16'  //imId
+          }
+    ]  
+    
+    var param={};
+    // param.teleSaleId=sessionStorage.getItem('userId');
+    param.teleSaleId=userId;
+    axios.post("/im/submitCustList", param)
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+            console.log(error);
+    });
+
+    var allData = this.cache.getFriendslistOnShow()
+    var newArray = []
+    for (let i = 0; i < allData.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (allData[i].account == array[j].account) {
+                newArray.push(Object.assign(allData[i], array[j]))
+            }
+        }
+    }
+    return newArray && newArray
+}
+
+
+
+YX.fn.buildFriends = function () {
+    var allData = this.friendListConcat() || []
+    var searchListInputVal = $('#searchListInputFriends').val()
+    var newArray = []
+    if (searchListInputVal) {
+        for (let i = 0; i < allData.length; i++) {
+            if (allData[i].nick.indexOf(searchListInputVal) > -1) {
+                newArray.push(allData[i])
+            }
+        }
+    }
+    var data = {
+        friends: searchListInputVal ? newArray : allData,
+        personSubscribes: this.cache.getPersonSubscribes(),
+        account: userUID
+    }
+    console.log(data, 'friends列表数据+请求接口拿到的数据');
+    if (!this.friends) {
+        var options = {
+            data: data,
+            onclickavatar: this.showInfo.bind(this),
+            onclickitem: this.openChatBox.bind(this),
+            infoprovider: this.infoProvider.bind(this)
+        }
+        this.friends = new NIMUIKit.FriendList(options)
+        this.friends.inject($('#friends').get(0))
+    } else {
+        this.friends.update(data)
+    }
+    this.doPoint()
+}
+
+
+/**
+ * 添加好友相关
+ * 
+ */
+YX.fn.showAddFriend = function () {
+    this.friendData = null
+    this.$addFriendBox.removeClass("hide")
+    this.$mask.removeClass('hide')
+    this.$addFriendBox.find(".j-account").focus()
+}
+YX.fn.hideAddFriend = function () {
+    this.resetSearchFriend()
+    this.$addFriendBox.addClass("hide")
+    this.$mask.addClass('hide')
+}
+YX.fn.searchFriend = function () {
+    var account = $.trim(this.$addFriendBox.find(".j-account").val().toLowerCase())
+    if (account !== "") {
+        this.mysdk.getUser(account, this.cbGetUserInfo.bind(this))
+    }
+}
+YX.fn.beginChat = function () {
+    var account = $.trim(this.$addFriendBox.find(".j-account").val().toLowerCase())
+    this.hideAddFriend()
+    this.openChatBox(account, "p2p")
+}
+YX.fn.resetSearchFriend = function () {
+    this.$addFriendBox.attr('class', "m-dialog")
+    this.$addFriendBox.find(".j-account").val("")
+}
+YX.fn.addFriend = function () {
+    var id = $.trim(this.$addFriendBox.find(".j-account").val().toLowerCase())
+    this.mysdk.addFriend(id, this.cbAddFriend.bind(this))
+}
+YX.fn.rmBlacklist = function () {
+    var id = $.trim(this.$addFriendBox.find(".j-account").val().toLowerCase())
+    this.mysdk.markInBlacklist(id, false, this.cbRmBlacklist.bind(this))
+}
+YX.fn.cbRmBlacklist = function (err, data) {
+    if (!err) {
+        var account = data.account
+        this.cache.removeFromBlacklist(account)
+        this.buildFriends()
+        var isFriend = this.cache.isFriend(account)
+        if (!isFriend) {
+            this.friendData = data
+        }
+        this.$addFriendBox.removeClass('blacklist')
+        this.$addFriendBox.addClass(isFriend ? "friend" : "noFriend")
+    }
+}
+YX.fn.inputAddFriend = function (evt) {
+    if (evt.keyCode == 13) {
+        $("#addFriendBox .j-account").blur()
+        this.searchFriend()
+    }
+}
+YX.fn.cbAddFriend = function (error, params) {
+    if (!error) {
+        this.$addFriendBox.find(".tip").html("添加好友成功！")
+        this.$addFriendBox.attr('class', "m-dialog done")
+        this.cache.addFriend(params.friend)
+        this.cache.updatePersonlist(this.friendData)
+        this.buildFriends()
+        // 订阅好友登录事件
+        this.mysdk.subscribeMultiPortEvent([params.account])
+    } else {
+        this.$addFriendBox.find(".tip").html("该帐号不存在，请检查你输入的帐号是否正确")
+        this.$addFriendBox.attr('class', "m-dialog done")
+    }
+
+}
+YX.fn.cbGetUserInfo = function (err, data) {
+    if (err) {
+        alert(err)
+    }
+    if (!!data) {
+        var $info = this.$addFriendBox.find(".info"),
+            user = data,
+            account = data.account
+        $info.find("img").attr("src", getAvatar(user.avatar))
+        $info.find(".j-nickname").html(user.nick)
+        $info.find(".j-username").html("帐号：" + account)
+        if (account == userUID) {
+            this.$addFriendBox.find(".tip").html("别看了就是你自己")
+            this.$addFriendBox.attr('class', "m-dialog done")
+        } else {
+            var isFriend = this.cache.isFriend(account),
+                inBlacklist = this.cache.inBlacklist(account)
+            if (!isFriend) {
+                this.friendData = data
+            }
+            if (inBlacklist) {
+                this.$addFriendBox.addClass("blacklist")
+            } else {
+                this.$addFriendBox.addClass(isFriend ? "friend" : "noFriend")
+            }
+        }
+
+    } else {
+        this.$addFriendBox.find(".tip").html("该帐号不存在，请检查你输入的帐号是否正确")
+        this.$addFriendBox.attr('class', "m-dialog done")
+    }
+}
+/************************
+ * 黑名单列表相关
+ ************************/
+YX.fn.showBlacklist = function () {
+    var data = this.cache.getBlacklist()
+    var html = appUI.buildBlacklist(data, this.cache)
+    this.$blacklist.find('.list').html(html)
+    this.$blacklist.removeClass('hide')
+    this.$mask.removeClass("hide")
+    document.documentElement.style.overflow = 'hidden'
+}
+YX.fn.hideBlacklist = function () {
+    $("#blacklist").addClass('hide')
+    this.$mask.addClass("hide")
+    document.documentElement.style.overflow = ''
+},
+    YX.fn.removeFromBlacklist = function (evt) {
+        var id = $(evt.target).attr("data-id")
+        this.mysdk.markInBlacklist(id, false, this.cbRemoveFromBlacklist.bind(this))
+    }
+YX.fn.cbRemoveFromBlacklist = function (err, data) {
+    if (!err) {
+        this.cache.removeFromBlacklist(data.account)
+        var html = appUI.buildBlacklist(this.cache.getBlacklist(), this.cache)
+        this.$blacklist.find('.list').html(html)
+        this.buildFriends()
+    } else {
+        alert("操作失败")
+    }
+}
+
+/**
+ * 我的手机
+ */
+YX.fn.sendToMyPhone = function () {
+    this.openChatBox(userUID, "p2p")
+}
