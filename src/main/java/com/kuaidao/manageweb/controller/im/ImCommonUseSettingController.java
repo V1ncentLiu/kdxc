ef6556432f25ada163ee3d0a174791f0fcd89665
fengyx <18734904593@163>
2020-09-07 13:24:03 +0800

    自动提交相关

--- src/main/java/com/kuaidao/manageweb/controller/im/ImCommonUseSettingController.java
+++ src/main/java/com/kuaidao/manageweb/controller/im/ImCommonUseSettingController.java
@@ -1,7 +1,6 @@
 package com.kuaidao.manageweb.controller.im;
 
 
-
 import com.kuaidao.common.entity.JSONResult;
 import com.kuaidao.custservice.constant.CommonLanguageTypeEnum;
 import com.kuaidao.custservice.dto.autoresponse.AutoResponseReqDto;
@@ -20,6 +19,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.annotation.Resource;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -34,9 +34,9 @@ import java.util.stream.Collectors;
 public class ImCommonUseSettingController {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Autowired
+    @Resource
     private AutoResponseFeignClient autoResponseFeignClient;
-    @Autowired
+    @Resource
     private CommonLanguageFeignClient commonLanguageFeignClient;
 
     /***
@@ -47,19 +47,19 @@ public class ImCommonUseSettingController {
     @RequestMapping("/imCommonUseSitting/index")
     public String imCommonUseSittingIndex(ModelMap modelMap) {
         //1、查询常用语
-        CommonLanguageReqDto commonLanguageReqDto  = new CommonLanguageReqDto();
+        CommonLanguageReqDto commonLanguageReqDto = new CommonLanguageReqDto();
         commonLanguageReqDto.setType(CommonLanguageTypeEnum.用户常用语.getCode());
         JSONResult<List<CommonLanguageRespDto>> commonLanguageResult = commonLanguageFeignClient.queryListByType(commonLanguageReqDto);
         if (commonLanguageResult != null && JSONResult.SUCCESS.equals(commonLanguageResult.getCode()) && commonLanguageResult.getData() != null) {
             List<CommonLanguageRespDto> commonLanguageRespDtoList = commonLanguageResult.getData();
-                modelMap.put("userLangList",commonLanguageRespDtoList);
+            modelMap.put("userLangList", commonLanguageRespDtoList);
         }
 
         commonLanguageReqDto.setType(CommonLanguageTypeEnum.顾问常用语.getCode());
         JSONResult<List<CommonLanguageRespDto>> languageResult = commonLanguageFeignClient.queryListByType(commonLanguageReqDto);
         if (languageResult != null && JSONResult.SUCCESS.equals(languageResult.getCode()) && languageResult.getData() != null) {
             List<CommonLanguageRespDto> commonLanguageRespDtoList = languageResult.getData();
-            modelMap.put("saleLangList",commonLanguageRespDtoList);
+            modelMap.put("saleLangList", commonLanguageRespDtoList);
         }
         //2、查询自动提交
         AutoResponseReqDto dto = new AutoResponseReqDto();
@@ -70,23 +70,21 @@ public class ImCommonUseSettingController {
             autoResponseMap = ListUtils.emptyIfNull(autoResponseRespDtoList).stream()
                     .filter(e -> e != null)
                     .collect(Collectors.groupingBy(AutoResponseRespDto::getType));
-            if(autoResponseMap.size()>0){
+            if (autoResponseMap.size() > 0) {
                 List<AutoResponseRespDto> kaichangList = autoResponseMap.get(1);
                 List<AutoResponseRespDto> timeoutList = autoResponseMap.get(2);
                 List<AutoResponseRespDto> noWorkList = autoResponseMap.get(3);
                 List<AutoResponseRespDto> lixianList = autoResponseMap.get(4);
-                modelMap.put("kaichangList",kaichangList);
-                modelMap.put("timeOut", CollectionUtils.isNotEmpty(timeoutList)?timeoutList.get(0):null);
-                modelMap.put("noWork",CollectionUtils.isNotEmpty(noWorkList)?noWorkList.get(0):null);
-                modelMap.put("lixian",CollectionUtils.isNotEmpty(lixianList)?lixianList.get(0):null);
+                modelMap.put("kaichangList", kaichangList);
+                modelMap.put("timeOut", CollectionUtils.isNotEmpty(timeoutList) ? timeoutList.get(0) : null);
+                modelMap.put("noWork", CollectionUtils.isNotEmpty(noWorkList) ? noWorkList.get(0) : null);
+                modelMap.put("lixian", CollectionUtils.isNotEmpty(lixianList) ? lixianList.get(0) : null);
             }
         } else {
             logger.error("AutoResponseController imCommonUseSittingIndex tree,res{{}}", listJSONResult);
         }
         return "im/imCommonUseSitting";
     }
-
-
 
 
 }
