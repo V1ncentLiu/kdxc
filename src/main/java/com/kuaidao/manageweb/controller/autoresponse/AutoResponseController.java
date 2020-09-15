1d35a58e5fc8f9c347bb658d1b8fccfcf0d0d28f
fengyx <18734904593@163>
2020-09-15 14:29:38 +0800

    自动提交代码判断删除

--- src/main/java/com/kuaidao/manageweb/controller/autoresponse/AutoResponseController.java
+++ src/main/java/com/kuaidao/manageweb/controller/autoresponse/AutoResponseController.java
@@ -68,52 +68,6 @@ public class AutoResponseController {
         if (autoResponseReq.getLixian() == null) {
             return new JSONResult<Boolean>().fail("-1", "请设置离线信息");
         }
-        List<AutoResponseReqDto> kaiChangList = autoResponseReq.getKaiChangList();
-        for (int i = 0; i < kaiChangList.size(); i++) {
-            AutoResponseReqDto autoResponseReqDto = kaiChangList.get(i);
-            if (StringUtils.isBlank(autoResponseReqDto.getContent())) {
-                return new JSONResult<Boolean>().fail("-1", "开场白第" + i + 1 + "条，内容不能为空！");
-            }
-            if (autoResponseReqDto.getContent().length() > 500) {
-                return new JSONResult<Boolean>().fail("-1", "开场白第" + i + 1 + "条，内容长度不能超过500！");
-            }
-        }
-
-        if (StringUtils.isBlank(autoResponseReq.getTimeOut().getContent())) {
-            return new JSONResult<Boolean>().fail("-1", "超时回复内容不能为空！");
-        }
-        if (autoResponseReq.getTimeOut().getContent().length() > 500) {
-            return new JSONResult<Boolean>().fail("-1", "超时回复内容长度不能超过500！");
-        }
-
-        if (autoResponseReq.getTimeOut().getTimeOutEnable() == null) {
-            return new JSONResult<Boolean>().fail("-1", "请设置超时回复启用状态！");
-        }
-        if (autoResponseReq.getTimeOut().getTimeOut() == null) {
-            return new JSONResult<Boolean>().fail("-1", "请填写超时自动回复时间！");
-        }
-
-
-        if (StringUtils.isBlank(autoResponseReq.getNonWorkTime().getContent())) {
-            return new JSONResult<Boolean>().fail("-1", "非工作时间自动回复内容不能为空！");
-        }
-        if (autoResponseReq.getNonWorkTime().getContent().length() > 500) {
-            return new JSONResult<Boolean>().fail("-1", "非工作时间自动回复内容长度不能超过500！");
-        }
-
-        if (autoResponseReq.getNonWorkTime().getNonWorktimeEnable() == null) {
-            return new JSONResult<Boolean>().fail("-1", "请设置非工作时间自动回复启用状态！");
-        }
-        if (autoResponseReq.getNonWorkTime().getStartTime() == null) {
-            return new JSONResult<Boolean>().fail("-1", "非工作时间段-开始时间！");
-        }
-        if (autoResponseReq.getNonWorkTime().getEndTime() == null) {
-            return new JSONResult<Boolean>().fail("-1", "非工作时间段-结束时间！");
-        }
-
-        if (autoResponseReq.getLixian().getContent() == null) {
-            return new JSONResult<Boolean>().fail("-1", "离线、忙碌回复内容不能为空");
-        }
         return new JSONResult<Boolean>().success(true);
     }
 
