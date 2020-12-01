4cedbf8b6de34adaafbc349e55a66dd47802edd4
jinxuesong <jinxuesong@kd.cn>
2020-12-01 11:31:10 +0800

    asyn加入

---
+++ src/main/java/com/kuaidao/manageweb/config/ThreadTaskPoolConfig.java
@@ -0,0 +1,31 @@
+package com.kuaidao.manageweb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @author admin
+ */
+@Configuration
+public class ThreadTaskPoolConfig {
+
+    @Bean("taskExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(Runtime.getRuntime().availableProcessors());
+        executor.setMaxPoolSize(Runtime.getRuntime().availableProcessors() * 2 );
+        executor.setQueueCapacity(200);
+        executor.setKeepAliveSeconds(60);
+        executor.setThreadNamePrefix("taskExecutor-");
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        // 设置线程池关闭的时候等待所有任务都完成再继续销毁其他的Bean
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        // 设置线程池中任务的等待时间，如果超过这个时候还没有销毁就强制销毁，以确保应用最后能够被关闭，而不是阻塞住
+        executor.setAwaitTerminationSeconds(60);
+        return executor;
+    }
+}
--- src/main/java/com/kuaidao/manageweb/service/impl/LogServiceImpl.java
+++ src/main/java/com/kuaidao/manageweb/service/impl/LogServiceImpl.java
@@ -1,13 +1,12 @@
 package com.kuaidao.manageweb.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Service;
-
 import com.kuaidao.common.entity.JSONResult;
 import com.kuaidao.logmgt.dto.AccessLogReqDTO;
 import com.kuaidao.manageweb.feign.log.LogMgtFeignClient;
 import com.kuaidao.manageweb.service.LogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
 
 
 @Service
@@ -15,7 +14,7 @@ public class LogServiceImpl  implements LogService {
 	@Autowired
 	LogMgtFeignClient logMgtFeignClient;
 
-	@Async
+	@Async("taskExecutor")
 	@Override
 	public JSONResult insertLogRecord(AccessLogReqDTO logReqDTO) {
 		return logMgtFeignClient.insertLogRecord(logReqDTO);
