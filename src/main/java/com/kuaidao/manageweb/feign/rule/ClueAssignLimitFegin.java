a0325f18f19fce5f80938d1dbde0bbc87e0237e2
fanjiandong <fjd89@126.com>
2021-01-11 14:51:10 +0800

    增加资源分配量删除

--- src/main/java/com/kuaidao/manageweb/feign/rule/ClueAssignLimitFegin.java
+++ src/main/java/com/kuaidao/manageweb/feign/rule/ClueAssignLimitFegin.java
@@ -1,79 +1,75 @@
 package com.kuaidao.manageweb.feign.rule;
 
+import com.kuaidao.businessconfig.dto.clueassign.ClueAssignLimitDTO;
+import com.kuaidao.businessconfig.dto.clueassign.ClueAssignLimitPageParam;
+import com.kuaidao.businessconfig.dto.clueassign.ClueAssignLimitReq;
+import com.kuaidao.common.constant.SysErrorCodeEnum;
+import com.kuaidao.common.entity.IdEntityLong;
+import com.kuaidao.common.entity.IdListLongReq;
+import com.kuaidao.common.entity.JSONResult;
+import com.kuaidao.common.entity.PageBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import com.kuaidao.businessconfig.dto.clueassign.ClueAssignLimitDTO;
-import com.kuaidao.businessconfig.dto.clueassign.ClueAssignLimitPageParam;
-import com.kuaidao.businessconfig.dto.clueassign.ClueAssignLimitReq;
-import com.kuaidao.common.constant.SysErrorCodeEnum;
-import com.kuaidao.common.entity.IdEntityLong;
-import com.kuaidao.common.entity.JSONResult;
-import com.kuaidao.common.entity.PageBean;
 
 /**
  * 资源分配量
- * 
  * @author zxy
- *
  */
-@FeignClient(name = "business-config-service", path = "/businessConfig/clueAssignLimit",
-        fallback = ClueAssignLimitFegin.HystrixClientFallback.class)
+@FeignClient(name = "business-config-service", path = "/businessConfig/clueAssignLimit", fallback = ClueAssignLimitFegin.HystrixClientFallback.class)
 public interface ClueAssignLimitFegin {
-
 
     /**
      * 新增资源分配量
-     *
      * @param commentInsertReq
      * @return
      */
     @PostMapping("/create")
-    public JSONResult<Long> create(@RequestBody ClueAssignLimitReq clueAssignLimitReq);
+    JSONResult<Long> create(@RequestBody ClueAssignLimitReq clueAssignLimitReq);
 
     /**
      * 修改资源分配量
-     *
      * @param commentInsertReq
      * @return
      */
     @PostMapping("/update")
-    public JSONResult<Long> update(@RequestBody ClueAssignLimitReq clueAssignLimitReq);
+    JSONResult<Long> update(@RequestBody ClueAssignLimitReq clueAssignLimitReq);
 
     /**
      * 根据id查询资源分配量
-     *
      * @param commentInsertReq
      * @return
      */
     @PostMapping("/get")
-    public JSONResult<ClueAssignLimitDTO> get(@RequestBody IdEntityLong idEntity);
+    JSONResult<ClueAssignLimitDTO> get(@RequestBody IdEntityLong idEntity);
 
     /**
      * 查询资源分配量列表
-     *
      * @param commentInsertReq
      * @return
      */
     @PostMapping("/list")
-    public JSONResult<PageBean<ClueAssignLimitDTO>> list(
-            @RequestBody ClueAssignLimitPageParam pageParam);
+    JSONResult<PageBean<ClueAssignLimitDTO>> list(@RequestBody ClueAssignLimitPageParam pageParam);
 
 
+    /**
+     * 删除资源分配量
+     * @param idList
+     * @return
+     */
+    JSONResult<String> delete(@RequestBody IdListLongReq idList);
 
     @Component
     static class HystrixClientFallback implements ClueAssignLimitFegin {
 
         private static Logger logger = LoggerFactory.getLogger(ClueAssignLimitFegin.class);
 
-
         private JSONResult fallBackError(String name) {
             logger.error(name + "接口调用失败：无法获取目标服务");
-            return new JSONResult().fail(SysErrorCodeEnum.ERR_REST_FAIL.getCode(),
-                    SysErrorCodeEnum.ERR_REST_FAIL.getMessage());
+            return new JSONResult().fail(SysErrorCodeEnum.ERR_REST_FAIL.getCode(), SysErrorCodeEnum.ERR_REST_FAIL.getMessage());
         }
 
         @Override
@@ -96,6 +92,10 @@ public interface ClueAssignLimitFegin {
             return fallBackError("查询资源分配量列表");
         }
 
+        @Override
+        public JSONResult<String> delete(IdListLongReq idList) {
+            return fallBackError("删除资源分配量");
+        }
     }
 
 }
