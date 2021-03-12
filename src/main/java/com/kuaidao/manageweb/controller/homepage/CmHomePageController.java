7536c7e373c057107ff2eb478ccc593b05961dd8
yangbiao <yangbiao@kd.cn>
2021-03-12 11:29:55 +0800

    提交

--- src/main/java/com/kuaidao/manageweb/controller/homepage/CmHomePageController.java
+++ src/main/java/com/kuaidao/manageweb/controller/homepage/CmHomePageController.java
@@ -1,6 +1,7 @@
 package com.kuaidao.manageweb.controller.homepage;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -75,6 +76,19 @@ public class CmHomePageController {
         request.setAttribute("user", userInfoRespDTO);
 
         List<IndexModuleDTO> menuList = user.getMenuList();
+        if(CollectionUtils.isNotEmpty(menuList)){
+            menuList = menuList.stream().filter(a->{
+                if(a.getId()!=null&&a.getId()==15){
+                    return false;
+                }
+                return true;
+            }).filter(a->{
+                if(a.getParentId()!=null&&  a.getParentId()==15){
+                    return false;
+                }
+                return true;
+            }).collect(Collectors.toList());
+        }
         request.setAttribute("menuList", menuList);
         request.setAttribute("isUpdatePassword", isUpdatePassword);
         request.setAttribute("wsUrlHttp", wsUrlHttp);
