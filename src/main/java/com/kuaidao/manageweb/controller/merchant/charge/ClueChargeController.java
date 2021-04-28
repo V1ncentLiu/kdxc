a55dc06f3b1c9f7e297a2370cefbf89ffb514623
fengyx <18734904593@163>
2021-04-28 16:21:04 +0800

    修改资费标准发送系统消息

--- src/main/java/com/kuaidao/manageweb/controller/merchant/charge/ClueChargeController.java
+++ src/main/java/com/kuaidao/manageweb/controller/merchant/charge/ClueChargeController.java
@@ -12,6 +12,7 @@ import com.kuaidao.manageweb.EmailSend;
 import com.kuaidao.manageweb.config.LogRecord;
 import com.kuaidao.manageweb.config.LogRecord.OperationType;
 import com.kuaidao.manageweb.constant.MenuEnum;
+import com.kuaidao.manageweb.feign.announcement.BusReceiveFeignClient;
 import com.kuaidao.manageweb.feign.dictionary.DictionaryItemFeignClient;
 import com.kuaidao.manageweb.feign.merchant.charge.ClueChargeFeignClient;
 import com.kuaidao.manageweb.feign.merchant.user.MerchantUserInfoFeignClient;
@@ -20,6 +21,7 @@ import com.kuaidao.merchant.dto.charge.MerchantClueChargeDTO;
 import com.kuaidao.merchant.dto.charge.MerchantClueChargePageParam;
 import com.kuaidao.merchant.dto.charge.MerchantClueChargeReq;
 import com.kuaidao.sys.constant.SysConstant;
+import com.kuaidao.sys.dto.announcement.bussReceive.BussReceiveInsertAndUpdateDTO;
 import com.kuaidao.sys.dto.dictionary.DictionaryItemRespDTO;
 import com.kuaidao.sys.dto.user.UserInfoDTO;
 import freemarker.template.Configuration;
@@ -27,6 +29,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
@@ -53,6 +57,7 @@ import java.util.Map;
 @Controller
 @RequestMapping("/merchant/clueChargeManager")
 public class ClueChargeController {
+    private Logger logger = LoggerFactory.getLogger(getClass());
     @Autowired
     private ClueChargeFeignClient clueChargeFeignClient;
     @Autowired
@@ -68,6 +73,8 @@ public class ClueChargeController {
 
     @Autowired
     private Configuration configuration;
+    @Autowired
+    private BusReceiveFeignClient busReceiveFeignClient;
     /***
      * 资源资费列表页
      * 
@@ -177,10 +184,21 @@ public class ClueChargeController {
                 //根据ID查询
                 JSONResult<UserInfoDTO> userInfoDTOJSONResult = userInfoFeignClient.get(new IdEntityLong(merchantClueChargeReq.getMainAccountId()));
                 if(JSONResult.SUCCESS.equals(userInfoDTOJSONResult.getCode()) && userInfoDTOJSONResult.getData()!=null && StringUtils.isNotBlank(userInfoDTOJSONResult.getData().getEmail())){
+                    String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm");
+                    pushMessage(merchantClueChargeReq.getMainAccountId(), "资费调整通知", "于"+time+" 已调整了您的资源计费规则，详情如下，请知晓。<br/>\n" +
+                            "账户名称：\n" +
+                            "\n" +
+                            "计费标准：\n" +
+                            "\n" +
+                            "账户信息\n" +
+                            "\n" +
+                            userInfoDTOJSONResult.getData().getName()+
+                            "\n" +
+                            "每条资源 "+merchantClueChargeReq.getCharge()+"个餐盟币", merchantClueChargeReq.getId()+"");
                     Map<String,Object> dataMap = new HashMap<>();
                     dataMap.put("name",userInfoDTOJSONResult.getData().getName());
                     dataMap.put("charge",merchantClueChargeReq.getCharge());
-                    dataMap.put("time", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm")));
+                    dataMap.put("time",time ));
                     String emailContent = FreeMarkerTemplateUtils.processTemplateIntoString(this.configuration.getTemplate("email/huijuMerchantCharge.html"),dataMap);
                     emailSend.sendEmail("【招商宝】资费调整通知",emailContent,userInfoDTOJSONResult.getData().getEmail());
                 }
@@ -188,6 +206,18 @@ public class ClueChargeController {
         } catch (Exception e) {
             log.error("ClueChargeController sendEmail e{}",e);
         }
+    }
+
+    public void pushMessage(Long userId, String title, String content, String remark) {
+        // 发送后台推送消息
+        logger.info("发送推送消息:{{}},内容{{}}", title, content);
+        BussReceiveInsertAndUpdateDTO rev = new BussReceiveInsertAndUpdateDTO();
+        rev.setTitle(title);
+        rev.setReceiveUser(userId);
+        rev.setContent(content);
+        rev.setRemark(remark);
+        JSONResult insertAndSent = busReceiveFeignClient.insertAndSent(rev);
+        logger.info("发送推送消息结果:" + insertAndSent);
     }
     /**
      * 获取当前登录账号ID
